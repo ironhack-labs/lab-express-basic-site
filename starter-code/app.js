ed665d275c0030389b187fbf400932d17e42d91a
JMBarcelo <jmbbazal@gmail.com>
2018-04-11 00:16:35 +0200

    Final

---
+++ starter-code/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (request, response, next) => {
+  response.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/photos', (request, response, next) => {
+  response.sendFile(__dirname + '/views/photos.html');
+});
+
+app.listen(3000, () => {
+  console.log('Listening');
+});
