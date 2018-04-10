e441542011d6efe42add3045b5ddbf1dc901e7ef
A-Tr <alvarotrancon@gmail.com>
2018-04-10 17:38:37 +0200

    primeros inicios de la pagina

---
+++ starter-code/app.js
@@ -0,0 +1,21 @@
+// Require Express
+const express = require("express");
+
+// Express server handling requests and responses
+const app = express();
+
+// Make everything inside of public/ available
+app.use(express.static("public"));
+app.use(express.static("views"));
+
+// our first Route:
+app.get("/", (request, response, next) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+app.get;
+
+// Server Started
+app.listen(3000, () => {
+  console.log("My first app listening on port 3000!");
+});
