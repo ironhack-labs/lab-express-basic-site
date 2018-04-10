f798cc1aacabe844357ed0a1477c14fd89cd9c6c
Clément Dubrisay <clement.dubrisay@gmail.com>
2018-04-11 00:43:23 +0200

    done

---
+++ public/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  
+    // var images = [
+    //     '<img src="/images/alton-1.jpg" class="margin-right-20"/>',
+    //     '<img src="/images/alton-2.jpg" class="margin-right-20"/>',
+    //     '<img src="/images/alton-3.jpg" class="margin-right-20"/>',
+    //     '<img src="/images/alton-4.jpg" class="margin-right-20"/>',
+    //     '<img src="/images/alton-5.jpg" class="margin-right-20"/>',
+    //     '<img src="/images/alton-6.jpg" class="margin-right-20"/>',
+    // ];
+
+    // $('.images-container').append(images);
+
+    // $('.left-button').click(function(){
+    //     $('.images-container img:eq(0)').animate({"left":-340}, 300);
+    // });
+
+
+
+
+});
