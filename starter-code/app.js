20cbb740b48a790afea534cd92aaf47db1257de9
Ryan Martin <rmart106@fiu.edu>
2018-04-24 12:59:59 -0400

    Done.

--- starter-code/app.js
+++ starter-code/app.js
@@ -14,17 +14,23 @@ get: the HTTP Verb needed to access this page
 request: An object containing information about the request, such as the headers. More on this later.
 response: An object containing information about the response, such as headers and any data we need to send to the client.
 next: We will use this later to handle errors. Leave it there for now.
+__dirname (two underscores) refers to the folder in which our app.js is located. 
 */
 
-
-app.get('/', (request, response, next) => { // Home Route
-  console.log(request);
-  response.send('<h1>Welcome to my Express.js site:)</h1>');
+// Home Route
+app.get('/', (request, response, next) => {
+  response.sendFile(__dirname + '/views/home.html'); 
 });
 
+// About Route
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about.html');
+});
 
-
-
+// About Route
+app.get('/photos', (request, response, next) => {
+  response.sendFile(__dirname + '/views/photos.html');
+});
 
 // Server
 app.listen(3000, () => {
