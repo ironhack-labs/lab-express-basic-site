ccdf41f23b70923c0156ed55ce8317e807f7e3cc
Richard Batista <stalfos@icloud.com>
2018-04-24 12:53:24 -0400

    Assignment complete

--- starter-code/app.js
+++ starter-code/app.js
@@ -6,13 +6,22 @@ app.use(express.static('public'));
 // our first Route
 app.get('/', (request, response, next) => {
 	console.log(request);
-	response.send('<h1>Welcome Ironhacker. :)</h1>');
+	response.sendfile(__dirname + '/views/index.html');
 });
 
-app.get('/cat', (request, response, next) => {
-	response.sendfile(__dirname + '/views/cat-page.html');
+app.get('/index.html', (request, response, next) => {
+	console.log(request);
+	response.sendfile(__dirname + '/views/index.html');
+});
+
+app.get('/about.html', (request, response, next) => {
+	response.sendfile(__dirname + '/views/about.html');
+});
+
+app.get('/gallery.html', (request, response, next) => {
+	response.sendfile(__dirname + '/views/gallery.html');
 });
 
 app.listen(3000, () => {
 	console.log('My first app listening on port 3000!');
-  });
+});
