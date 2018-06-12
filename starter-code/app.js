4a1fdaa3bcd21f8242478c939905397cb3f23e21
Chris Manthey <calined@gmx.de>
2018-06-12 11:51:27 +0200

    made the 3 basic pages with a nav but no content yet

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,1 +1,33 @@
-//
+express = require("express");
+
+app = express();
+
+app.get("/", (request, response, next) => {
+
+	response.sendFile(__dirname + "/views/home.html");
+
+});
+
+
+app.get("/about", (request, response, next) => {
+
+	response.sendFile(__dirname + "/views/about.html");
+
+});
+
+
+app.get("/gallery", (request, response, next) => {
+
+	response.sendFile(__dirname + "/views/gallery.html");
+
+});
+
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.listen(3000, () => {
+
+	console.log("server started listening...");
+
+});
