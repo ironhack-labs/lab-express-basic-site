e33237e219d9eea932b73633a83b4611ecb13820
Paula Quitián <pquitian@idealista.com>
2018-06-22 16:41:20 +0200

    [feat]routes and html pages

--- starter-code/app.js
+++ starter-code/app.js
@@ -9,4 +9,12 @@ app.get('/', (req, res, next)=> {
     res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/about', (req, res, next)=> {
+    res.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/gallery', (req, res, next)=> {
+    res.sendFile(__dirname + '/views/gallery.html');
+});
+
 app.listen(PORT, console.log('READY!'));
