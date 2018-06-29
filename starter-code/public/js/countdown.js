b5c2caf7a45b0b188aa70cd91bcfc964184f59ab
Paula Quitián <pquitian@idealista.com>
2018-06-30 00:13:11 +0200

    Fran's JS fix :DD <3

--- starter-code/public/js/countdown.js
+++ starter-code/public/js/countdown.js
@@ -1,50 +1,46 @@
-// const deadLine = new Date("Aug 10, 2018 17:00:00").getTime();
-
-
-// let x = setInterval(function() {
-
-//     let now = new Date().getTime();
-
-//     let howMuchLeft = deadLine - now;
-
-//     let days = Math.floor(howMuchLeft / (1000 * 60 * 60 * 24));
-//     let hours = Math.floor((howMuchLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//     let minutes = Math.floor((howMuchLeft % (1000 * 60 * 60)) / (1000 * 60));
-//     let seconds = Math.floor((howMuchLeft % (1000 * 60)) / 1000);
-
-//     document.getElementById("tour-date").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-
-//     if (howMuchLeft < 0) {
-//         clearInterval(x);
-//         document.getElementById("tour-date").innerHTML = "Time out";
-//     }
-// }, 1000);
-
-
-
 class Counter {
     constructor(deadline) {
+        this.deadLine = deadline ;
+        this.days, this.hours, this.minutes, this.seconds = ''
+        this.setTime()
+    }
 
-        const deadLine = deadline ;
-        let now = new Date().getTime();
+    howMuchLeft(){
+        return this.deadLine - new Date().getTime() 
+    }
 
-        let howMuchLeft = deadLine - now;
+    setTime(){
+        this.setDays()
+        this.setHours()
+        this.setMinutes()
+        this.setSeconds()
+    }
 
-        this.days = Math.floor(howMuchLeft / (1000 * 60 * 60 * 24));
-        this.hours = Math.floor((howMuchLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        this.minutes = Math.floor((howMuchLeft % (1000 * 60 * 60)) / (1000 * 60));
-        this.seconds = Math.floor((howMuchLeft % (1000 * 60)) / 1000);
+    paintOnWindow() {
+        document.getElementById("tour-date").innerHTML = `${this.days}d ${this.hours}h ${this.minutes}m ${this.seconds}s`;
     }
 
     timer() {
         setInterval(() => {
-            document.getElementById("tour-date").innerHTML = `${this.days}d ${this.hours}h ${this.minutes}m ${this.seconds}s`;
-        
-            // if (howMuchLeft < 0) {
-            //     clearInterval(x);
-            //     document.getElementById("tour-date").innerHTML = "Time out";
-            // }
+            this.setTime()
+            this.paintOnWindow()
         }, 1000);
+    }
+
+    setDays(){
+        this.days = Math.floor(this.howMuchLeft() / (1000 * 60 * 60 * 24));
+    }
+
+    setHours() {
+        this.hours = Math.floor((this.howMuchLeft() % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    }
+
+    setMinutes() {
+        this.minutes = Math.floor((this.howMuchLeft() % (1000 * 60 * 60)) / (1000 * 60));
+    }
+
+    setSeconds() {
+        this.seconds = Math.floor((this.howMuchLeft() % (1000 * 60)) / 1000);
     }
 
 } 
