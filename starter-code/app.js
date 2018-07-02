a38dfc38df37dd31583a6a15e05678573bdc11f1
Ryanjso <ryanjsoderberg@gmail.com>
2018-07-02 19:34:34 -0400

    done

---
+++ starter-code/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.static('public'));
+
+// our first route
+app.get('/', (req, res, next) => {
+  res.sendFile(__dirname + '/views/home.html');
+});
+
+
+app.get('/about', (req, res, next) => {
+  res.sendFile(__dirname + '/views/about.html');
+});
+
+
+app.get('/images', (req, res, next) => {
+  res.sendFile(__dirname + '/views/images.html');
+});
+
+
+
+
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!')
+});
