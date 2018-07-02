d6989b77e63a180a97cc51b311ef45725f92cc98
Alina Kitaeva <akitaeva@gmail.com>
2018-07-02 17:15:50 -0400

    server changed to 3000 from 2000 x 2

--- starter-code/app.js
+++ starter-code/app.js
@@ -23,4 +23,4 @@ app.get('/gallery', (request, response, next)=> {
 
 
 
-app.listen(2000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
