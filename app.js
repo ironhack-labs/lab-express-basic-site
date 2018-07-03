c462844783784b1d674d6dda8d2c358efb37b052
Rodrigo Flores <rflor012@gmail.com>
2018-07-02 21:02:07 -0400

    small changes

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const app = express();
+const path = __dirname;
 
 app.use(express.static('public'));
 
 app.get('/', (request, response, next) => {
   // console.log(reque  st);
-  response.sendFile(__dirname + '/views/home.html');
+  response.sendFile(path + '/views/home.html');
 });
 
 
 app.get('/about-us', (req, res, next) => {
-  res.sendFile(__dirname + '/views/about-us.html');
+  res.sendFile(path + '/views/about-us.html');
 });
 
 app.get('/gallery', (req, res, next) => {
-  res.sendFile(__dirname + '/views/gallery.html');
+  res.sendFile(path + '/views/gallery.html');
 });
 
 
 app.listen(3000, () => {
-  console.log('Its up and running..');
+  console.log("₍˄·͈༝·͈˄₎◞ ̑̑ෆ⃛ (=ↀωↀ=)✧ (^･o･^)ﾉ");
 });
