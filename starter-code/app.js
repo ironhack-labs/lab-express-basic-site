3b731592309378d6677dcd58621ca5d7e3f17745
Julien Chil Hagopian <julienchilhagopian@gmail.com>
2018-07-03 20:54:41 +0200

    files created

---
+++ starter-code/app.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public')); 
+
+app.get('/', (request, response, next) => {
+  response.sendFile(__dirname + '/views/home-page.html');
+});
+
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/photo-gallery.html');
+});
+
+app.listen(3000, () => {
+  console.log('First express projet !!!!')
+});
