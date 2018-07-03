3cbb96baf6722eca965393b0d8197a48a7b81ff1
Montse Badia <mbadia_11@hotmail.com>
2018-07-03 21:24:48 +0200

    done

---
+++ starter-code/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/home', (request, response, next) => {
+  response.sendFile(__dirname + '/views/home-page.html');
+});
+
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about-page.html');
+});
+
+app.get('/photo-gallery', (request, response, next) => {
+  response.sendFile(__dirname + '/views/photo-gallery-page.html');
+});
+
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!')
+});
