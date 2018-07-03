f5c8084c846d1307af788346b220840d856c2bb8
Jorge L <kolmax@gmail.com>
2018-07-03 20:01:45 +0200

    Start the daily exercise

---
+++ starter-code/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+
+
+app.use(express.static('public'))
+app.use(express.static(__dirname + '/node_modules/jquery/dist'))
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'))
+
+
+app.get('/', (request, response, next) => {
+  response.sendFile(__dirname + '/views/index.html');
+})
+
+
+app.get('/gallery', (request, response, next) => {
+  response.sendFile(__dirname + '/views/gallery.html');
+})
+
+
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about.html');
+});
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!')
+})
