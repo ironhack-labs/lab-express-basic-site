492622253d597e356d4cfa93509cd7fe01d3c7cf
Pablo Rezola <pablorezola20@yahoo.es>
2018-07-04 02:09:44 +0200

    falta maquetar

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,18 +1,15 @@
-// Require Express
+
 const express = require('express');
 
-// Express server handling requests and responses
+
 const app = express();
 
-// Make everything inside of public/ available
 app.use(express.static('public'));
 
-// our first Route:
 app.get('/', (request, response, next) => {
   response.sendFile(__dirname + '/views/home.html');
 });
 
-// About route:
 app.get('/About', (request, response, next) => {
   response.sendFile(__dirname + '/views/About.html');
 });
@@ -21,7 +18,6 @@ app.get('/Gallery', (request, response, next) => {
     response.sendFile(__dirname + '/views/Gallery.html');
   });
 
-// Server Started
 app.listen(3000, () => {
   console.log('My first app listening on port 3000!');
 });
