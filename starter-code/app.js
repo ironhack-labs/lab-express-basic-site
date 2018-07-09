5322ca48b12e7171e40478581bb6e85c50a3ce45
xavifserra <38772006+xavifserra@users.noreply.github.com>
2018-07-10 01:40:52 +0200

    end with partials

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,20 +3,19 @@ const app = express();
 const hbs = require('hbs');
 const path = require('path');
 
-let data = {};
+let element = [];
 
-for (let n = 1; n <=20; n++) {
-  Object.defineProperty(data, `image${n}`, {
-    value: `/images/gallery/thumbs/asc${n}.jpg`,
-    writable: true,
-    enumerable: true,
-    configurable: true
+for (let n = 1; n <=19; n++) {
+  element.push({
+    image: "/images/gallery/thumbs/asc" + n + ".jpg"
   });
 }
-console.log(data);
+
+console.log(element);
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static('public'));
 
 app.get('/', (request, response, nextFunction) => {
@@ -28,8 +27,11 @@ app.get('/about', (request, response, nextFunction) => {
 });
 
 app.get('/gallery', (request, response, nextFunction) => {
-  response.render('gallery', data);
+  response.render('gallery', {
+    element
+  });
 });
+
 
 
 app.listen(3000, () => {
--- starter-code/public/js/main.js
+++ starter-code/public/js/main.js
@@ -1,25 +1,24 @@
-window.onload = function () {
+window.onload = () => {
   console.log('document loaded');
+  
+  document.querySelectorAll(".img-thumbnail").forEach((element) => {
+    element.addEventListener('click', (id) => {
+      console.log(element.src.replace(/^.*[\\\/]/, ''));
+      popUp("/images/gallery/" + element.src.replace(/^.*[\\\/]/, ''));
+    });
+  });
 };
 
-window.addEventListener('click', (e) => {
-  e.target.
-  "img-thumbnail"
-});
-$("img-thumbnail").addEventListener("click", (e) => {
-  console.log(e)
-});
+newWindow = undefined;
 
-function popup(img) {
+function popUp(img) {
   image = new Image();
   image.src = (img);
   imageWith = image.width;
   imageHeight = image.height;
-  X = screen.width / 2 - imageWith / 2;
-  Y = screen.height / 2 - imageHeight / 2;
-  if (P != null) P.close();
-  P = window.open(img, "P", "width=" + imageWith + ",height=" + imageHeight + ",left=" + X + ",top=" + Y + ",screenX=" + X + ",screenY=" + Y + "");
-  P.document.title = "Imagen";
-  P.self.focus();
-  return false;
+  xPos = screen.width / 2 - imageWith / 2;
+  yPos = screen.height / 2 - imageHeight / 2;
+  if (newWindow != null) newWindow.close();
+  newWindow = window.open(img, "newWindow", "width=" + imageWith + ",height=" + imageHeight + ",left=" + xPos + ",top=" + yPos + ",screenX=" + xPos + ",screenY=" + yPos + "");
+  newWindow.self.focus();
 }
