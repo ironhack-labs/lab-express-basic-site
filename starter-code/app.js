9768f56562aebb8511089f3d0e4cddfce7c23cc8
xavifserra <38772006+xavifserra@users.noreply.github.com>
2018-07-09 15:49:31 +0200

    Final

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,33 +1,38 @@
-
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
 const path = require('path');
 
-let data = {
-  name: "Ironhacker",
-  bootcamp: "IronHack WebDev",
-};
+let data = {};
+
+for (let n = 1; n <=20; n++) {
+  Object.defineProperty(data, `image${n}`, {
+    value: `/images/gallery/thumbs/asc${n}.jpg`,
+    writable: true,
+    enumerable: true,
+    configurable: true
+  });
+}
+console.log(data);
 
 app.set('view engine', 'hbs');
-app.set('views',__dirname + '/views');
-
+app.set('views', __dirname + '/views');
 app.use(express.static('public'));
 
 app.get('/', (request, response, nextFunction) => {
-  response.render('index',data); 
-});
-
-app.get('/gallery', (request, response, nextFunction) => {
-  response.render('gallery',data);
+  response.render('index');
 });
 
 app.get('/about', (request, response, nextFunction) => {
-  response.render('about', data);
+  response.render('about');
 });
+
+app.get('/gallery', (request, response, nextFunction) => {
+  response.render('gallery', data);
+});
+
 
 app.listen(3000, () => {
   console.log('server listening at localhost port 3000');
-  console.log( __dirname + '/views');
-  console.log( __dirname + '/public');
+  console.log(__dirname);
 });
