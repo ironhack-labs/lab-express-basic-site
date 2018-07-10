4395cb5aae5113af9737ea06daa22a424be6f8db
Pere Busquet <perebusquet87gmail.com>
2018-07-10 17:07:12 +0200

    done

---
+++ starter-code/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express()
+
+// Necesitamos carpeta public para estos ficheros
+app.use(express.static('public'));
+
+//si vas a home
+app.get('/', (request, response, next) => {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/photogallery', (request, response, next) => {
+  response.sendFile(__dirname + '/views/photogallery.html');
+});
+
+//escucha y queda a la espera (no hace falta reiniciar servidor)
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!')
+});
