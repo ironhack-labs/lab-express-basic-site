8be10fced994b5c853ba21e7a7b264cc40e02bae
Jay <luvntruth77@gmail.com>
2018-07-21 20:57:11 -0400

    Add files via upload

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+// our first Route
+app.get('/', (request, response, next) => {
+  console.log(request);
+  response.send('<h1>Welcome Ironhacker. :)</h1>');
+});
+
 app.use(express.static('public'));
 
 app.get('/', (request, response, next) => {
@@ -17,6 +23,11 @@ app.get('/gallery-page', (request, response, next) => {
   console.log(request);
   response.sendFile(__dirname + '/views/gallery-page.html');
 });
+
+app.use(express.static('public'));
+
+//app.get('/', (request, response, next) => {
+
 
 app.listen(3000, () => {
   console.log('My first app listening on port 3000!')
