44d6671448997ac707665a179f80aff8ca4ca830
Helene <Helene@Helenes-MacBook-Pro.local>
2018-08-22 20:28:13 +0200

    my beautiful website

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/index.html"));
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/about.html"));
+});
+
+app.get("/gallery", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/gallery.html"));
+});
+
+app.listen(3000);
