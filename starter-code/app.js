a758072ac7f91e5339efa1a50643e68fe9d89fcc
Nicole Jelonek <nicolejelonek@gmail.com>
2018-08-23 01:32:07 +0200

    Ber Haakon & Nicole

---
+++ starter-code/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+
+app.get('/', (request, response, next) => {
+  response.sendFile(__dirname + '/views/home-page.html');
+});
+
+
+app.get('/best-homes', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about-page.html');
+});
+
+app.get('/best-homes', (request, response, next) => {
+  response.sendFile(__dirname + '/views/gallery.html');
+});
+
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!')
+})
