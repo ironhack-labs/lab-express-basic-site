c0d7adfdf8f7c37d5e503baffac22bb0b41c9ac9
alexrojas <alex.rojas@agintechllc.com>
2018-09-05 13:41:45 -0400

    more animation

--- basicApp/public/javascript/script.js
+++ basicApp/public/javascript/script.js
@@ -10,3 +10,19 @@ $(".foodieMiami").on("mouseover", function(){
   })
   // console.log("carajo")
 })
+
+var card1 = $(".card1")
+
+card1.on("mouseover", function(){
+
+	for(var i = 0; i <= card1.length; i++){
+
+	$(this).animate({
+		opacity: 0.25,
+		height: "toggle"
+		}, 5000, function(){
+			})
+
+	}
+
+})
