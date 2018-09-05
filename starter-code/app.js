1f27701690bfc62d02349e7f70f34ce36db92537
Nicholas Crispino <crispinonicky@gmail.com>
2018-09-05 13:10:52 -0400

    this is a joke future employers dont be mad pls

---
+++ starter-code/app.js
@@ -0,0 +1,27 @@
+// Require Express
+const express = require('express');
+
+// Express server handling requests and responses
+const app = express();
+
+
+app.use(express.static('public'));
+
+// our first Route:
+app.get('/home', (request, response, next) => {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/gallery', (request, response, next) => {
+  response.sendFile(__dirname + '/views/gallery.html');
+});
+
+// Server Started
+app.listen(2000, () => {
+  console.log('My first app listening on port 2000!');
+});
