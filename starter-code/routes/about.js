c55b1b0144ff286020901f335f115a9e7c0befa7
Barbara Koehler <barkoehler@gmail.com>
2018-09-11 14:32:24 -0400

    changed some CSS

--- starter-code/routes/about.js
+++ starter-code/routes/about.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send('/about About');
+  res.render('about', {title: 'About'});
 });
 
 
