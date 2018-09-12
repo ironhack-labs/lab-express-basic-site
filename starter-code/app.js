7f729a75ec8c18f400c63dd8c0541c5ebb396e40
pablotaboada <pablo.tabu@hotmail.com>
2018-09-12 17:57:52 +0200

    done

---
+++ starter-code/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.use(express.static('views'));
+
+
+app.get('/', (request, response, next) => {
+     response.sendFile(__dirname + '/views/index.html');
+  });
+
+  app.get('/about', (request, response, next) => {
+    response.sendFile(__dirname + '/views/about.html');
+ });
+
+ app.get('/gallery', (request, response, next) => {
+  response.sendFile(__dirname + '/views/gallery.html');
+});
+
+
+
+  
+
+  app.listen(3000, () => {
+    console.log('pepe');
+  });
+
+  
+  
