7037b542fde5c32b1bc4b962745f95b7c7ee7822
Alberto Martinez <jose_alberto12216@hotmail.com>
2018-09-26 20:56:35 -0500

    Se finaliza pequeño sitio montado en express by Albert

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,12 +8,21 @@ app.get('/', (request, response, next) => {
   response.sendFile(__dirname + '/views/home.html');
 });
 
+app.get('/home', (request, response, next) => {
+  response.sendFile(__dirname + '/views/home.html');
+});
+
 app.get('/about', (request, response, next) => {
   response.sendFile(__dirname + '/views/about.html');
 });
 
 app.get('/gallery', (request, response, next) => {
   response.sendFile(__dirname + '/views/gallery.html');
+});
+
+// Handle 404
+app.use(function(req, res) {
+  res.send('404: Aquí no hay lo que buscas ', 404);
 });
 
 // Server Started
