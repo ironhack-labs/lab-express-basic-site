868795a6c867b5e6352d13b7950b5c8f0c471783
Bjornhona <asaeri3@hotmail.com>
2018-10-09 22:46:40 +0200

    added some more css and images

--- starter-code/public/bonus.js
+++ starter-code/public/bonus.js
@@ -1,1 +1,7 @@
 console.log('Javascript working, yayyyy!!!');
+
+const coolEffect = document.querySelector('.cool');
+
+coolEffect.addEventListener("click", () => {
+  coolEffect.innerHTML = 'Cool!';
+});
