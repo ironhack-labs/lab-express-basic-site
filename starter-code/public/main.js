9f042eff803e592c366c86b8ffe6781f528c7329
wildhamster26 <38760849+wildhamster26@users.noreply.github.com>
2018-10-31 15:04:44 +0100

    BER-Ziggy-done

---
+++ starter-code/public/main.js
@@ -0,0 +1,12 @@
+let images = document.querySelectorAll("img");
+images[0].addEventListener("click", function(){
+    alert("Expelliarmus!");
+})
+
+images[1].addEventListener("click", function(){
+    alert("Toads!");
+})
+
+images[2].addEventListener("click", function(){
+    alert("Wingardium Leviosa!");
+})
