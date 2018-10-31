0b92077a00cd12027a41601bd6bf46c5c8829679
Anna Sch <anna-schaeffner@gmx.de>
2018-10-31 15:08:08 +0100

    First

---
+++ starter-code/app.js
@@ -0,0 +1,28 @@
+// Require Express
+const express = require('express');
+
+// Express server handling requests and responses
+const app = express();
+
+// Make everything inside of public/ available
+app.use(express.static('public'));
+app.use(express.static('views'));
+
+// our first Route:
+app.get('/', (request, response, next) => {
+  response.sendFile(__dirname + '/views/home-page.html');
+});
+
+// cat route:
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/photogallery', (request, response, next) => {
+  response.sendFile(__dirname + '/views/photogallery.html');
+});
+
+// Server Started
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!');
+});
