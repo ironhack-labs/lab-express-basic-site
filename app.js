203e267fc853197a9615654dc86d69e7ce090dc1
Felicitas Deutges <deutges.ironhack@gmail.com>
2018-10-31 15:13:26 +0100

    done

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (request, response, next) => {
-  response.send("<h1>Home<h1>")
-  // console.log(request);
-  // response.sendFile(__dirname + '/views/home-page.html');
+  response.sendFile(__dirname + '/views/home-page.html');
 });
-app.get('/about', (request, response, next) => { response.send("<h1>About<h1>") })
-app.get('/gallery', (request, response, next) => { response.send("<h1>Gallery<h1>") })
+app.get('/about', (request, response, next) => { response.sendFile(__dirname + '/views/about.html'); })
+app.get('/gallery', (request, response, next) => { response.sendFile(__dirname + '/views/gallery.html'); })
 
 // Server Started
 app.listen(3000, () => {
