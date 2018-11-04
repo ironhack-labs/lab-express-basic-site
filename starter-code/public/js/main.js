dbb9d01d6d3cd8b72dff913705c3c99866f3289c
aemartos <aemartos@gmail.com>
2018-11-04 22:11:52 +0100

    Set menu functionality, set page contents and general styles

--- starter-code/public/js/main.js
+++ starter-code/public/js/main.js
@@ -1,18 +1,27 @@
 
-window.onload = ()=> {
+//window.onload = ()=> {
+
+  const btnMenu = document.getElementById('menu-icon');
+  const menu = document.getElementById('container_menu');
+  const words = document.getElementsByClassName('wColor');
+
 
   const random_color = ()=> {
     let color = '#';
     const letters = ['FF5252','87C860','00FFBB', '0086FF','FFBF00', '894598'];
     color += letters[Math.floor(Math.random() * letters.length)];
-    document.getElementById('container_menu').style.backgroundColor = color;
+    menu.style.backgroundColor = color;
+    [].forEach.call(words, (e)=> {
+      console.log(e);
+      e.style.color = color;
+    });
   }
   random_color();
 
-  const btnMenu = document.getElementById('menu-icon');
 
   btnMenu.onclick = ()=> {
     btnMenu.classList.toggle("clicked");
+    menu.classList.toggle("closed");
   };
 
-}
+//}
