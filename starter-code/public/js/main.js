efc6a44c2ab4b90117c1b2d878fe8171ab0ef84f
aemartos <aemartos@gmail.com>
2018-11-04 20:31:47 +0100

    Cross button change on click

--- starter-code/public/js/main.js
+++ starter-code/public/js/main.js
@@ -1,22 +1,18 @@
-window.onload = function (){
-  function random_color() {
-    var color = '#';
-    var letters = ['FF5252','87C860','00FFBB', '0086FF','FFBF00', '894598'];
+
+window.onload = ()=> {
+
+  const random_color = ()=> {
+    let color = '#';
+    const letters = ['FF5252','87C860','00FFBB', '0086FF','FFBF00', '894598'];
     color += letters[Math.floor(Math.random() * letters.length)];
     document.getElementById('container_menu').style.backgroundColor = color;
   }
   random_color();
-}
 
-var btnMenu = document.getElementsByClassName('menu-icon');
+  const btnMenu = document.getElementById('menu-icon');
 
-// Array.prototype.forEach.call(btnMenu, function(button) {
-//   button.addEventListener('click', function() {
-//     var menuEl = this.querySelectorAll('.menu-icon')[0];
-//     var openedClassname = menuEl.dataset.toggled;
-//     menuEl.classList.toggle(openedClassname);
-//   });
-// });
+  btnMenu.onclick = ()=> {
+    btnMenu.classList.toggle("clicked");
+  };
 
-btnMenu.addEventListener('click', function() {
 }
