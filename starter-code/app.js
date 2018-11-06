91bd8df7485c7bc0ec54445559bb920ac1577d56
nadal084 <nadalriera84@gmail.com>
2018-11-06 10:46:28 +0100

    things to be fixed

---
+++ starter-code/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(express.static('views'));
+
+app.get("/index", (request, response, next) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+app.get("/about", (request, response, next) => {
+  response.sendFile(__dirname + "/views/about.html");
+});
+app.get("/gallery", (request, response, next) => {
+  response.sendFile(__dirname + "/views/gallery.html");
+});
+
+app.listen(port,()=>{console.log("looks like it does the job")})
