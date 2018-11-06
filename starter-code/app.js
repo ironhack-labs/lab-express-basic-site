2d84ebe550ed03a04dbfcdccd456ab791fbc534a
Alfredo Hinarejos <alfredo.hina@gmail.com>
2018-11-06 16:55:04 +0100

    done

---
+++ starter-code/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+
+app.get('/',(req,res) => {
+    res.sendFile(__dirname + '/views/index.html');
+})
+
+app.get('/about.html',(req,res) => {
+    res.sendFile(__dirname + '/views/about.html');
+})
+
+app.get('/gallery.html', (req, res) => {
+    res.sendFile(__dirname + '/views/gallery.html');
+})
+
+
+app.listen(port,() => {
+    console.log('Listening port ${port}');
+})
