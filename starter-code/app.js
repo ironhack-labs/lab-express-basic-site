6fe8f7fcbdf01a0d729624b1d56cd390c6a8b63e
Leticia Olábarri <leticiaolabarri@yahoo.es>
2018-11-06 17:40:33 +0100

    done

---
+++ starter-code/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (request, response, next) => {
+  response.sendFile(__dirname + '/views/home-page.html');
+});
+
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about-page.html');
+});
+
+app.get('/photo-gallery', (request, response, next) => {
+  response.sendFile(__dirname + '/views/photo-gallery.html');
+});
+
+let port = 3000;
+
+app.listen(port, () => {
+  console.log('My first app listening on port 3000!');
+}); 
