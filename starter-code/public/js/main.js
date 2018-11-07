2406d4e3c574b2c17d5206f2a5c9c16129927b11
Fareaha Isnin <fareaha09@gmail.com>
2018-11-07 15:21:11 +0100

    done

---
+++ starter-code/public/js/main.js
@@ -0,0 +1,13 @@
+let pictures = document.querySelectorAll("img");
+pictures[0].addEventListener("click", function(){
+    alert("TULUM,MEXICO");
+})
+pictures[1].addEventListener("click", function(){
+    alert("CHICHEN ITZA");
+})
+pictures[2].addEventListener("click", function(){
+    alert("JUST KEEP SWIMMING");
+})
+pictures[3].addEventListener("click", function(){
+    alert("FEED ME!!!");
+})
