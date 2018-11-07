804dfd7aabd6a1169edee39f5e37ab2d6c229014
Geoffroy Baghdassarian <geoffroy.baghdassarian@gmail.com>
2018-11-07 15:32:37 +0100

    done

---
+++ starter-code/app.js
@@ -0,0 +1,24 @@
+// SET UP ---------------------------------------------------------------------------
+// ####################################################################################
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, ()=> {
+    console.log("Server READY!")
+});
+
+app.use(express.static(__dirname + "/public"));
+
+// ROUTES ----------------------------------------------------------------------------------
+// #########################################################################################
+
+app.get("/", (request, response, next)=>{
+    response.sendFile(__dirname + "/views/home-page.html")
+})
+app.get("/about", (request, response, next)=>{
+    response.sendFile(__dirname + "/views/about-page.html")
+})
+app.get("/gallery", (request, response, next)=>{
+    response.sendFile(__dirname + "/views/gallery-page.html")
+})
