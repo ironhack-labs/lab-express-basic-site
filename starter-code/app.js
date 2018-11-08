119155a02308616e7d6a0e7cb1aecfaa0bfe090d
aemartos <aemartos@gmail.com>
2018-11-08 15:59:21 +0100

    Change to dynamic views

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,19 +1,37 @@
 
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
-app.use(express.static('public'));
+const path = require('path');
 
+//app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/home.html');
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+  let data = {
+    name: "home"
+  };
+  res.render(data.name, data);
+  //res.sendFile(__dirname + '/views/home.html');
 });
 
-app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/views/about.html');
+app.get('/about', (req, res, next) => {
+  let data = {
+    name: "about"
+  };
+  res.render(data.name, data);
+  //res.sendFile(__dirname + '/views/about.html');
 });
 
-app.get('/gallery', (req, res) => {
-  res.sendFile(__dirname + '/views/gallery.html');
+app.get('/gallery', (req, res, next) => {
+  let data = {
+    name: "gallery"
+  };
+  res.render(data.name, data);
+  //res.sendFile(__dirname + '/views/gallery.html');
 });
 
 const port = 3000;
