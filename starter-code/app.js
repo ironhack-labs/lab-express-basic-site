3722ad74fc7a48dc7f48d9c88e6fb54bce70db94
aemartos <aemartos@gmail.com>
2018-11-08 17:07:05 +0100

    Add layout and partial

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,34 +4,46 @@ const hbs = require('hbs');
 const app = express();
 const path = require('path');
 
-//app.use(express.static('public'));
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+//Function that allows to compare two statements
+hbs.registerHelper('ifCond', function(v1, v2, options) {
+  //console.log(v1,v2);
+  if(v1 === v2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
+
 
 app.get('/', (req, res, next) => {
   let data = {
-    name: "home"
+    //layout: false,
+    name: "home",
+    home: true
   };
   res.render(data.name, data);
-  //res.sendFile(__dirname + '/views/home.html');
 });
 
 app.get('/about', (req, res, next) => {
   let data = {
-    name: "about"
+    name: "about",
+    about: true
   };
   res.render(data.name, data);
-  //res.sendFile(__dirname + '/views/about.html');
 });
 
 app.get('/gallery', (req, res, next) => {
   let data = {
-    name: "gallery"
+    name: "gallery",
+    gallery: true
   };
   res.render(data.name, data);
-  //res.sendFile(__dirname + '/views/gallery.html');
 });
 
 const port = 3000;
