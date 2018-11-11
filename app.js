ff8e4fa60bf6887e2cf14f65912b2b55346ea0e5
Isaiah Gonzalez <isaiah@Isaiahs-MacBook-Pro-2.local>
2018-11-11 13:11:08 -0500

    done

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const app = express();
+
+
+
+app.use(express.static('public'));
+
+
+
+
+
+app.get('/', (req, res, next)=>{
+    res.sendFile(__dirname + '/views/index.html');
+  })
+
+
+
+  app.get('/about', (req, res, next)=>{
+    res.sendFile(__dirname + '/views/about.html');
+  })
+
+
+  app.get('/info', (req, res, next)=>{
+    res.sendFile(__dirname + '/views/info.html');
+  })
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('My first app listening on port 3000!')
+  });
