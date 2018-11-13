0fdf460f75bd5cc27bbe2263c05df401caeb4d85
David Diez <ddiezr@gmail.com>
2018-11-13 19:22:18 +0100

    First Web Iteration 3 get routes

---
+++ starter-code/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+var path = require("path");
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+// Index 
+var serveIndex = require('serve-index');
+app.use('/', serveIndex(path.join(__dirname, 'public'))); 
+
+
+app.get('/home', (request, response, next) => {
+
+    response.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/about', (request, response, next) => {
+    response.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/gallery', (request, response, next) => {
+    response.sendFile(__dirname + '/views/gallery.html');
+});
+
+app.listen(3000, () => {
+    console.log('My first app listening on port 3000!')
+});
