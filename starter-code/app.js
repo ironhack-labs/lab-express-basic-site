604e069d735b90de283503380d4bbeea2cc760c8
Ricardo Cefalo <ricardo.cefalo@gmail.com>
2018-11-13 22:00:55 +0100

    Important stuff done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,18 +1,25 @@
-
 const express = require('express');
-
 
 const app = express();
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 app.get('/', (request, response, next) => {
+  console.log(request);
   response.sendFile(`${__dirname}/public/views/index.html`);
 });
+app.get('/about', (request, response, next) => {
+  console.log(request);
+  response.sendFile(`${__dirname}/public/views/about.html`);
+});
+app.get('/gallery', (request, response, next) => {
+  console.log(request);
+  response.sendFile(`${__dirname}/public/views/gallery.html`);
+});
 app.get('/CSS', (request, response, next) => {
+  console.log(request);
   response.sendFile(`${__dirname}/public/CSS/styles.css`);
 });
-
 
 app.listen(3000, () => {
   console.log('Server ready');
