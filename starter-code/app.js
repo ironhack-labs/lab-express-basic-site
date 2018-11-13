6c3521bc3857472de543fb41c23ad28c2341782c
Javier Samper <jsamperarias@hotmail.com>
2018-11-13 19:11:45 +0100

    Servidor funcional

---
+++ starter-code/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.static('views'));
+app.use(express.static('public'));
+
+// our first Route
+app.get('/', (request, response, next) => {
+    console.log(request);
+    response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/about', (request, response, next) => {
+    console.log(request);
+    response.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/galery', (request, response, next) => {
+    console.log(request);
+    response.sendFile(__dirname + '/views/index.html');
+});
+
+app.listen(3000, () => {
+    console.log('My first app listening on port 3000!');
+});
