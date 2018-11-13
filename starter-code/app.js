3d7c4e8f8fb47a7afb09db495a15786fac1ca792
PomPom248 <sofiroggero@gmail.com>
2018-11-13 19:45:50 +0100

    done

---
+++ starter-code/app.js
@@ -0,0 +1,23 @@
+const express = require ("express");
+const path = require("path")
+const app = express();
+
+app.get(express.static("public"));
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.get('/', (request, response, next) => {
+   // console.log(__dirname)
+    response.sendFile(__dirname + '/views/home.html');
+  });
+
+  app.get('/about', (request, response, next) => {
+    response.sendFile(__dirname + '/views/about.html');
+  });
+
+  app.get('/photos', (request, response, next) => {
+    response.sendFile(__dirname + '/views/photo-gallery.html');
+  });
+
+app.listen(3000, ()=>{
+    console.log("Basic Express Site");
+})
