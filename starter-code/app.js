f100df236ae9f0c297074c5cc68b7d5e4fb48f67
David <davidmunozvi@icloud.com>
2018-11-14 09:26:43 +0100

    new styles/exercise finished

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,8 +3,17 @@ const app = express();
 
 app.use(express.static("views"));
 app.use(express.static("public"));
+
 app.get('/', (request, response, next) => {
     response.sendFile(__dirname + '/views/home-page.html');
+  });
+
+
+  app.get('/about', (request, response, next) => {
+    response.sendFile(__dirname + '/views/about.html');
+  });
+  app.get('/gallery', (request, response, next) => {
+    response.sendFile(__dirname + '/views/gallery.html');
   });
 
   app.listen(3000, ()=>{
