ded53637fde0883eeca0bf7bdcf472c1c41c37ce
aterron84 <aterron84@gmail.com>
2018-11-14 09:14:24 +0100

    complete with some frontend elements

---
+++ starter-code/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+
+const app = express();
+// app.get('/', (request, response, next) => {
+//     console.log(request);
+//     response.send('<h1>Welcome Ironhacker. :)</h1>');
+//   });
+
+app.use(express.static('public'));
+
+  app.get('/', (request, response, next) => {
+    response.sendFile(__dirname + '/views/index.html');
+  });
+  app.get('/about', (request, response, next) => {
+    response.sendFile(__dirname + '/views/about.html');
+  });
+  app.get('/photo-gallery', (request, response, next) => {
+    response.sendFile(__dirname + '/views/photo-gallery.html');
+  });
+
+  app.listen(3000, () => {
+    console.log('My first app listening on port 3000!');
+  });
