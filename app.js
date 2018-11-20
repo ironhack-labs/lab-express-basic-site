5852e972aa60c5bf569ffacf4397afa3a9550114
EloK6 <ecassignol@gmail.com>
2018-11-20 21:32:20 +0100

    done

---
+++ app.js
@@ -0,0 +1,28 @@
+// Require Express
+const express = require("express");
+
+// Express server handling requests and responses
+const app = express();
+
+// Make everything inside of public/ available
+app.use(express.static("public"));
+
+// index :
+app.get("/", (request, response, next) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+// gallery route:
+app.get("/gallery", (request, response, next) => {
+  response.sendFile(__dirname + "/views/gallery.html");
+});
+
+// about route:
+app.get("/about", (request, response, next) => {
+  response.sendFile(__dirname + "/views/about.html");
+});
+
+// Server Started
+app.listen(3000, () => {
+  console.log("My first app listening on port 3000!");
+});
