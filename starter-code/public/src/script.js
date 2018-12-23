96fe4f5a10a761e7ac5803e766513f375b40e54e
daniloxxv <danilosoares@gmail.com>
2018-12-23 09:19:51 -0600

    updaded

--- starter-code/public/src/script.js
+++ starter-code/public/src/script.js
@@ -1,19 +1,29 @@
 var photoButton = document.getElementById("photobutton");
 var gallery = document.getElementById("gallery");
+var quote = document.getElementById("mannyquote");
+
 photoButton.onclick = function() {
-    if (gallery.innerHTML == `<img src="/images/manny_title.jpg">`) {
-        gallery.innerHTML = `<img src="/images/manny_cosplay.jpg">`;
-    }
-    else if (gallery.innerHTML == `<img src="/images/manny_cosplay.jpg">`) {
-        gallery.innerHTML = `<img src="/images/Manny_calavera_2.png">`;
-    }
-    else if (gallery.innerHTML == `<img src="/images/Manny_calavera_2.png">`) {
+    
+     if (gallery.innerHTML == `<img src="/images/Manny_calavera_2.png">`) {
         gallery.innerHTML = `<img src="/images/Manny_calavera_3.jpg">`;
+        quote.innerHTML = "The living still give me the creeps.";
     }
+
     else if (gallery.innerHTML == `<img src="/images/Manny_calavera_3.jpg">`) {
         gallery.innerHTML = `<img src="/images/Manny_calavera_4.png">`;
+        quote.innerHTML = "I'm the Grim Reaper, Lardass, and you're my next customer.";
     }
-    else {
+
+    else if (gallery.innerHTML == `<img src="/images/Manny_calavera_4.png">`){
         gallery.innerHTML = `<img src="/images/manny_title.jpg">`;
+        quote.innerHTML = "As a rule, I never touch anything more sophisticated and delicate than myself.";
+    }
+    else if (gallery.innerHTML == `<img src="/images/manny_title.jpg">`) {
+        gallery.innerHTML = `<img src="/images/manny_cosplay.jpg">`;
+        quote.innerHTML = "Can you hear the sound of us not talking?";
+    }
+    else  {
+        gallery.innerHTML = `<img src="/images/Manny_calavera_2.png">`;
+        quote.innerHTML = "My scythe... I like to keep it next to where my heart used to be."
     }
 }
