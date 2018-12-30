2924fd3a37fc57c4431f21c7d3954d6b2394f3cb
Nerea <nereaparicio@gmail.com>
2018-12-30 01:17:13 +0100

    add nav pagess

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,3 +12,14 @@ app.listen (PORT, () => {
   console.info(`App listen at ${PORT} port`);
 });
 
+app.get('/', (request, response, next) => {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/gallery', (request, response, next) => {
+  response.sendFile(__dirname + '/views/gallery.html');
+});
