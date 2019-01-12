306c2a2b15e0e09ee1afa54308be133eecc19f58
“leonzlotnik” <zlotnik.leon91@gmail.com>
2019-01-12 09:26:19 -0600

    final

---
+++ starter-code/public/javascripts/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  console.log('IronGenerator JS imported successfully!');
+
+}, false);
+
+function changeColor(){
+ 
+  var color = "#f4ee42";
+
+  document.getElementById("yellow");
+
+  document.fgColor = color;
+}
