b7c2fabaed53a88a2325682cf899af5b477fefec
Alvaro Bonilla <alvarobonillavillalba@gmail.com>
2019-01-29 19:55:23 +0100

    done

---
+++ starter-code/app.js
@@ -0,0 +1,27 @@
+// Require Express
+const express = require('express');
+
+// Express server handling requests and responses
+const app = express();
+const fs = require("fs")
+
+// Make everything inside of public/ available
+app.use(express.static('public'));
+
+// our first Route
+app.get('/', (request, response, next) => {
+    response.sendFile(__dirname + '/views/home-page.html');
+});
+
+app.get('/about', (request, response, next) => {
+    response.sendFile(__dirname + '/views/about-page.html');
+});
+
+app.get('/gallery', (request, response, next) => {
+    response.sendFile(__dirname + '/views/gallery-page.html');
+});
+
+// Server Started
+app.listen(3000, () => {
+    console.log('My first app listening on port 3000!');
+});
