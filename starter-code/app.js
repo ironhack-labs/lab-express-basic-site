399e367fd47ff243924fb2d7db42a3e175108aa3
Malte <m.felmy@googlemail.com>
2019-01-29 17:48:59 +0100

    Added dynamic views

--- starter-code/app.js
+++ starter-code/app.js
@@ -7,16 +7,56 @@ const app = express();
 // make everything inside of /public available
 app.use(express.static("public"));
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+
+let pictures = [
+  "michael-1",
+  "michael-2",
+  "michael-3",
+  "michael-4",
+  "michael-5",
+  "michael-6",
+  "michael-7",
+  "michael-8"
+];
+
 // our first route: GET http://localhost:3000
-app.get("/", (request, response, next) => {
-  response.sendFile(__dirname + "/views/home.html")
+app.get("/", (req, res, next) => {
+  let hour = new Date().getHours();
+  let minutes = new Date().getMinutes();
+  shuffle(pictures);
+  res.render("index", {
+    variable1: "<i>the fastest</i>",
+    variable2: 42,
+    hour,
+    minutes,
+    isMorning: hour <= 11,
+    isAfternoon: hour >= 12 && hour <= 18,
+    pictures: pictures
+  });
 });
-app.get("/about", (request, response, next) => {
-  response.sendFile(__dirname + "/views/about.html")
+app.get("/about", (req, res, next) => {
+  res.render("about");
 });
-app.get("/photo-gallery", (request, response, next) => {
-  response.sendFile(__dirname + "/views/photo-gallery.html")
+// app.get("/photo-gallery", (req, res, next) => {
+//   res.sendFile(__dirname + "/views/photo-gallery.html");
+// });
+app.get("/gallery", (req, res, next) => {
+  shuffle(pictures);
+  res.render("gallery", { pictures: pictures });
 });
+
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
 
 // starts the server
 app.listen(3000, () => {
