4db474b2f7a8256cd409a2b77dc26257397f400f
Sofía <sofianombela@gmail.com>
2019-01-29 22:45:24 +0100

    Done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
 
-app.get('/', (request, response, next) => {
+app.use(express.static('public'));
 
+app.get('/', (request, response, next) => {
+    response.sendFile(__dirname + "/views/home.html")
+})
+
+app.get('/about', (request, response, next) => {
+    response.sendFile(__dirname + "/views/about.html")
+})
+
+app.get('/gallery', (request, response, next) => {
+    response.sendFile(__dirname + "/views/images.html")
 })
 
 
