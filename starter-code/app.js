bc5cd273aceea52abbac8d54b9b7132031be2d8a
Guilherme Davi Gomes de Lima <guiionfire@hotmail.com>
2019-01-29 16:50:44 -0200

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,23 +1,25 @@
-const express = require('express')
+/* eslint-disable no-unused-vars */
+const express = require('express');
+
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/",(request, response, next) => {
-    console.log(request);
-    response.sendFile(__dirname + "/public/index.html");
+app.get('/', (request, response, next) => {
+  console.log(request);
+  response.sendFile(`${__dirname}/public/index.html`);
 });
 
-app.get("/about",(request, response, next) => {
-    console.log(request);
-    response.sendFile(__dirname + "/public/about.html");
+app.get('/about', (request, response, next) => {
+  console.log(request);
+  response.sendFile(`${__dirname}/public/about.html`);
 });
 
-app.get("/gallery",(request, response, next) => {
-    console.log(request);
-    response.sendFile(__dirname + "/public/gallery.html");
+app.get('/gallery', (request, response, next) => {
+  console.log(request);
+  response.sendFile(`${__dirname}/public/gallery.html`);
 });
 
 app.listen(3300, () => {
-    console.log("FOI!FOI!FOI!")
+  console.log('Connect to port 3300');
 });
