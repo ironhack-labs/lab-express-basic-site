9ce473756d66049ca6f3ceb42415a3d74553a884
Mile Suarez <milesuarez@hotmail.com>
2019-01-30 09:29:25 +0100

    me parecio muy sencillo solo seguir la leccion del dia !!!

---
+++ starter-code/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+// our first Route
+app.get('/', (request, response, next) => {
+   
+    response.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/about', (request, response, next) => {
+    
+    response.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/gallery', (request, response, next) => {
+  
+  response.sendFile(__dirname + '/views/gallery.html');
+});
+
+
+app.listen(4005)
