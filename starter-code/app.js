fa9d42a28ffc1c8affc60bb492a0d3f3dc058315
Thilo Sperber <t.sperber@gmx.net>
2019-01-30 08:03:58 +0100

    basic version done

---
+++ starter-code/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+app.get('/', (request, response, next) => {
+  response.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/gallery', (request, response, next) => {
+  response.sendFile(__dirname + '/views/gallery.html');
+});
+
+app.listen(3000);
