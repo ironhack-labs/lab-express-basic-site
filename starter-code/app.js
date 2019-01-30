e3276a3748685832e1bd309cc9f80fa046f3da42
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-01-30 15:49:56 +0100

    sushi webpage

---
+++ starter-code/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.render('home');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/photos', (req, res) => {
+  res.render('photos');
+});
+
+
+app.listen(port, () => { console.log(`port ${port}`); });
