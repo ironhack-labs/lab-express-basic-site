3857a657e7ce1143d95aef0fd87f52f8f8486803
Inés Guerra <inesguerra@MacBook-Air-de-Ines.local>
2019-01-30 09:43:32 +0100

    done

---
+++ starter-code/app.js
@@ -0,0 +1,23 @@
+
+
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response, next) => {
+	response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/about', (request, response, next) => {
+	response.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/gallery', (request, response, next) => {
+	response.sendFile(__dirname + '/views/gallery.html');
+});
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+	console.log('My first app listening on port 3000!');
+});
+
