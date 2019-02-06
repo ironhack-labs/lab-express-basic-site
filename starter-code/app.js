9291e4af1689b09a5bf45c0551f3ec8f32c530e6
Isabella Onate <isabellaonate@gmail.com>
2019-02-05 22:09:27 -0600

    Added home.html and styles but can't seem to make css visible in browser

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.listen()
+app.use(express.static('public'))
+
+app.get('/', (req, res)=>{
+	res.sendFile(__dirname + '/views/home.html')
+})
 
 app.listen(3000, ()=>{
 	console.log("Running. Port 3000")
