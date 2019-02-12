7897ac06953021fb16233b4ed0b271341831d783
ChristopheP96 <protin.christophe@gmail.com>
2019-02-12 07:08:23 +0100

    done

---
+++ starter-code/app.js
@@ -0,0 +1,58 @@
+const createError = require('http-errors');
+const express = require('express');
+const ejs = require('ejs');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+// const cookieParser = require('cookie-parser');
+// const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+const port = 3000;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('layout', 'layouts/main');
+
+app.use(expressLayouts);
+
+// app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use(express.static('public'));
+app.use(expressLayouts);
+
+app.get('/about', (req, res, next) => {
+  res.render('about');
+  next();
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Start the server
+app.listen(port, () => console.log(`Everything is listening on port ${port}!`));
+
+module.exports = app;
