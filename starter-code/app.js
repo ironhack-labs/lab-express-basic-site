a31f1b938d1835ca6d6e4ae982b86d95b101f57d
Carlos Martínez Crespo <carlos.martinez.crespo@gmail.com>
2019-02-27 08:33:03 +0100

    done

---
+++ starter-code/app.js
@@ -0,0 +1,28 @@
+// Require Express
+const express = require('express');
+
+// Express server handling requests and responses
+const app = express();
+
+// Make everything inside of public/ available
+app.use(express.static('public'));
+
+// home route:
+app.get('/', (request, response, next) => {
+    response.sendFile(__dirname + '/views/home.html');
+});
+
+// about route:
+app.get('/about', (request, response, next) => {
+    response.sendFile(__dirname + '/views/about.html');
+});
+
+// gallery route:
+app.get('/gallery', (request, response, next) => {
+    response.sendFile(__dirname + '/views/gallery.html');
+});
+
+// Server Started
+app.listen(3000, () => {
+    console.log('Server running!');
+});
