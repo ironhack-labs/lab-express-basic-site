664b3d1158482f66d73b5370dbd45f6385d79897
gui <gbrein@gmail.com>
2019-03-30 14:52:28 -0300

    Clone concluido

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,2 +1,21 @@
-cont express = const express = require('express');
+const express = require('express');
 
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/home', (request, response) => {
+    response.sendFile(`${__dirname}/views/home.html`);
+});
+app.get('/about', (request, response) => {
+    response.sendFile(`${__dirname}/views/about.html`);
+});
+app.get('/gallery', (request, response) => {
+    response.sendFile(`${__dirname}/views/gallery.html`);
+});
+app.get('/', (request, response) => {
+    response.sendFile(`${__dirname}`);
+});
+
+
+app.listen(3001);
