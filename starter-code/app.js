663796caf254bf927327cb5245521359b6f4169a
Luca Manfredi <luca.manfredi1987@gmail.com>
2019-04-09 23:46:29 +0200

    done css

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,17 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/homepage", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/about-us", (req, res) => {
+app.get("/about", (req, res) => {
     res.sendFile(__dirname + '/views/about.html');
 });
 
 app.get("/photo-gallery", (req, res) => {
     res.sendFile(__dirname + '/views/photo.html');
 });
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function () {
     console.log(`I am listening on port ${PORT}`);
