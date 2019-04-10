994343c9fd17778dbdc4d4f237c7f91fa3dad701
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2019-04-10 09:37:15 +0200

    basic home site. not finished

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const hbs = require("hbs");
 const app = express(); //app is an object
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + "/views/partials");
 
 // app sets public folder ressources as accessible : this is mandatory if clients must access some static resources, such as images, etc
 app.use(express.static(__dirname + "/public"));
 
-//On the base route (http://localhost:3010/), listen to get request comming
-app.get("/", (request, response) => {
-  response.send("<h1>Hello world</h1>");
-});
 
-app.get("/home", (request, response) => {
+
+//On the base route (http://localhost:3010/), listen to get request comming
+// app.get("/", (request, response) => {
+//   response.send("<h1>Hello world</h1>");
+// });
+
+app.get("/", (request, response) => {
   response.sendFile(`${__dirname}/views/home.html`);
 });
 
@@ -22,6 +28,22 @@ app.get("/courses", (request, response) => {
   response.sendFile(`${__dirname}/views/courses.html`);
 });
 
-app.listen(3102, () => {
-  console.log("my app started @ http://localhost:3102")
+
+app.listen(3100, () => {
+  console.log("my app started @ http://localhost:3100")
 });
+
+
+//Partial routes
+// app.get('/head', (req, res, next) => {
+//   res.render('head');
+// });
+
+
+
+// //HBS file
+// app.get('/', (req, res, next) => {
+//   // send views/index.hbs for displaying in the browser
+//   res.render('home');
+// });
+
