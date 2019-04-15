f0bcdce3d9e4362c50f4530fbd94ea6c8f86616e
Susan Almassi <susan.z.almassi@gmail.com>
2019-04-15 10:33:36 +0200

    added partials

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const app = express();
+const hbs = require("hbs");
+app.set("view engine", "hbs");
 
 app.use(express.static("public"));
+hbs.registerPartials(__dirname + "/views/partials");
 
 app.get("/", (request, response, next) => {
-  response.sendFile(__dirname + "/views/home.html");
+  response.render("home");
 });
 
 app.get("/about", (request, response, next) => {
-  response.sendFile(__dirname + "/views/about.html");
+  response.render("about");
 });
 
 app.get("/gallery", (request, response, next) => {
-  response.sendFile(__dirname + "/views/gallery.html");
+  response.render("gallery");
 });
 
 // app.get("/gallery", (request, response, next) => {
@@ -31,6 +34,6 @@ app.get("/gallery", (request, response, next) => {
 //   response.sendFile(__dirname + "/views/what-to-do.html");
 // });
 
-app.listen(1000, () => {
-  console.log("your express app is running at: http://localhost:1000");
+app.listen(4000, () => {
+  console.log("your express app is running at: http://localhost:4000");
 });
