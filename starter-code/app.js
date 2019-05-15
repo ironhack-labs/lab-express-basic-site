9f9fcd5a99bf559e6c266ed3a0add3025aa18af1
Lucia Astray <lu@MacBook-Pro-de-Lucia.local>
2019-05-15 09:32:09 +0200

    hecho

---
+++ starter-code/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/home.html');
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/gallery', (req, res) => {
+  res.sendFile(__dirname + '/views/gallery.html');
+});
+
+app.listen(port, () => {
+  console.log(`App escuchando en el puerto ${port}`)
+});
+
---
+++ starter-code/public/js/gallery.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName("photos").onclick = (() => {
+
+  document.getElementsByClassName("photos").forEach((photo) => {
+    photo.style.width = "800px"
+    //document.getElementsByClassName("photos").forEach( (photo) => photo.style.)
+  })
+})
