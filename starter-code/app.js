66d6dbb2105044dc8ccc18e617438f3ae8c79ebb
root <root@JORGE.localdomain>
2019-05-15 09:09:11 +0200

    Express_Dayli_Exercice_2-Day_4-Week

---
+++ starter-code/app.js
@@ -0,0 +1,23 @@
+// 1.- Requerir Express
+const express = require("express");
+
+// 2.- Instanciar Express
+const app = express();
+const port = 3000;
+
+// 3.- Definir directorio de recursos estáticos
+app.use(express.static("public"));
+
+// 4.- Enrutar
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home_page.html");
+});
+
+app.get("/cat", (req, res) => {
+  res.sendFile(__dirname + "/views/airport_page.html");
+});
+
+// 5.- Abrir servidor
+app.listen(port, () => {
+  console.log(`App escuchando en el puerto ${port}`);
+});
