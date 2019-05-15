36f7fd5a7142d1dd1193b389a0447cdd42916143
Mariel ML <marielml@MacBook-Pro-de-Mariel.local>
2019-05-15 12:03:52 +0200

    incomplete

---
+++ starter-code/app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.sendFile(__dirname + view / home - page.html))
