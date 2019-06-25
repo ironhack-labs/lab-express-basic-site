0bb8b447578b0069e9d3eb3e2d258e587dbf8849
Vanesa Puerta <Vanesa@MacBook-Air-de-Vanesa.local>
2019-06-25 19:35:09 +0200

    done

---
+++ starter-code/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+// Express server handling requests and responses
+const app = express();
+
+// Make everything inside of public/ available
+app.use(express.static('public'));
+
+// home:
+app.get('/', (request, response, next) => {
+  response.sendFile(__dirname + '/views/home-pages.html');
+});
+
+// cat:
+app.get('/about', (request, response, next) => {
+    response.sendFile(__dirname + '/views/about.html');
+  });
+
+// gallery:
+app.get('/gallery', (request, response, next) => {
+  response.sendFile(__dirname + '/views/photo-gallery.html');
+});
+
+// Server Started
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!');
+});
