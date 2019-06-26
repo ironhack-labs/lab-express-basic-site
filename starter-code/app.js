d6d60fcde7b52b7c1890664165ee1b384eeee601
Anna Dontcova <a.dontcova@gmail.com>
2019-06-26 15:12:17 +0200

    done

---
+++ starter-code/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/about", (req, res) => {
+  
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/gallery", (req, res) => {
+
+  res.sendFile(__dirname + "/views/gallery.html");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
+
+app.listen(3000);
