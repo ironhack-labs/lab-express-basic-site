7bc1fea6c127fd0469db9694d647d6c08823f5a4
Taluama Velludo <taluama@gmail.com>
2019-07-03 09:20:06 -0400

    home link added

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,16 +4,16 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.get('/', (request, response, next) => {
-  response.sendFile(__dirname + '/views/home.html');
+app.get('/', (req, res, next) => {
+  res.sendFile(__dirname + '/views/home.html');
 });
 
-app.get('/gallery', (request, response, next) => {
-  response.sendFile(__dirname + '/views/gallery.html');
+app.get('/gallery', (req, res, next) => {
+  res.sendFile(__dirname + '/views/gallery.html');
 });
 
-app.get('/about', (request, response, next) => {
-  response.sendFile(__dirname + '/views/about.html');
+app.get('/about', (req, res, next) => {
+  res.sendFile(__dirname + '/views/about.html');
 });
 
 
