e6a960eac24fa156ace5e17c01c721ef7edbdcd0
arstrel <arstrel@gmail.com>
2019-07-03 10:37:52 -0400

    Some of morning lecture additions

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,23 +1,53 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 app.use(express.static('public'));
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+
+// hbs.registerPartial(__dirname + '/views/partials');
+
+
+
 app.get('/', (request, response) => {
-  response.sendFile(__dirname + '/views/index.html');
+  response.render('index');
 })
 app.get('/about', (request, response) => {
-  response.sendFile(__dirname + '/views/about.html');
+  let stuff = {
+    img: '/images/three.jpg'
+  }
+  response.render('about', {thePerson: stuff});
 })
+app.get('/about/coding', (request, response) => {
+  let stuff = {
+    name: 'coding',
+    img: '/images/one.jpg',
+    bio: 'Some hipster description of coding process',
+    coding: true
+  }
+  response.render('about', {thePerson: stuff});
+})
+app.get('/about/computer', (request, response) => {
+  let stuff = {
+    name: 'Computer',
+    img: '/images/two.jpg',
+    bio: 'Some other picture of coding process',
+    coding: false
+  }
+  response.render('about', {thePerson: stuff});
+})
+
 app.get('/more', (request, response) => {
-  response.sendFile(__dirname + '/views/some-more.html');
+  response.render('some-more');
 })
 
 app.get('*', function(req, response){
-  response.sendFile(__dirname + '/views/404.html');
+  response.render( '404');
 });
 
 app.listen(3000, () => {
-  console.log('Mega website is up and running at post 3000')
+  console.log('Mega componental website is up and running at port 3000')
 })
