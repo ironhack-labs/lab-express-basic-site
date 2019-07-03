a3dfd2ce93f785e4761a590fcc930d7486cefd17
billywonka3 <billywonka3@gmail.com>
2019-07-03 09:05:15 -0400

    done

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+
+app.get('/', (request, response, next) => {
+    response.sendFile(__dirname + '/views/home-page.html');
+  });
+
+  app.get('/about', (request, response, next) => {
+    response.sendFile(__dirname + '/views/about-page.html');
+  });
+ 
+  app.get('/gallery', (request, response, next) => {
+    response.sendFile(__dirname + '/views/photo-gallery.html');
+  });
+
+app.listen(3000, () => {
+    console.log('My first app listening on port 3000!')
+  });
