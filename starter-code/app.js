ce021acce2ab381a21b110cb748cf3411b4de62b
Carla Rodriguez <carlavrodriguezm@gmail.com>
2019-07-23 22:34:29 +0200

    done

---
+++ starter-code/app.js
@@ -0,0 +1,17 @@
+// Requerimiento Express
+const express = require('express')
+const port = 3000
+
+// Instancia Express
+const app = express()
+
+// Directorio público
+app.use(express.static('public'))
+
+// Enrutado
+app.get('/', (req, res) => res.sendFile(`${__dirname}/views/home-page.html`))
+app.get('/about', (req, res) => res.sendFile(`${__dirname}/views/about-page.html`))
+app.get('/photos', (req, res) => res.sendFile(`${__dirname}/views/photo-gallery.html`))
+
+// Escucha servidor
+app.listen(port, () => console.log(`App escuchando en el puerto ${port}`))
