b2b06139e33bdf826d6e4b153faeefa46d7f55da
Roberto Camacho <camachof.roberto@gmail.com>
2019-08-12 17:55:54 -0300

    -Starting Project

---
+++ starter-code/app.js
@@ -0,0 +1,14 @@
+// Require Express
+const express = require('express');
+
+// Express server handling requests and responses
+const app = express();
+
+// Make everything inside of public/ available
+app.use(express.static('public'));
+
+// Home Page
+
+// About Page
+
+// Photo Gallery Page
