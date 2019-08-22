885e3f5fbbaf9c2ac84ad279ab579b6a50299e01
Christoph Butenschön <c.butenschoen@gmail.com>
2019-08-22 21:18:21 +0200

    restructured pages with views; used handlebars to display a list

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,14 +8,17 @@ app.listen(3000, () => {
   console.log('My first app listening on port 3000!')
 });
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+
 app.get('/', (request, response, next) => {
-  response.sendFile(__dirname + '/views/index.html');
+  response.render('index', { username: 'Christoph', classes: ["Marauder", "Witch", "Trickster", "Scion", "Duelist", "Templar", "Shadow"] })
 });
 
 app.get('/about', (request, response, next) => {
-  response.sendFile(__dirname + '/views/about.html');
+  response.render('about')
 });
 
 app.get('/photo-gallery', (request, response, next) => {
-  response.sendFile(__dirname + '/views/photo-gallery.html');
+  response.render('photo-gallery')
 });
