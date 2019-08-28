685e005fd7e99afdad2e7cab13e869a408ed4768
Maxime <justine.gander@gmail.com>
2019-08-28 19:00:15 +0200

    home and about finish

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+hbs.registerPartials(path.join(__dirname + "/views/partials"));
+
+
+app.get("/", (req, res)=>{
+  res.render("index");
+});
+
+app.get("/about", (req, res)=>{
+  res.render("about");
+});
+
+app.get("/contact", (req, res)=> {
+  res.render("contact");
+});
+
+const listener = app.listen(3333, ()=> {
+  console.log(`server is up @ http://localhost:${listener.address().port}`);
+})
