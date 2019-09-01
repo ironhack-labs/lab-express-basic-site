bc3dabdc85301376e5e86c5f5b571fa2fad434c4
Anaïs Lorteau <anaislor@orange.fr>
2019-09-01 18:01:51 +0200

    done

--- starter-code/public/js/app.js
+++ starter-code/public/js/app.js
@@ -1,2 +1,8 @@
-let mainlink = document.querySelectorAll("#main-nav a");
-let title = document.title;
+const burger = document.querySelector("#mobile-icon");
+const navMobile = document.getElementById("nav_mobile");
+
+function toggleMobileNav(evt) {
+  navMobile.classList.toggle("is-active");
+}
+
+burger.addEventListener("click", toggleMobileNav);
