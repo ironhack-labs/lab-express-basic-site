d52951cded4bc6c5d71779d76f3ee46744d6fdf2
Marco Accardi <contact.marco.accardi@gmail.com>
2019-09-04 17:36:49 +0200

    styling updated

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.static("views"));
 
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/home.html");
