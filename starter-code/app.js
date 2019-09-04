1019780f327ce78e7aabb5ffdeccec49cf1c7677
Xavier Martinez <luis.martz7@hotmail.com>
2019-09-03 23:10:00 -0500

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,9 +4,6 @@ const app = express()
 app.use(express.static('public'))
 
 app.get('/', (request, response) => {
-    //Aquí podemos hacer lo que queramos
-    //podemos acceder a la base de datos
-    //realizar operaciones, etc
     response.sendFile(`${__dirname}/views/home-page.html`)
 })
 
@@ -14,8 +11,8 @@ app.get('/about', (req, res) => {
     res.sendFile(`${__dirname}/views/about.html`)
 })
 
-app.get('/photo-galery', (req, res) => {
-    res.sendFile(`${__dirname}/views/photo-galery.html`)
+app.get('/photo-gallery', (req, res) => {
+    res.sendFile(`${__dirname}/views/photo-gallery.html`)
 })
 
 app.listen(3000, () => {
