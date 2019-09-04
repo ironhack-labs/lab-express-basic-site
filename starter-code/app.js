3556ccf10eb93e8f52e2fcf7d65f7b6f61e6d789
guimartinsalmeida <guimartinsalmeida@gmail.com>
2019-09-03 23:37:01 -0500

    done

---
+++ starter-code/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', (request, response) => {
+  response.sendFile(`${__dirname}/views/home-page.html`)
+})
+app.get('/about', (req, res) => {
+  res.sendFile(`${__dirname}/views/about-page.html`)
+})
+app.listen(3000, () => {
+  console.log('server on http://localhost:3000')
+})
