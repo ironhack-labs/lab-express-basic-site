3644ac2527d0f422ef4f6e1e951b7af1892da456
Andre <itstheandre@gmail.com>
2019-09-04 18:11:19 +0200

    Not finished. Will finish as soon as possible

---
+++ starter-code/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.static("views"));
+app.use(express.static("assets"));
+
+// express method
+app.get("/", (request, response) => {
+  response.sendFile(`${__dirname}/views/index.html`); // express method
+});
+
+app.get("/about", (request, response) => {
+  //   response.send("This is the about page");
+  response.sendFile(`${__dirname}/views/about.html`);
+});
+
+app.get("/gallery", (request, response) => {
+  //   response.send("This is the about page");
+  response.sendFile(`${__dirname}/views/gallery.html`);
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port: 3000");
+});
