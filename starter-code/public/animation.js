32784e06fb4c3b509e089e08a626a2db19c19c35
Thuy Le <gitnap@mailbox.org>
2019-09-05 10:47:21 +0200

    Check in files, images, assets. Done.

---
+++ starter-code/public/animation.js
@@ -0,0 +1,217 @@
+    $(document).ready(function () {
+        console.log("document loaded");
+
+        // Home
+
+        $("button.btn.btn-outline-warning").on("click", function () {
+            console.log("button warning was clicked");
+
+            let catSound = document.createElement('audio');
+            catSound.setAttribute('src', 'audio/rainforest-ambience.mp3');
+            catSound.play();
+        });
+
+        $("button.btn.btn-outline-info").on("click", function () {
+            console.log("button info was clicked");
+            window.location.href = '/gallery';
+
+        });
+
+        // Gallery
+
+        $('button#100').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/100');
+        });
+
+        $('button#101').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/101');
+        });
+
+        $('button#200').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/200');
+        });
+
+        $('button#201').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/201');
+        });
+
+        $('button#202').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/202');
+        });
+
+        $('button#204').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/204');
+        });
+
+        $('button#206').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/206');
+        });
+
+        $('button#207').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/207');
+        });
+
+        $('button#300').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/300');
+        });
+
+        $('button#301').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/301');
+        });
+
+        $('button#302').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/302');
+        });
+
+        $('button#303').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/303');
+        });
+
+        $('button#304').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/304');
+        });
+
+        $('button#305').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/305');
+        });
+
+        $('button#307').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/307');
+        });
+
+        $('button#400').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/400');
+        });
+
+        $('button#401').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/401');
+        });
+
+        $('button#402').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/402');
+        });
+
+        $('button#403').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/403');
+        });
+        $('button#404').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/404');
+        });
+
+        $('button#405').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/405');
+        });
+
+        $('button#406').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/406');
+        });
+
+        $('button#408').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/408');
+        });
+
+        $('button#409').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/409');
+        });
+
+        $('button#410').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/410');
+        });
+
+        $('button#411').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/411');
+        });
+
+        $('button#412').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/412');
+        });
+
+        $('button#413').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/413');
+        });
+
+        $('button#414').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/414');
+        });
+
+        $('button#415').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/415');
+        });
+
+        $('button#416').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/416');
+        });
+
+        $('button#417').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/417');
+        });
+
+        $('button#418').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/418');
+        });
+
+        $('button#420').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/420');
+        });
+
+        $('button#421').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/421');
+        });
+
+        $('button#422').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/422');
+        });
+
+        $('button#423').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/423');
+        });
+
+        $('button#424').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/424');
+        });
+
+        $('button#425').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/425');
+        });
+
+        $('button#426').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/426');
+        });
+
+        $('button#429').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/429');
+        });
+
+        $('button#431').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/431');
+        });
+
+        $('button#444').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/444');
+        });
+
+        $('button#450').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/450');
+        });
+
+        $('button#451').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/451');
+        });
+
+        $('button#500').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/500');
+        });
+
+        $('button#502').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/502');
+        });
+
+        $('button#511').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/511');
+        });
+
+        $('button#599').click(function () {
+            $('#cat-image').attr('src', 'https://http.cat/599');
+        });
+    });
+
