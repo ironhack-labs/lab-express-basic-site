5fc5f5213e7af685feb06e8ddc6408c129794b64
borgila <elbienesbueno@gmail.com>
2019-09-10 19:04:18 +0200

    crash

---
+++ starter-code/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express()
+const PORT = 4100
+
+app.use(express.static('public'));
+
+app.get("/home", (req, res) => {
+  res.sendFile(__dirname + '/views/home.html')
+})
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + '/views/about.html')
+})
+
+app.get("/gallery", (req, res) => {
+  res.sendFile(__dirname + '/views/gallery.html')
+})
+
+app.listen(PORT)
+console.log(`Listening on port ${PORT}` )
