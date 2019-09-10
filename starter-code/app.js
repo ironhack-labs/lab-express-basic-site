33b03b8f3139321f7a2014dfec396bb6ad41f19d
Unai Bolivar <ujbolivar@gmail.com>
2019-09-10 18:54:01 +0200

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,22 +1,22 @@
 const express = require("express");
-const app = express();
-const PORT = 4100;
+const app = express()
+const PORT = 4100
 
 app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-    response.sendFile(__dirname + '/views/home.html')
+app.get("/index", (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
 })
 
 app.get("/about", (req, res) => {
-    response.sendFile(__dirname + '/views/about.html')
+    res.sendFile(__dirname + '/views/about.html')
 })
 
 app.get("/gallery", (req, res) => {
-    response.sendFile(__dirname + '/views/gallery.html')
+    res.sendFile(__dirname + '/views/gallery.html')
 })
 
-app.Listen(PORT)
+app.listen(PORT)
 console.log(`Listening on port ${PORT}` )
 
 
