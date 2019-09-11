f208dd3715908490f903aac5debd2e5481de4971
Julio <juhume.exe@gmail.com>
2019-09-11 11:31:35 +0200

    done

---
+++ starter-code/app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+
+const PORT = 3000
+
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html")
+})
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html")
+})
+
+app.get("/gallery", (req, res) => {
+  res.sendFile(__dirname + "/views/gallery.html")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
