5d66bf512201b12ed9bd785300c3f3f962acff8d
guimartinsalmeida <guimartinsalmeida@gmail.com>
2019-09-13 09:45:29 -0500

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const app = express()
+const port = 3000
 
 app.use(express.static('public'))
 
-app.get('/', (request, response) => {
-  response.sendFile(`${__dirname}/views/home-page.html`)
-})
-app.get('/about', (req, res) => {
-  res.sendFile(`${__dirname}/views/about-page.html`)
-})
-app.listen(3000, () => {
-  console.log('server on http://localhost:3000')
-})
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/home.html'))
+app.get('/about', (req, res) => res.sendFile(__dirname + '/views/about.html'))
+app.get('/gallery', (req, res) => res.sendFile(__dirname + '/views/gallery.html'))
+
+app.listen(port, () => console.log(`listening on port ${port}!`))
