516d336f09aeae7dd17c1e08c3ba4a9952fb44a7
Dan Gil <dan.nek0.bass@gmail.com>
2019-09-15 01:47:37 -0500

    done

---
+++ starter-code/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/views/home-page.html`);
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(`${__dirname}/views/about-page.html`);
+});
+
+app.get('/images', (req, res) => {
+  res.sendFile(`${__dirname}/views/images-page.html`);
+});
+
+app.listen(3000, () => {
+  console.log(`server on http://localhost:3000`);
+});
