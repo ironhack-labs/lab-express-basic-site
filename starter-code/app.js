57961a9c0c90c44a24a860684dd45ff46a8b367e
Rafael Dias <rafaels.dias@me.com>
2019-09-25 13:31:28 -0300

    done

---
+++ starter-code/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+// our first Route:
+app.get('/', (request, response, next) => {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+// cat route:
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/photo-gallery', (request, response, next) => {
+  response.sendFile(__dirname + '/views/photo-gallery.html');
+});
+
+
+
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!')
+});
---
+++ starter-code/public/js/script.js
@@ -0,0 +1,24 @@
+
+let h1 = document.getElementById("titulo")
+console.log(h1)
+
+let colorh1 = "red"
+
+function color(elem){
+  if(colorh1 === "red"){
+    elem.classList.add("h1green")
+    colorh1 = "green"
+  } else if (colorh1 === "green"){
+    elem.classList.remove("h1green")
+    elem.classList.add("h1blue")
+    colorh1 = "blue"
+  } else {
+    elem.classList.remove("h1blue")
+    colorh1 = "red"
+  }
+}
+
+setInterval(()=>{
+  color(h1) }, 1000)
+
+
