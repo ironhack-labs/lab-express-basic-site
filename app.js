3729d12104a28d6d00689448756baf4bebe2f7c9
newericg <eric.gomes0105@gmail.com>
2019-09-25 13:36:34 -0300

    done

---
+++ app.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-path-concat */
+/* eslint-disable prefer-template */
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/home-page.html');
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/gallery', (req, res) => {
+  res.sendFile(__dirname + '/views/gallery.html');
+});
+
+app.listen(3000, () => {
+
+});
