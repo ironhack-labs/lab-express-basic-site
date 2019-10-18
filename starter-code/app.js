c680a5ea12443eddb9b3a75dd9d5f46b15474df5
marciano <mcnvrodrigues@gmail.com>
2019-10-17 22:25:42 -0300

    version 1.0

---
+++ starter-code/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => {
+    const templatePath = __dirname + '/views/home.html';
+    console.log(templatePath);
+    response.sendFile(templatePath);
+});
+
+app.get('/about', (request, response) => {
+    const templatePath = __dirname + '/views/about.html';
+    console.log(templatePath);
+    response.sendFile(templatePath);
+});
+
+app.get('/gallery', (request, response) => {
+    const templatePath = __dirname + '/views/gallery.html';
+    console.log(templatePath);
+    response.sendFile(templatePath);
+});
+
+app.listen(3000, () => {
+    console.log('App is listening on port 3000');
+})
