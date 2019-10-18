8e196830b96df52d429e060dd7beb752cfc010a1
Jeferson Silva <jeferson.s.silva1@gmail.com>
2019-10-17 21:29:51 -0300

    Criado estrutura de pastas

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const app = express();
 
+app.get('/', (request, response, next) => {
+    response.send('/');
+});
+
+app.get('/home', (request, response, next) => {
+    response.send('Home');
+});
+
+app.get('/about', (request, response, next) => {
+    response.send('About');
+});
+
+app.get('/photo', (request, response, next) => {
+    response.send('Photo');
+});
+
+app.listen(3000);
