cf944a16c4aaad253dec2fadb138a66691d69b60
Flor Urbinati <flor29600@hotmail.com>
2019-11-12 15:54:49 +0100

    basic structure

---
+++ starter-code/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+app.use(express.static('public'))
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + 'views/home.html')
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + 'views/about.html')
+})
+
+app.get('/gallery', (req, res) => {
+    res.sendFile(__dirname + 'views/gallery.html')
+})
+
+app.listen(PORT)
