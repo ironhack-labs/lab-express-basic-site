385d23809e87e5e2631e29283fdb28c2b26608af
Victor <victor@victor.com>
2019-11-13 09:17:36 +0100

    done

---
+++ starter-code/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', (req, res)=> res.sendFile(__dirname+'/views/home.html'))
+app.get('/about', (req, res)=> res.sendFile(__dirname +'/views/about.html'))
+app.get('/photoGallery', (req,res)=> res.sendFile(__dirname +'/views/photoGallery.html'))
+
+app.listen(3000, ()=> console.log('app escuchando'))
