5f3dbff1fba0dfd4579484707c618913dbd45670
Boris Vogt <boris.vogt@posteo.de>
2019-11-13 20:23:15 +0100

    File structured, basic html added, server added index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+var morgan = require('morgan');
+
+const logger = morgan('tiny');
+
+const app = express();
+
+app.use(logger);
+app.use(express.static('public'));
+
+
+//ROUTING
+app.get( '/', ( require, response, next) => {
+    response.send( '<h1>WELCOME!</h1>' );
+});
+
+app.get( '/home', ( require, response, next) => {
+    response.sendFile(__dirname + '/public/views/index.html');
+})
+
+app.get( '/about', (require, response, next ) => {
+    response.sendFile(__dirname + '/public/views/about.html');
+} )
+
+app.get('/photo-galery', ( require, response, next ) => {
+    response.sendFile(__dirname + '/public/views/photo-gallery.html');
+})
+
+
+//START SERVER
+app.listen(3000,  () => {
+    console.log('SERVER RUNNING');
+});
