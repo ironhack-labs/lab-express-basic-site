ddfe4ab554183edef8a0228ac61121b235653334
Rose Beazzy <rose.beatty@gmail.com>
2019-11-13 17:44:59 +0100

    my app

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const morgan = require('morgan')
+const app = express();
+const logger = morgan('tiny')
+
+app.use(logger);
+app.use( express.static('public') );
+
+app.use((req, res, next) => {
+    console.log('CUSTOM MIDDLEWARE -> VACA');
+
+    next();
+});
+
+app.get('/', (req, res, next) => {
+    res.send('<h1>Welcome Ironhacker!</h1>')
+})
+app.get('/home', (req, res, next) => {
+    res.sendFile( __dirname + '/public/views/index.html');
+})
+app.get('/about', (req, res, next) => {
+    res.sendFile( __dirname + '/public/views/about.html');
+})
+app.get('/gallery', (req, res, next) => {
+    res.sendFile( __dirname + '/public/views/gallery.html');
+})
+
+
+app.listen(3000, () => {
+    console.log("server running!")
+});
