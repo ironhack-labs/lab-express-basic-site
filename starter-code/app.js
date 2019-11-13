906467629c25a16828405f620682b54e65c1dd1b
EChColorado <edxz7c@gmail.com>
2019-11-13 01:39:41 -0600

    Done

---
+++ starter-code/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+// config express
+app.use(express.static(__dirname + "/public"));
+
+// rutas
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/gallery", (req, res) => {
+    res.sendFile(__dirname + "/views/gallery.html")
+})
+
+app.listen(3000, () => {
+  console.log(`Running on http://localhost:3000`);
+});
