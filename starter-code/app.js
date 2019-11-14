f1e90b74a029ccef1cdd8962dcada2c577ae6761
Fred011 <54903136+Fred011@users.noreply.github.com>
2019-11-14 09:53:53 +0100

    [BCN-WDFT] - Frederic V

---
+++ starter-code/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const app = express();
+
+
+//MIDDLEWARE
+app.use(express.static('public'));
+app.use((request, response, next) => {
+    next();
+})
+
+app.get('/', (request, response, next) => {
+
+    response.send('<h1>Welcome Ironhacker !!</h1>');
+});
+
+app.get('/home', (request, response, next) => {
+
+    response.sendFile(__dirname + '/public/views/home.html')
+})
+
+app.get('/about', (request, response, next) => {
+
+    response.sendFile(__dirname + '/public/views/about.html')
+})
+
+app.get('/gallery', (request, response, next) => {
+
+    response.sendFile(__dirname + '/public/views/gallery.html')
+})
+
+// SERVER STARTED
+app.listen(3000, () => {
+    console.log('My first app listening on port 3000!');
+   });
