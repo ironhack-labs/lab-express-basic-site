7695fcbadb553a597da8f0d1d455839786256706
jesus-flores <chucho_quidproquo@hotmail.com>
2019-11-24 23:12:05 -0600

    Realice las correcciones

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,27 +1,21 @@
-// Require Express
 const express = require("express");
 
-// Express server handling requests and responses
-const app = express();
+const app = express()
 
-app.use(express.static('public'))
-// our first Route:
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+    res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(__dirname + "/views/about.html");
+    res.sendFile(__dirname + "/views/about.html");
 });
 
-app.get("/photo", (req, res) => {
-  res.sendFile(__dirname + "/views/photo.html");
+app.get("/gallery", (req, res) => {
+    res.sendFile(__dirname + "/views/gallery.html");
 });
 
-// Server Started
 app.listen(3000, () => {
-  console.log("My first app listening on port 3000!");
+    console.log(`Running on http://localhost:3000`);
 });
-
-//iniciar nodemon
