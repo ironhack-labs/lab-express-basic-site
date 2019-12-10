50f03788ff4f543b160f038cef18e22720e89395
Strapak, Sergej <strapak@mediamarktsaturn.com>
2019-12-10 17:26:21 +0100

    I had 0 ideas for a webpage, so grabbed some existing from a training before. Sorry

---
+++ starter-code/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('./public'));
+
+app.get('/home', (request, response) => {
+    response.sendFile(__dirname + '/views/home.html');
+})
+
+app.get('/about', (request, response) => {
+    response.sendFile(__dirname + '/views/about.html');
+})
+
+app.get('/gallery', (request, response) => {
+    response.sendFile(__dirname + '/views/gallery.html');
+})
+
+
+app.listen(3000, () => {
+    console.log('Server up on port 3000')
+})
