3d4b49688e9429c125efa6e8be9ad7d3bf415a55
Gooss <gooss@media-saturn.com>
2019-12-10 17:32:18 +0100

    structure copied for general layout. Spent to much time in the kitchen talking, sorry

---
+++ starter-code/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/home', (request, response) => {
+    response.sendFile(__dirname + '/views/homepage.html')
+});
+
+app.get('/about', (request, response) => {
+    response.sendFile(__dirname + '/views/about.html')
+});
+
+app.get('/gallery', (request, response) => {
+    response.sendFile(__dirname + '/views/gallery.html')
+});
+
+
+app.listen(3000, () => {
+    console.log('server listening on port 3000');
+});
