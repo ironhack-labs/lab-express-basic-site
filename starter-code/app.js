53771ee0394bc5e2a453405e1d725a4ba92db585
Ardash <56517452+ardache@users.noreply.github.com>
2019-12-12 21:15:58 -0600

    correccion de paginas

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,16 +12,16 @@ app.set('views', __dirname + '/views');
 
 
 app.get('/', (request, response, next) => {
-    response.sendFile(__dirname + '/views/cat-page.html');
+    response.sendFile(__dirname + '/views/index.html');
   });
 
 
 app.get('/about', (request, response, next) => {
-    response.sendFile(__dirname + '/views/cat-page.html');
+    response.sendFile(__dirname + '/views/about.html');
 });
 
 app.get('/gallery', (request, response, next) => {
-    response.sendFile(__dirname + '/views/cat-page.html');
+    response.sendFile(__dirname + '/views/gallery.html');
 });
 
 
