233b3cf4409171b2bd3d9df676005a0b990b35b3
Gustavo <gustavocruzb@outlook.com>
2019-12-12 23:45:57 -0600

    half-way

---
+++ starter-code/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/',(request, response, next) => {
+    response.sendFile(__dirname + '/views/home-page.html')
+});
+
+app.get('/about',(request, response, next) => {
+    response.sendFile(__dirname + '/views/about-page.html')
+});
+
+app.get('/gallery',(request, response, next) => {
+    response.sendFile(__dirname + '/views/gallery-page.html')
+});
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000!');
+  });
+
