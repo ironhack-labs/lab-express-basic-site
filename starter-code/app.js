a38414c483a909c6fb607afbe5d02c455812c138
Analin Flores <foglzerika@gmail.com>
2019-12-14 06:41:17 -0600

    Adding about page

---
+++ starter-code/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+app.use(express.static('public'));
+
+app.get('/',(req,res) =>{
+    res.sendFile(__dirname + "/views/index.html");
+})
+app.get('/about' , (req,res)=>{
+    res.sendFile(__dirname + "views/about");
+})
+
+//Start server
+app.listen(3000 , ()=>{
+    console.log(`Running coldplay tribute fan page`);
+})
