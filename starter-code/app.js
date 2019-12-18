9a5abdf97b34e2b4c92181cc07d4a30210e50170
Maxwell Martins Lima Monteiro <maxwba@AbelardosiPhone.localdomain>
2019-12-18 12:21:45 -0300

    Complete

---
+++ starter-code/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+app.get('/about', (req,res) => {
+    res.sendFile(__dirname + '/views/about.html')
+})
+
+app.get('/photos', (req,res) => {
+    res.sendFile(__dirname + '/views/photos.html')
+})
+
+
+app.listen(3000)
+
