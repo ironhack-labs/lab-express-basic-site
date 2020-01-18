c7c057bb1c5290fc318dc0e99ab78e84db204ca0
Ruben Vaquero <rvaquero@elpublicamaleon.com>
2020-01-18 16:50:50 +0100

    install boostrap and configure order archives

--- starter-code/app.js
+++ starter-code/app.js
@@ -7,15 +7,15 @@ app.set("view engine", "hbs");
 app.use("static", express.static(__dirname + "/static"));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {tituloPag:'HOME'});
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", {tituloPag:'ABOUT'});
 });
 
 app.get("/gallery", (req, res) => {
-  res.render("gallery");
+  res.render("gallery", {tituloPag:'GALLERY'});
 });
 
 const port = 3000;
