7ef3759cdaf76496810583e8775a3c179b36b243
Ivan Esteban <ivan.estebanisabel@gmail.com>
2020-01-18 15:05:00 +0100

    start server

---
+++ starter-code/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const hbs = require("hbs");
+const app = express();
+
+const port = 3000;
+app.listen(port, () => console.log(`Ready on port ${port}`));
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
