c752e7b1302df076fea6332bdaedda63feba9537
carballo <carballet@gmail.com>
2020-01-18 16:04:26 +0100

    Páginas creadas a falta de rellenar el Body

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,5 +2,22 @@ const express = require("express");
 const hbs = require("hbs");
 const app = express();
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+
+app.use("/static", express.static(__dirname + "/public"));
+
+app.get("/", (request, response) => {
+  response.render("home");
+});
+
+app.get("/about", (request, response) => {
+  response.render("about");
+});
+
+app.get("/gallery", (request, response) => {
+  response.render("gallery");
+});
+
 const port = 3000;
 app.listen(port, () => console.log(`Ready on port ${port}`));
