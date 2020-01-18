c18f0952dd1c439de90b10e664b82e1e6ee04862
Kevin Medina <kmedinakm@hotmail.com>
2020-01-17 21:04:13 -0500

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,7 +1,8 @@
 
-
 const express = require('express');
 const app = express();
+
+require('dotenv').config();
 
 app.use(express.static('public'));
 
@@ -17,4 +18,4 @@ app.get('/gallery', (req, res) => {
     res.sendFile(__dirname + '/views/gallery.html');
 });
 
-app.listen(3000, () => console.log('You are runing on port: 3000'));
+app.listen(process.env.PORT, () => console.log(`Running on port: ${process.env.PORT}`));
