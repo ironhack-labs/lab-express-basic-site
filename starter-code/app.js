a32256b0f458db78c178deb5f9a37759fe191aca
Gonzalo Hernández <gonzalohernandez@icloud.com>
2020-01-19 21:53:43 +0100

    basic site finished

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,13 +1,19 @@
+const players = require("./players.js");
+
 const express = require("express");
+const hbs = require("hbs");
 const app = express();
 
 app.use(express.static("public"));
 
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
+hbs.registerPartials(__dirname + "/views/partials");
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/about", (req, res) => res.render("about"));
-app.get("/images", (req, res) => res.render("images"));
+app.get("/", (req, res) => res.render("home", { homeNav: true }));
+app.get("/about", (req, res) => res.render("about", { aboutNav: true }));
+app.get("/players", (req, res) =>
+  res.render("players", { players, playersNav: true })
+);
 
-app.listen(3000, console.log("Listening"));
+app.listen(3000);
---
+++ starter-code/players.js
@@ -0,0 +1,46 @@
+const players = [
+  {
+    name: "Zinedine Zidane",
+    text:
+      "No explotó su pontencial como cabezeador hasta el final de su carrera.",
+    fileName: "zidane.jpg"
+  },
+  {
+    name: "Loris Karius",
+    text: "Los madridistas siempre le recordarán como un héroe.",
+    fileName: "karius.jpg"
+  },
+  {
+    name: "Éric Cantona",
+    text: "Era un jugador de contacto.",
+    fileName: "cantona.jpg"
+  },
+  {
+    name: "Míchel González",
+    text: "Tenía un toque exquisito.",
+    fileName: "michel.jpg"
+  },
+  {
+    name: "Diego Armando Maradona",
+    text:
+      "Hizo un anuncio advirtiendo sobre los peligros de las drogas. ¿Quién mejor que él?",
+    fileName: "maradona.jpg"
+  },
+  {
+    name: "Robbie Fowler",
+    text: "Su ídolo era claramente Maradona.",
+    fileName: "fowler.jpg"
+  },
+  {
+    name: "Lionel Messi",
+    text: "Con los brazos no es tan habilidoso.",
+    fileName: "messi.jpg"
+  },
+  {
+    name: "George Best",
+    text: "Su apellido lo describe perfectamente.",
+    fileName: "best.jpg"
+  }
+];
+
+module.exports = players;
