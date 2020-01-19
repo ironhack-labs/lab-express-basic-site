1a8d297b0996b9f3c1e87c6eceb388929e002c26
mario010203 <mariojose1490@hotmail.com>
2020-01-19 16:02:51 +0100

    setup of static files

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,11 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+app.use("/static", express.static("static"));
 
-
-app.get('/', (request, response) => {
-    response.send('first server of me');
-  });
+app.get("/", (request, response) => {
+    response.send(`
+    <!doctype html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Cat</title>
+        <link rel="stylesheet" href="/static/style.css" />
+      </head>
+      <body>
+        <h1>Cat</h1>
+        <p>This is my second route</p>
+        <img src="/images/cool-cat.jpg" />
+      </body>
+    </html>
+  `);
+});
 
 const port = 3000;
 app.listen(port, () => console.log(`Ready express on port ${port}`));
