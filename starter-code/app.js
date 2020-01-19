550281280cf299df049bf1316bd6ea6830792f0c
Ana Segarra <anasegarraber@gmail.com>
2020-01-19 01:20:20 +0100

    Add about route

--- starter-code/app.js
+++ starter-code/app.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
 	res.render('index');
 });
 
+app.get('/about', (req, res) => {
+	res.render('about');
+});
+
 // Start server
 const port = 3000;
 app.listen(port, () => {
