e3050fa1d488ed6b0e55fcc29d102ac14ffa0836
mario010203 <mariojose1490@hotmail.com>
2020-01-19 19:33:30 +0100

    hbs and bootstrap implemented

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,25 +1,22 @@
 const express = require("express");
+const hbs = require("hbs");
 const app = express();
 
 app.use("/public", express.static("public"));
 
-app.get("/", (request, response) => {
-    response.send(`
-    <!doctype html>
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <title>Cat</title>
-        <link rel="stylesheet" href="/public/style.css" />
-      </head>
-      <body>
-        <h1>Cat</h1>
-        <p>This is my second route</p>
-        <img src="/images/cool-cat.jpg" />
-      </body>
-    </html>
-  `);
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("home");
 });
+
+app.get("/about", (req, res) => {
+    res.render("about");
+  });
+  app.get("/photo-gallery", (req, res) => {
+    res.render("photo-gallery");
+  });
 
 const port = 3000;
 app.listen(port, () => console.log(`Ready express on port ${port}`));
