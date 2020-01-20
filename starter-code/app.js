88d2bc650d182110d64fa07674a10d8bde6c6960
Peacock-Co <pavaoleonardo@hotmail.com>
2020-01-20 17:06:07 +0100

    Added files to the views files

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,25 +1,21 @@
 const express = require('express');
-const hbs = require('hbs');
+
 const app = express();
-const path = require('path')
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.render('home');
+app.get('/home', (request, response, next) => {
+  response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/about', (req, res) => {
-  res.render('about', data);
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about.html');
 });
 
-app.get('/photo gallery', (req, res) => {
-   res.render('photo gallery');
-  });
+app.get('/photo_gallery', (request, response, next) => {
+  response.sendFile(__dirname + '/views/photo_gallery.html');
+});
 
-app.listen(3000, '127.0.0.1', () => {
-  console.log('Listening to the port 3000')
+app.listen(3000, () => {
+  console.log('Listening on port 3000!');
 });
