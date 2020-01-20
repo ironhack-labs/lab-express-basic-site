a807f4e12e53d8935f3434f950b2ce0b97baf4da
Ana Segarra <anasegarraber@gmail.com>
2020-01-20 13:51:06 +0100

    Create new routes and register partial

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,11 +1,43 @@
+// import modules
 const express = require('express');
 const hbs = require('hbs');
+
+// global variables
 const app = express();
+const people = [
+	{
+		name: 'Ellie',
+		age: 14,
+		status: 'alive',
+		fictional: true,
+		img: 'ellie.jpeg'
+	},
+	{
+		name: 'Joel',
+		age: 'uknown',
+		status: 'alive',
+		fictional: true,
+		img: 'joel.jpg'
+	},
+	{
+		name: 'Ashely Johnson',
+		age: 36,
+		appearances: [ 'Blindstop', 'Critical Role', 'The Last of Us: Left Behind' ],
+		fictional: false
+	},
+	{
+		name: 'Troy Baker',
+		age: 43,
+		appearances: [ 'BioShock Infinite', "Uncharted 4: A Thief's End", 'Death Stranding' ],
+		fictional: false
+	}
+];
 
 // App setup
 app.set('view engine', 'hbs');
 app.set('views', `${__dirname}/views`);
 app.use('/static', express.static(`${__dirname}/public`));
+hbs.registerPartials(`${__dirname}/views/partials`);
 
 // Routes
 app.get('/', (req, res) => {
@@ -18,6 +50,14 @@ app.get('/about', (req, res) => {
 
 app.get('/gallery', (req, res) => {
 	res.render('gallery');
+});
+
+app.get('/cast', (req, res) => {
+	res.render('cast', { people: people.filter(person => !person.fictional) });
+});
+
+app.get('/characters', (req, res) => {
+	res.render('characters', { people: people.filter(person => person.fictional) });
 });
 
 // Start server
