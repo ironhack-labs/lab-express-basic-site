358df1ef8659315c3449d5c0c3f332ea3a2de7b6
vctorChamizo <victor.chamizo96@gmail.com>
2020-01-20 14:40:32 +0100

    Add paths of website

--- starter-code/app.js
+++ starter-code/app.js
@@ -9,7 +9,15 @@ app.set("view engine", "hbs");
 app.use("/static", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.send("Hola mundo!");
+  res.send("HOME");
+});
+
+app.get("/about", (req, res) => {
+  res.send("ABOUT");
+});
+
+app.get("/photo-gallery", (req, res) => {
+  res.send("PHOTO-GALLERY");
 });
 
 const port = 5555;
