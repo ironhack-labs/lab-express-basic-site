a02dcc2b7449aa3bdd61ba18eb8f304cbbc23f97
Gustavo Jordao <gustavo.jordao021@gmail.com>
2020-01-20 13:21:11 -0500

    Finish 'Home' and 'About' sections

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home-page.html');
+    console.log('Request successful!');
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/views/about.html');
+    console.log('Request successful!');
+})
+
+app.get('/gallery', (req, res) => {
+    res.sendFile(__dirname + '/views/gallery.html');
+    console.log('Request successful!');
+})
+
+app.listen(3000, () => console.log('Server now open on port 3000! 💻'));
