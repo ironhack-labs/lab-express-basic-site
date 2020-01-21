87acaed7e840be80f5aa1ad763c1561f55fa63cd
Gustavo Jordao <gustavo.jordao021@gmail.com>
2020-01-21 11:17:43 -0500

    Done

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const app = express();
 
 app.use(express.static('public'));
 
-
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/home-page.html');
     console.log('Request successful!');
