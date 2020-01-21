a6e26a0d7586c9a224b661c3051f8b5b8c2ab8f6
David Garcia <dgarciabravo13@gmail.com>
2020-01-21 08:55:45 +0100

    working in about page

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,33 +4,60 @@ const app = express();
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
 
 app.use("/static", express.static(__dirname + "/static"));
 
 const dogs = [
-  { name: "Kenshi", edad: 11, campeon: "yes" },
-  { name: "Kazan", edad: 10 },
-  { name: "Kuro", edad: 10 },
-  { name: "Nami", edad: 6 },
-  { name: "Kun", edad: 8 }
+  {
+    image: "/static/images/kenshi.jpg",
+    name: "Kenshi",
+    age: 11,
+    champion: "campeon",
+    owner: "Silvia Ordas"
+  },
+  {
+    image: "/static/images/kazan.jpg",
+    name: "Kazan",
+    age: 10,
+    owner: "David Garcia"
+  },
+  {
+    image: "/static/images/nami.jpg",
+    name: "Nami",
+    age: 6,
+    champion: "campeon",
+    owner: "Nerea Alarcon"
+  },
+  {
+    image: "/static/images/kun.jpg",
+    name: "Kun",
+    age: 8,
+    owner: "Sara Dacal"
+  }
 ];
 
 app.get("/", (req, res) => {
-  res.render("index", { dogs });
+  res.render("index", { dogs, Document: "INDEX", headers: "Administradores" });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", { Document: "ABOUT" });
+  res.render("about", { Document: "ABOUT", headers: "Acerca del Shiba Inu" });
 });
 
 app.get("/galery", (req, res) => {
-  res.render("galery", { Document: "GALERY" });
+  res.render("galery", {
+    dogs,
+    Document: "GALERY",
+    headers: "Galeria de Imagenes"
+  });
 });
 
 app.get("/champion", (req, res) => {
   res.render("champion", {
+    dogs: dogs.filter(e => e.champion),
     Document: "CHAMPIONS",
-    dogs: dogs.filter(e => e.campeon == "yes")
+    headers: "Campeones de España"
   });
 });
 
