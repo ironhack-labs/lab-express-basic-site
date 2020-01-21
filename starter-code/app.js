8477a78794e99eb5c16ea109be4acb78b94ae8ce
Román Méndez <mendezbeck@gmail.com>
2020-01-21 18:15:08 +0100

    active indicator on navBar

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,20 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
 
 app.use("/static", express.static("public"));
+app.use((req, res, next) => {
+  res.locals = {
+    home: false,
+    about: false,
+    photos: false
+  };
+  next();
+});
 
 app.get("/", (req, res) => {
-  res.render("home")
-})
+  res.render("home", { home: true });
+});
 app.get("/about", (req, res) => {
-  res.render("about")
-})
+  res.render("about", { about: true });
+});
 app.get("/photos", (req, res) => {
-  res.render("photos")
-})
+  res.render("photos", { photos: true });
+});
 
 const port = 3000;
-app.listen(port, () => console.log(`Ready express on port ${port}`))
+app.listen(port, () => console.log(`Ready express on port ${port}`));
