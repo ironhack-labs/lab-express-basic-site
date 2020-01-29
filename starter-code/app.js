65b539857bd7a29c3bb1f19daea0c26eee387410
arturo <arturo.araujo.alvarez@gmail.com>
2020-01-29 02:13:28 -0600

    done

---
+++ starter-code/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  console.log(__dirname);
+  res.sendFile(`${__dirname}/views/home-page.html`);
+});
+
+app.get("/about", (req, res) => {
+  console.log(__dirname);
+  res.sendFile(`${__dirname}/views/about.html`);
+});
+
+app.get("/gallery", (req, res) => {
+  console.log(__dirname);
+  res.sendFile(`${__dirname}/views/gallery.html`);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`server listening on http://localhost:${PORT}`);
+});
