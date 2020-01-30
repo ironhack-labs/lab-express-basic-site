fd3e80d57f81ae7a38d6e509bd31a24f3bedab60
Yannick Bourenane <yannick.bourenane@gmail.com>
2020-01-30 20:07:52 +0100

    [WDFT-PAR0120] Yannick Bourenane

---
+++ starter-code/app.js
@@ -0,0 +1,44 @@
+const dataGifs = require('./bin/data');
+const express = require("express");
+const server = express();
+const PORT = 1111;
+const hbs = require("hbs");
+const path = require("path");
+
+// Make everything inside of public/ available to the browser (styles, images, frontend scripts)
+server.use(express.static(path.join(__dirname, "public"))); // rock solid syntax
+
+// indicates express where our "views" templates are located
+// view is a term part of MVC design pattern... more coming. in a nutshell, a view is a page ; )
+server.set("views", path.join(__dirname, "views"));
+
+// indicates express wich view engine this app will use ; )
+server.set("view engine", "hbs"); // in this case hbs, there are others ... pug/jade etc.engine !
+
+hbs.registerPartials(path.join(__dirname, "views/partials"));
+
+//hbs.registerPartial("titleLogo", "{{titleLogo}}");
+// what is a view engine ???
+// basicaly a way to code HTML++ (loops, conditionals)
+
+server.get("/", (req, res) => {
+  const data = {
+    text: "some text",
+    loveCoding: false,
+    siteOwner: "Yannick Bourenane"
+  };
+  res.render("home", data);
+});
+
+server.get("/about", (req, res) => {
+  const data = {
+    html: "<h1>About</h1><p>This is about page</p>"
+  };
+  res.render("about", data);
+});
+server.get("/gifs", (req, res) => {
+  res.render("gifs", dataGifs);
+});
+server.listen(PORT, () => {
+  //console.log(`Yata ! server is running @ http://localhost:${PORT}`);
+});
---
+++ starter-code/bin/data.js
@@ -0,0 +1,62 @@
+let dataGifs = {
+    js: ["gifs"],
+    gifs: [{
+            link: "https://media.giphy.com/media/LmNwrBhejkK9EFP504/giphy.gif",
+            keywords: ["code", "master", "focus", "tryhard", "zone", "cat"]
+        },
+        {
+            link: "https://media.giphy.com/media/3oKIPnAiaMCws8nOsE/giphy.gif",
+            keywords: ["code", "lost", "noob", "cat"]
+        },
+        {
+            link: "https://media.giphy.com/media/zOvBKUUEERdNm/source.gif",
+            keywords: ["code", "monkey", "wtf", "help", "angry"]
+        },
+        {
+            link: "https://media.giphy.com/media/dlMIwDQAxXn1K/source.gif",
+            keywords: ["code", "angry", "mad", "furious"]
+        },
+        {
+            link: "https://media.giphy.com/media/vzO0Vc8b2VBLi/source.gif",
+            keywords: ["code", "dog", "tired", "chill"]
+        },
+        {
+            link: "https://media.giphy.com/media/ZIH0Slo6J6PQY/giphy.gif",
+            keywords: ["code", "angry", "mad", "furious"]
+        },
+        {
+            link: "https://media.giphy.com/media/12BYUePgtn7sis/giphy.gif",
+            keywords: ["code", "yay", "happy", "dancing", "dance"]
+        },
+        {
+            link: "https://media.giphy.com/media/gG6OcTSRWaSis/source.gif",
+            keywords: ["code", "bug", "unexpected"]
+        },
+        {
+            link: "https://media.giphy.com/media/wh9Ftb014GmKQ/source.gif",
+            keywords: ["code", "css", "why", "help"]
+        },
+        {
+            link: "https://media.giphy.com/media/Oq0tvN9qoXVgk/giphy.gif",
+            keywords: ["code", "test", "client"]
+        },
+        {
+            link: "https://media.giphy.com/media/akFPzLYDHTEUU/source.gif",
+            keywords: ["code", "what", "help", "lost"]
+        },
+        {
+            link: "https://media.giphy.com/media/WbEqInnv5XvQk/source.gif",
+            keywords: ["code", "wrong", "error", "bad"]
+        },
+        {
+            link: "https://media.giphy.com/media/11fDMHAzihB8D6/source.gif",
+            keywords: ["code", "reproduce", "bug", "tesr"]
+        },
+        {
+            link: "https://media.giphy.com/media/ta5eOL0Wvhh4s/source.gif",
+            keywords: ["code", "mad", "trash", "furious"]
+        }
+    ]
+}
+
+module.exports = dataGifs;
