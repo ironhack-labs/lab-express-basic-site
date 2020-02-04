c9043a1711b45bab7ba340ce07009cf7548550ad
Juan Antonio Martin <juanantoniomartinb@gmail.com>
2020-02-04 19:13:46 +0100

    Enjoy

---
+++ starter-code/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.sendFile(`${__dirname}/views/home.html`))
+app.get('/about', (req, res) => res.sendFile(`${__dirname}/views/about.html`))
+app.get('/photos', (req, res) => res.sendFile(`${__dirname}/views/photo-gallery.html`))
+
+app.listen(3000, () => console.log("Servidor levantado..."))
