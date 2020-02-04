c01ce7c12bfc6a3b4fc94d42f4a01f2d1b822197
Juan Carlos <jcarlosriego@gmail.com>
2020-02-04 18:54:49 +0100

    Done

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,3 +4,18 @@ const PORT = 3000;
 
 app.use(express.static("public"));
 
+app.get("/home", (req, res) => {
+    res.sendFile(__dirname + "/public/views/home.html");
+  });
+app.get("/about", (req, res) => {
+res.sendFile(__dirname + "/public/views/about.html");
+});
+
+app.get("/gallery", (req, res) => {
+res.sendFile(__dirname + "/public/views/gallery.html");
+});
+
+app.listen(PORT, () => {
+
+console.log(`listening on port ${PORT}`);
+});
