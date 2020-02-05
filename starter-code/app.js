3e00b6837998d9966c447831d4771ed88df93666
Pauline Morsli <pauline.morsli@gmail.com>
2020-02-05 16:28:14 +0100

    i love cats

---
+++ starter-code/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const hbs     = require('hbs');
+const app     = express();
+const path    = require('path')
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res, next) => {
+  res.render('about');
+});
+
+app.get('/contact', (req, res, next) => {
+  res.render('contact');
+});
+
+app.listen(3000);
