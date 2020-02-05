a908f3d3af6745ede987cc9378e7af4d029d6f85
CrisSugar <aguacero18@hotmail.com>
2020-02-05 09:28:25 +0100

    done

---
+++ starter-code/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+app.use(express.static('public'));
+
+app.get('/', (request, response, next) => {
+
+    response.sendFile(__dirname + '/views/home.html');
+
+  });
+
+
+app.get('/about', (request, response, next) => {
+
+    response.sendFile(__dirname + '/views/about.html');
+
+  });
+
+
+  app.get('/gallery', (request, response, next) => {
+
+    response.sendFile(__dirname + '/views/gallery.html');
+
+  });
+
+  app.listen(4000, () => {
+
+  });
