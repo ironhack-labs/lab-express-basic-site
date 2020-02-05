e8bc78894ef4e51e41db1a8e90818643e5271be4
Arnoldo Sicilia <arnoldo.sicilia.labarquilla@gmail.com>
2020-02-05 01:39:10 +0100

    Ejercicio Terminado

--- starter-code/app.js
+++ starter-code/app.js
@@ -10,7 +10,7 @@ app.use(express.static('public'))
 //creamos los enpoints de ls paginas que vamos a utilizar 
 app.get('/', (req, res) => res.sendFile(`${__dirname}/views/index.html`))
 app.get('/about', (req, res) => res.sendFile(`${__dirname}/views/about.html`))
-app.get('/photo', (req, res) => res.sendFile(`${__dirname}/views/photo-gallery.html`))
+app.get('/memes', (req, res) => res.sendFile(`${__dirname}/views/memes.html`))
 
 //levantar el servidor 
 
