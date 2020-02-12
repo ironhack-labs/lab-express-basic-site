3c64c5cec378d504f47dd799bb1c4bd6904c807d
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-02-11 23:26:46 -0500

    Home page complete

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,1 +1,23 @@
-//Finish MongoDB lab and Express lab before starting IronBeers
+// const express = require('express');
+// const app = express();
+
+// // our first Route
+// app.get('/home', (request, response, next) => {
+//   console.log(request);
+//   response.send('<h1>Welcome Ironhacker. :)</h1>')
+// });
+
+// Require Express
+const express = require('express');
+
+// Express server handling requests and responses
+const app = express();
+
+// Make everything inside of public/ available
+app.use(express.static('public'));
+
+// our first Route:
+app.get('/home', (request, response, next) => response.send('<h1>Welcome Ironhacker. :)</h1>'));
+
+// Server Started
+app.listen(3000, () => console.log('My first app listening on port 3000! '));
