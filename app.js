acf1e00c307ced75a3cb71cdc1115d5d45f3895e
Pablolo <pablo_olondriz@hotmail.com>
2020-02-14 15:12:28 +0100

    writing the app

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const express = require('express');
 
 const app = express();
+
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => res.render(`${__dirname}/views/index`));
+app.get('/projects', (req, res) => res.render(`${__dirname}/views/project-detail`));
+app.get('/contact', (req, res) => res.render(`${__dirname}/views/contact`));
+
+app.listen(5000);
