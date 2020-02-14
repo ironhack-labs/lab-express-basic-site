dc06d5e63509ba0481d0938e9b642e3b418e1731
Marc Serra <mserrahidalgo@gmail.com>
2020-02-14 18:47:19 +0100

    with portfolio code

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const port = 3000;
 app.use(express.static("public"));
 app.set("view engine", "hbs");
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/about", (req, res) => res.render("about"));
-app.get("/works", (req, res) => res.render("works"));
+app.get("/", (req, res) => res.render("index"));
+app.get("/educacion", (req, res) => res.render("educacion"));
+app.get("/experiencia", (req, res) => res.render("experiencia"));
+app.get("/proyectos", (req, res) => res.render("proyectos"));
+app.get("/contacto", (req, res) => res.render("contacto"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Listening on port ${port}!`));
