3e9409af3c821073c7fb13dd84b10fe03733b5df
Jaime <jaimemoravallejo@gmail.com>
2020-02-14 19:35:46 +0100

    done portfolio with hbs

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
-app.set("view engine", "hbs");
-app.set("views", __dirname + "/views");
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+const information = {
+  firstname: 'Jaime', 
+  lastname: 'Mora',
+  title: {portfolio: 'Portfolio', contact: 'Contact', aboutme: 'About me', projects: 'Projects' },
+  tech: {html: 'HTML5', css: 'CSS 3', js: 'Javascript'},
+  project: {revera: 'Revera', slack: 'Slack', rover: 'Rover', npm: 'NPM'}
+}
 
 
-app.get("/", (req, res, next) => res.render("index"));
-app.get("/contact", (req, res, next) => res.render("contact"));
-app.get("/about-me", (req, res, next) => res.render("about-me"));
-app.get("/projects", (req, res, next) => res.render("projects"));
+app.get('/', (req, res, next) => res.render('index', information));
+app.get('/contact', (req, res, next) => res.render('contact', information));
+app.get('/about-me', (req, res, next) => res.render('about-me', information));
+app.get('/projects', (req, res, next) => res.render('projects', information));
 
 
 app.listen(3000, function() {
-  console.log("Jaime's portfolio in node.js");
+  console.log('Jaime\'s portfolio in node.js');
 });
