ccaeda083efa951a1149d4101b5781a0dd0ae09e
Pablolo <pablo_olondriz@hotmail.com>
2020-02-14 18:46:05 +0100

    module exports

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const data = require('./file');
 
 const app = express();
 
@@ -7,13 +8,7 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.render(`${__dirname}/views/index`));
-app.get('/projects', (req, res) => {
-  const data = {
-    name: 'Legis Music',
-    desc: 'Website dedicated to Royalty-Free Music',
-  };
-  res.render(`${__dirname}/views/project-detail`, data);
-});
+app.get('/projects', (req, res) => res.render(`${__dirname}/views/project-detail`, data));
 
 app.get('/contact', (req, res) => res.render(`${__dirname}/views/contact`));
 
---
+++ file.js
@@ -0,0 +1,6 @@
+const data = {
+  name: 'Legis Music',
+  desc: 'Website dedicated to Royalty-Free Music',
+};
+
+module.exports = data;
