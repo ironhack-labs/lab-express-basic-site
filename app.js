2c8988eac82eab7bf69b97cd20a548ff59d4d1f3
Pablolo <pablo_olondriz@hotmail.com>
2020-02-14 18:16:06 +0100

    making some things dynamic

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.render(`${__dirname}/views/index`));
-app.get('/projects', (req, res) => res.render(`${__dirname}/views/project-detail`));
+app.get('/projects', (req, res) => {
+  const data = {
+    name: 'Legis Music',
+    desc: 'Website dedicated to Royalty-Free Music',
+  };
+  res.render(`${__dirname}/views/project-detail`, data);
+});
+
 app.get('/contact', (req, res) => res.render(`${__dirname}/views/contact`));
 
 app.listen(5000);
