f5c7d827128c5c04e29e8d7e5af7797a44257139
Carles Galí Bou <carles_ga@hotmail.com>
2020-02-14 20:00:06 +0100

    All the 3 pages done with handlebars. Packages installed in dependencies: express and hbs. Packages installed in devDependencies: ESlint and Nodemon.

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
+
 const app = express();
+const port = 3000;
+
+// app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
 
 app.use(express.static('public'));
 
-app.get('/', (request, response) => 
-    response.sendFile(__dirname + '/views/index.html'));
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
-app.get('/projects', (request, response) => 
-    response.sendFile(__dirname + '/views/projects.html'));
+app.get('/projects', (req, res) => {
+  res.render('projects');
+});
 
-app.get('/contact', (request, response) => 
-    response.sendFile(__dirname + '/views/contact.html'));
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
 
-app.listen(3000, () => console.log('app listening on port 3000'));
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`app.js listening on port ${port}`));
