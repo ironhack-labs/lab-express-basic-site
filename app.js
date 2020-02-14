6a8f7ee156d69048f2dd58db2c4de145a3dd4f66
Pablolo <pablo_olondriz@hotmail.com>
2020-02-14 15:38:58 +0100

    fixing routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'hbs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => res.render(`${__dirname}/views/index`));
 app.get('/projects', (req, res) => res.render(`${__dirname}/views/project-detail`));
