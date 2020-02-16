e0af29bb25ef6d0cb7fcb326f4b7a07a9e5014df
Your Name <hurtadojose89@gmail.com>
2020-02-16 13:52:17 +0100

    [master] - done

--- app.js
+++ app.js
@@ -26,7 +26,69 @@ app.get('/', (req, res) => {
 });
 
 app.get('/projects', (req, res) => {
-  res.render('projects', { title: 'Projectes', showContact: true });
+  const projects = [
+    {
+      title: 'Arac Revenge',
+      img: 'aracrevenge.png',
+      description: "Creació d'un petit videojoc com a projecte final del primer módul de l'Ironhack\n <a href='https://jhurtado123.github.io/gameproject/' target='_blank'>Demo ( Only PC, not responsive )</a>",
+      tags: ['Html', 'DOM', 'Css', 'Javascript'],
+    },
+    {
+      title: 'Naturgy - Easy Go',
+      img: 'easygo.png',
+      description: "Conjuntament amb la resta de l'equip de treball, creació d'un aplicatiu que permet al client contractar serveis de reparació dels electrodomèstics.",
+      tags: ['Symfony', 'Html', 'Css', 'Javascript'],
+    },
+    {
+      title: 'Naturgy - Contractació',
+      img: 'col.png',
+      description: 'Mantenir i afegir noves funcionalitats al procés de contractació de Naturgy, així com solventar petits errors que es puguin ocasionar a l\'hora modificar el codi.',
+      tags: ['Yii', 'Symfony', 'Html', 'Css', 'Javascript'],
+    },
+    {
+      title: 'Naturgy - Easy Sales',
+      img: 'easysales.png',
+      description: "Conjuntament amb la resta de l'equip de treball, creació i manteniment d'un aplicatiu per gestionar les ventes i/o noves contractacions de Naturgy, així com tot un procés de contractació personalitzat.",
+      tags: ['Symfony', 'Html', 'Css', 'Javascript'],
+    },
+    {
+      title: 'Naturgy - Amazon',
+      img: 'amazon.png',
+      description: "Creació i manteniment d'una página que permet als clients de Naturgy obtenir i gaudir de descomptes per les seves compres fetes a Amazon, tot això amb un backoffice darrere per gestionar les dades.",
+      tags: ['Yii', 'Html', 'Css', 'Javascript'],
+    },
+    {
+      title: 'Back Stock',
+      img: 'takeit.png',
+      description: "Projecte final de grau, realitzat conjuntament amb un company. Consta d'una APP web que permet gestionar un negoci de restauració, des de l'enviament de comandes a cuina, seguiment de l'stock de l'establiment, fins al menú virtual, on el propi client, des del seu teléfon, és qui gestiona les seves comandes i els pagaments.",
+      tags: ['Symfony', 'Html', 'Css', 'Javascript'],
+    },
+    {
+      title: 'Mecarun',
+      img: 'mecarun.png',
+      description: "Projecte d'E-commerce enfocat a la venta de productes per a motors de vehicles. Elaboració d'una plantilla feta a mida segons els requeriments del client adaptada a Woocommerce i la configuració d'aquet.",
+      tags: ['Wordpress', 'Woocommerce'],
+    },
+    {
+      title: 'Aida Huertas',
+      img: 'aida.png',
+      description: "Projecte d'una página web a l'estil portafolis per a la consulta d'una psicologa especialista. Wordpress amb plantilla feta a mida i amb el seu propi blog.",
+      tags: ['Wordpress'],
+    },
+    {
+      title: 'MySnikers',
+      img: 'mysnikers.png',
+      description: "Projecte d'E-commerce destinat a la venda online de calçat, que requeria manteniment i la creació de noves funcionalitats.",
+      tags: ['Wordpress', 'Woocommerce'],
+    },
+    {
+      title: 'Engiser',
+      img: 'engiser.png',
+      description: "Mateniment i evolutius d'un projecte d'E-commerce destinat a la venda i subministrament de recanvis per a vehicles, que requeria manteniment i la creació de noves funcionalitats.",
+      tags: ['Wordpress', 'Woocommerce'],
+    },
+  ];
+  res.render('projects', { projects, title: 'Projectes', showContact: true });
 });
 
 app.get('/contact', (req, res) => {
