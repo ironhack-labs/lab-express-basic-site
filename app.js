0defe31e2955c8fa7a6b1a1debf4cc0e8cfa0885
Michael Mueller <michael@miniguide.co>
2020-02-16 21:55:07 +0100

    initial setup, basic home

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+app.listen(3000);
