406e825feedb2252c7fcaa810eccfd325cbebfed
Marta Mullor <Marta@MacBook-Air-de-marta.local>
2020-02-16 11:12:53 +0100

    install some npm dependencies

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+marta 
+
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+
+app.listen(3000, () => console.log('My first app listening on port 3000! '));
