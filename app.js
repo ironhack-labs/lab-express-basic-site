6b110fc59eba0bd5a259f82b524eb826528d87cc
Thibault-d <thibaultdelfaud@gmail.com>
2020-02-18 18:03:47 +0100

    image works, added more CSS. The design is shit, but I know how to do basic stuff with Node and Express :-)

--- app.js
+++ app.js
@@ -12,5 +12,7 @@ app.get('/home', (request, response, next) => response.sendFile('C:/Users/Thibau
 app.get('/about', (request, response, next) => response.sendFile('C:/Users/Thibault/Desktop/Node/lab-express-basic-site/views/about.html'));
 app.get('/works', (request, response, next) => response.sendFile('C:/Users/Thibault/Desktop/Node/lab-express-basic-site/views/works.html'));
 app.get('/gallery', (request, response, next) => response.sendFile('C:/Users/Thibault/Desktop/Node/lab-express-basic-site/views/gallery.html'));
+app.get('/', (request, response, next) => response.sendFile('C:/Users/Thibault/Desktop/Node/lab-express-basic-site/views/home-page.html'));
+
 // Server Started
-app.listen(3000, () => console.log('My first app listening on port 3000! '));
+app.listen(3000, () => console.log('Congratulations Thibault, your server is started'));
