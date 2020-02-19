958cafa31359b39d0ae34b398148fbd0cb5b0911
Michaela DeCamara <michaela.decamara@gmail.com>
2020-02-19 00:09:39 -0500

    handlebars added

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+
+app.get("/", (req, res, next) => res.render("index"));
+app.get("/about", (req, res, next) => res.render("about"));
 
 app.get('/home', (request, response, next) =>
       response.sendFile(__dirname + '/views/home.html'))
