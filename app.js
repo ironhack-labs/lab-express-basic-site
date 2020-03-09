3eb696c6807f0a3dbb6e6bf1e18cce01e6377f10
Fábio Costa <fabiodanielmc@gmail.com>
2020-03-09 17:54:55 +0100

    done

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+
+app.use(express.static('public'));
+
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+
+
+app.get("/", (req, res)=>{
+    res.render("home");
+})
+
+app.get("/about", (req, res)=>{
+    res.render("about");
+})
+
+app.get("/works", (req, res)=>{
+    res.render("works");
+})
+
+app.get("/gallery", (req, res)=>{
+    res.render("gallery");
+})
+
+app.listen(3000, ()=> {
+    console.log("listening on port", 3000)
+})
