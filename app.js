10ac29118198166dc54b4b968ef60aaae1e4321a
grountish <grountish@gmail.com>
2020-03-10 16:43:15 +0100

    done

---
+++ app.js
@@ -0,0 +1,17 @@
+let express = require('express')
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/',(request,response,next)=> {
+    response.sendFile(__dirname + '/views/home.html')
+})
+app.get('/works',(request,response,next)=> {
+    response.sendFile(__dirname + '/views/works.html')
+})
+app.get('/about',(request,response,next)=> {
+    response.sendFile(__dirname + '/views/about.html')
+})
+app.listen(3000, ()=> {
+    console.log("server running");
+})
