fef663e69095f82de6abf95f518ce4ad16f132d1
Daniel Paiva <danielcspaiva@gmail.com>
2020-03-23 22:55:15 -0300

    done

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html')
+});
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html')
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/views/about.html')
+});
+
+app.get('/works', (req, res) => {
+    res.sendFile(__dirname + '/views/works.html')
+});
+
+app.get('/photo-gallery', (req, res) => {
+    res.sendFile(__dirname + '/views/photo-gallery.html')
+});
+
+
+app.listen(3000, () => {
+    console.log('My first app listening on port 3000!');
+});
