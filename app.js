b9cb9aca2ac9a05a2884db4de2f2e5826197f866
brunobsb <tavares.bruno@gmail.com>
2020-03-24 08:35:24 -0300

    basic structure ok

---
+++ app.js
@@ -0,0 +1,28 @@
+// Require Express
+const express = require('express');
+
+// Express server handling requests and responses
+const app = express();
+
+// Make everything inside of public/ available
+app.use(express.static('public'));
+
+// our first Route:
+app.get('/', (request, response, next) => {
+  response.sendFile(__dirname + '/views/home.html');
+});
+
+// about route:
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about.html');
+});
+
+// works route:
+app.get('/works', (request, response, next) => {
+    response.sendFile(__dirname + '/views/works.html');
+  });
+
+// Server Started
+app.listen(3000, () => {
+  console.log('port 3000 Started!');
+});
