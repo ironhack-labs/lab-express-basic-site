811f1b19fe8e101f63aed86e5cf26d05bd7abe00
Gabriela Aguiar <aguiar.gabi95@gmail.com>
2020-03-24 09:04:02 -0300

    done

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));  
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/about', (req, res, next) => {
+    res.sendFile(__dirname + '/views/about.html');
+  });
+
+  app.get('/works', (req, res, next) => {
+    res.sendFile(__dirname + '/views/works.html');
+  });
+
+  app.get('/photo', (req, res, next) => {
+    res.sendFile(__dirname + '/views/photo.html');
+  });
+  
+
+
+app.listen(3000, () => console.log('server is running...'))
