381bfecc09e52a9565de86a8eca8faf3c1ac41b6
carlosgvinuesa <56604586+carlosgvinuesa@users.noreply.github.com>
2020-03-30 09:32:33 -0600

    estructura construida

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/views/index.html`);
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(`${__dirname}/views/about.html`);
+});
+
+app.get("/work", (req, res) => {
+  res.sendFile(`${__dirname}/views/work.html`);
+});
+app.get("/gallery", (req, res) => {
+  res.sendFile(`${__dirname}/views/gallery.html`);
+});
