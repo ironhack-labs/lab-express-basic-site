5c112790c377cc656eff342a092da8f8047adfa1
juliamendesc <juliamendesc@gmail.com>
2020-04-08 21:27:57 +0100

    lab done

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/home', (request, response) => {
+    response.sendFile(`${__dirname}/views/index.html`);
+});
+
+app.get('/works', (request, response) => {
+    response.sendFile(`${__dirname}/views/works.html`);
+});
+
+app.get('/about', (request, response) => {
+    response.sendFile(`${__dirname}/views/about.html`);
+});
+
+app.listen(3000, () => console.log('My first app is listening'));
