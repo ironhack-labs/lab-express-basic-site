8d92d12a3e4dac1319dc6727d8a765a62fb321a0
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-14 17:18:16 +0200

    set server listener, ensure requests are successfully received

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-const express = require("express")
+const express = require('express')
 
 const app = express()
 
+//public directory middleware
+
+//endpoints
+
+app.get('/', (req, res) => {
+  console.log(req)
+  res.send('welcome nerd')
+})
+
+//listener
+app.listen(3000, () => console.log('App listening on port 3000'))
