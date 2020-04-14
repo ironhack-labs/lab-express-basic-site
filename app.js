070bb587976f712498358c77aa82267602366d15
Yai <myd.rio@hotmail.com>
2020-04-14 18:15:10 +0200

    home page estilada y terminada

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+//Requerimiento y aplicación
 const express = require('express')
 const app = express()
 
+//Directorio público
 app.use(express.static('public'))
 
 //enrutados
@@ -9,5 +11,5 @@ app.get('/me', (req, res) => res.sendFile(`${__dirname}/views/about-me.html`))
 app.get('/work', (req, res) => res.sendFile(`${__dirname}/views/works.html`))
 app.get('/gallery', (req, res) => res.sendFile(`${__dirname}/views/gallery.html`))
 
-
+//escucha
 app.listen(3000, () => console.log('Servidor on'))
