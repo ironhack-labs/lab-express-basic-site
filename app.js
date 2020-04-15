864fe66c32c7a6931db432d0b4f9be87b6c20768
Loriel Sher <loriel099@gmail.com>
2020-04-15 14:15:27 +0200

    Solved Lab

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (request, response, next) => {
-    response.sendFile(__dirname + "/views/index.html")
-});
 
-app.get('/about', (request, response, next) => {
-    response.sendFile(__dirname + "/views/about.html")
-});
+//app.get('/home') is equivalent to '/'
+app.get('/', (req, res, next) => res.sendFile(__dirname + '/views/home.html'))
+
+app.get('/about', (req, res, next) => res.sendFile(__dirname + '/views/about.html'))
+
+//this page isn't working for some reason - Cannot GET /views/works.html
+app.get('/views/works.html', (req, res, next) => res.sendFile(__dirname + '/views/works.html'))
 
 app.listen(3000)
