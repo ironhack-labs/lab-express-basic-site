8dfecda6938e681995b828a1d3c11ff456a92c1a
Samuel Littlefair-Wallace <sam@littlefairwallace.ca>
2020-04-15 22:43:09 +0200

    Draft of basic express site

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const server = express();
+const hbs = require("hbs");
+const data = require("./data.json");
+
+console.log(data.home);
+
+server.use(express.static(path.join(__dirname, "public")));
+server.set("view engine", "hbs");
+
+server.get("/", function (request, response) {
+  response.render("home.hbs", data.home);
+});
+
+server.get("/portfolio", function (request, response) {
+  response.render("portfolio.hbs", data.portfolio);
+});
+
+server.get("/photos", function (request, response) {
+  response.render("photos.hbs", data.photos);
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server listening on http://localhost:${process.env.PORT}`);
+});
