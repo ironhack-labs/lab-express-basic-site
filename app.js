361378964fce58f8d9e87ace64ad64d69e2bbbdb
Karen Perez <karenp.flores47@gmail.com>
2020-04-16 17:32:41 -0500

    done

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/views/home.html`);
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(`${__dirname}/views/about.html`);
+});
+
+app.get("/works", (req, res) => {
+    res.sendFile(`${__dirname}/views/works.html`);
+});
+
+app.get("/gallery", (req, res) => {
+    res.sendFile(`${__dirname}/views/gallery.html`);
+});
+
+app.listen(3000, () => {
+    console.log("El servidor esta corriendo en el puerto 3000")
+});
