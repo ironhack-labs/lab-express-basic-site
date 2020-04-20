67445ac4d03f5fd31527ed3aa09f95825859fd55
Florian Jomain <florian.jomain@gmail.com>
2020-04-20 10:17:06 +0200

    Committing final version with basic newsletter signup module

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ const mongoose = require("mongoose");
 const express = require("express");
 const hbs = require("hbs");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 hbs.registerPartials(path.join(__dirname, "views/partials"));
 app.use(express.static(path.join(__dirname, "public")));
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+);
 
 const baseRouter = require("./routes/baseRoutes");
 app.use("/", baseRouter);
-
-app.use(express.urlencoded({
-    extended: true
-}));
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on http://localhost:${process.env.PORT}`);
--- routes/baseRoutes.js
+++ routes/baseRoutes.js
@@ -1,51 +1,50 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Newsletter = require('../models/newsletter.js');
+const Newsletter = require("../models/newsletter.js");
 
-router.get('/', (req, res) => {
-    res.render('index.hbs', {
-        title: 'Eliane Radigue',
-        styles: ['index.css']
+router.get("/", (req, res) => {
+    res.render("index.hbs", {
+        title: "Eliane Radigue",
+        styles: ["index.css"],
     });
 });
 
-router.post('/', (req, res) => {
-    console.log(req.body);
-    console.log("Newsletter post route");
+router.post("/", (req, res) => {
     Newsletter.create(req.body)
         .then((dbResult) => {
-            res.redirect('/');
+            console.log(req.body);
+            res.redirect("/");
         })
         .catch((dbErr) => {
             console.log(dbErr);
         });
 });
 
-router.get('/about', (req, res) => {
-    res.render('about.hbs', {
-        title: 'About Eliane Radigue',
-        styles: ['about.css'],
+router.get("/about", (req, res) => {
+    res.render("about.hbs", {
+        title: "About Eliane Radigue",
+        styles: ["about.css"],
     });
 });
 
-router.get('/works', (req, res) => {
+router.get("/works", (req, res) => {
     const videos = [{
-            url: 'https://www.youtube.com/embed/D2U0q4lZiFg',
-            caption: 'IMA Portrait documentary',
+            url: "https://www.youtube.com/embed/D2U0q4lZiFg",
+            caption: "IMA Portrait documentary",
         },
         {
-            url: 'https://www.youtube.com/embed/NLEy94_FjG0',
-            caption: 'Adnos I-III',
+            url: "https://www.youtube.com/embed/NLEy94_FjG0",
+            caption: "Adnos I-III",
         },
         {
-            url: 'https://www.youtube.com/embed/l6gncKkp34E',
+            url: "https://www.youtube.com/embed/l6gncKkp34E",
             caption: `L'entretien infini - Hans Ulrich Obrist - 2014`,
-        }
+        },
     ];
 
-    res.render('works.hbs', {
-        title: 'Sound works',
-        styles: ['works.css'],
+    res.render("works.hbs", {
+        title: "Sound works",
+        styles: ["works.css"],
         videos: videos,
     });
 });
