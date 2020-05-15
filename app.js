fe7ed30d1135f1d8d35fa090ae0be803d9b4790e
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-05-15 20:45:22 +0200

    website pages created + static directory and files

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.get('/home', (req,res, next) => {
-    console.log(req);
-    res.send('<h1> Welcome Ironhacker </h1>')
+// Make public directory available
+app.use(express.static('public'));
+
+app.get('/home', (req, res, next) => {
+    res.sendFile(__dirname + '/views/home-page.html')
+});
+
+app.get('/about', (req, res, next) => {
+    res.sendFile(__dirname + '/views/about-page.html')
+});
+
+app.get('/works', (req, res, next) => {
+    res.sendFile(__dirname + '/views/works-page.html')
+});
+
+app.get('/gallery', (req, res, next) => {
+    res.sendFile(__dirname + '/views/gallery-page.html')
 });
 
 app.listen(3000)
