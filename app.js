336ec516d268486de95aa56177ffcc6d0ce1b6bd
Marc De Mena <mdemena@gmail.com>
2020-05-15 17:53:25 +0200

    Finished

--- app.js
+++ app.js
@@ -6,18 +6,24 @@ const app = express();
 app.use(express.static("public"));
 app.set("views", __dirname + "/views");
 
+const data = {
+  name: "Marc De Mena",
+  siteTittle: "Marc De Mena site",
+  facebookLink: "https://www.facebook.com/mdemena",
+  linkedInLink: "https://www.linkedin.com/in/mdemena",
+  twitterLink: "https://www.twitter.com/mdemena",
+  siteLink: "https://www.sctechsolutions.es",
+  mobilePhone: "+34627462305",
+};
 //Routes
 app.get("/", (req, res) => {
-  res.render("index.hbs");
-});
-app.get("/", (req, res) => {
-  res.render("index.hbs");
+  res.render("index.hbs", data);
 });
 app.get("/about", (req, res) => {
-  res.render("about.hbs");
+  res.render("about.hbs", data);
 });
 app.get("/photo", (req, res) => {
-  res.render("photo.hbs");
+  res.render("photo.hbs", data);
 });
 
 app.listen(3000);
