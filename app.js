9ad79857f215891ba2df0417f3ec32f0dfaa71c2
irene villanueva <irens.vm@gmail.com>
2020-05-15 18:36:35 +0200

    done

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+
+
+app.get('/', (req, res) => {
+    res.send('/', __dirname + '/views/home.html');
+
+});
+app.get('/works', (req, res) => {
+    res.send('/', __dirname + '/views/works.html');
+
+});
+
+app.get('/about', (req, res) => {
+    res.send('/', __dirname + '/views/about.html');
+
+});
+
+app.listen(3000);
