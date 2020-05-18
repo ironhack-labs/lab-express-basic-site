559376155a7a2bd83675e667cec401c35e823e43
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-05-18 12:12:49 +0200

    added HBS + created gallery

--- app.js
+++ app.js
@@ -1,24 +1,54 @@
 const express = require("express");
-
 const app = express();
 
 // Make public directory available
 app.use(express.static('public'));
+// Set HBS to render HTML
+app.set("view engine", "hbs");
+// Set an absolute path pointing to a folder called "views"
+app.set("views", __dirname + "/views");
 
-app.get('/home', (req, res, next) => {
-    res.sendFile(__dirname + '/views/home-page.html')
+let data = {
+    name: "John",
+    lastname: "Doe",
+    fanpage: "<em> Pikachu fanpage </em>",
+    situations: [{
+        title: "Working",
+        img: "https://pbs.twimg.com/media/C5e2Al2UYAExILo.jpg"
+        },
+        {
+            title: "Cutie",
+            img: "https://pm1.narvii.com/6112/232df22578e73fdf24b23bd029e9482b95f5b322_hq.jpg"
+        },
+        {
+            title: "Chilling",
+            img: "https://pbs.twimg.com/media/ETcrPWvWkAEb2nv.jpg"
+        },
+        {
+            title: "Messing around",
+            img: "https://i.pinimg.com/originals/2f/5a/31/2f5a31f98c176441ccbe61c1eca1282f.png"
+        }],
+    newuser: false
+}
+
+app.get('/', (req, res, next) => {
+    // res.sendFile(__dirname + '/views/home-page.html')
+    res.render("home-page.hbs",data)
 });
 
 app.get('/about', (req, res, next) => {
-    res.sendFile(__dirname + '/views/about-page.html')
+    // res.sendFile(__dirname + '/views/about-page.html')
+    res.render("about-page.hbs")
 });
 
 app.get('/works', (req, res, next) => {
-    res.sendFile(__dirname + '/views/works-page.html')
+    // res.sendFile(__dirname + '/views/works-page.html')
+    res.render("works-page.hbs")
 });
 
 app.get('/gallery', (req, res, next) => {
-    res.sendFile(__dirname + '/views/gallery-page.html')
+    // res.sendFile(__dirname + '/views/gallery-page.html')
+    res.render("gallery-page.hbs",data)
 });
 
-app.listen(3000)
+app.listen(3000);
