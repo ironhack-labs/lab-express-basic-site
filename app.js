f7b2fec069c0b07dd8c17611aa9b6ae674d5e837
Guillem Millan <millerguillem@gmail.com>
2020-05-25 04:40:24 +0200

    done

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+ 
+const app = express();
+ 
+app.use(express.static('public'));
+ 
+app.get('/', (req, res, next) => res.sendFile(__dirname + '/views/home.html')); 
+app.get('/about', (req, res, next) => res.sendFile(__dirname + '/views/about.html'));
+app.get('/works', (req, res, next) => res.sendFile(__dirname + '/views/work.html'));
+app.get('/gallery', (req, res, next) => res.sendFile(__dirname + '/views/gallery.html'));
+
+app.listen(3000, () => console.log('My first app listening on port 3000!'));
