b969d25e005f09397d9620fdd770d49d93385eaf
LucyJunior <53796420+LucyJunior@users.noreply.github.com>
2020-05-27 15:48:02 +0200

    final

--- app.js
+++ app.js
@@ -3,44 +3,29 @@
 const express = require('express')
 const app = express()
 
-function ourMiddleWare(req, res, next){
-    console.log(__dirname);
+function ourMiddleWare(req, res, next) {
     next();
-}
-
-
-app.use(ourMiddleWare);
-app.use(express.static('public'))
-
-app.set('view engine', 'hbs');
-
-// app.get('/', function (req, res) {
-//     res.sendFile(__dirname + '/views/home.html');
-// })
-
-app.get('/', function (req, res) {
-       res.send('This is Home')
-     })
-
-app.get('/about', function (req, res) {
-    res.sendFile(__dirname + '/views/about.html');
-})
-
-
-app.get('/info', function (req, res) {
-    let herInfo =  [
-        {name: 'Andy', age: 19},
-        {name: 'Abdy', age: 22},
-        {name: 'Lucy', age: 21},
-        {name: 'Juan', age: 20}
-    ]
-    res.render(__dirname + '/views/hbs/herInfo.hbs', {age})
-})
-
-// app.get('/student/:studentName', function (req, res) {
-    
-//       res.render(__dirname + '/views/hbs/students.hbs', {studentName: req.params.studentName})
-// })
+  }
+  
+  app.use(ourMiddleWare);
+  app.use(express.static('public'))
+  
+  app.get('/', function (req, res) {
+      res.sendFile(__dirname + '/views/home.html')
+  })
+  
+  app.get('/about', function (req, res) {
+      res.sendFile(__dirname + '/views/about.html')
+  })
+  
+  app.get('/pic', function (req, res) {
+      res.sendFile(__dirname + '/views/pic.html')
+  })
+  
+  app.get('/work', function (req, res) {
+      res.sendFile(__dirname + '/views/work.html')
+  })
+   
 
 app.listen(3008, '127.0.0.1', () => {
     console.log(`Server running at http://127.0.0.1:3008/`);
