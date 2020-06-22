5df921793512776a2334161ddff1591b67ad36c7
miguelbachi5a <miguelbachi5a@gmail.com>
2020-06-22 20:55:19 +0200

    Finished: minor changes

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const path  = require("path");
 
+app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', (request, response, next) => response.render('home'))
 
