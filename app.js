57d999c310ddf8c7a1b311872c1515e9971c9a59
Pumpel <romanbaos@gmail.com>
2020-06-23 20:43:14 +0200

    simple express wesite

--- app.js
+++ app.js
@@ -2,8 +2,26 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.static('public'))
+
 app.get('/',(req,res,next) => {
-    res.send('<h1>Welcome Ironhack.... :)</h1>')
+    res.redirect('/home')
+})
+
+app.get('/home',(req,res,next) => {
+    res.sendFile(__dirname + '/views/home.html')
+})
+
+app.get('/about',(req,res,next) => {
+    res.sendFile(__dirname + '/views/about.html')
+})
+
+app.get('/work',(req,res,next) => {
+    res.sendFile(__dirname + '/views/works.html')
+})
+
+app.get('/photo-gallery',(req,res,next) => {
+    res.sendFile(__dirname + '/views/photo-gallery.html')
 })
 
 app.listen(3000,() => 'Starting server')
