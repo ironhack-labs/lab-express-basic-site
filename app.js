7911c17f8a09211b22b6bd365137d1392d8d3449
aitorgb <aitorguerrero9@gmail.com>
2020-06-23 13:27:55 +0200

    exercise completed

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', (req, res, next) => {
+  res.sendFile(__dirname + '/views/index.html')
+  })
+
+app.get('/about', (req, res, next) => {
+  res.sendFile(__dirname + '/views/about.html')
+  })
+app.get('/work', (req, res, next) => {
+    res.sendFile(__dirname + '/views/work.html')
+  })
+app.get('/gallery', (req, res, next) => {
+    res.sendFile(__dirname + '/views/gallery.html')
+  })
+
+
+  app.listen(3000, () => {
+    console.log('hello');
+  })
+
