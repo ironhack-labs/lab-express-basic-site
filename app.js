eb3dec2f2115f67dd2113a2e540139221379b009
SiEr0212 <simonertel@gmx.de>
2020-06-24 16:06:04 +0200

    basic structure with links between the pages done

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res)=> {
+    res.sendFile(__dirname + '/views/home-page.html');
+});
+app.get('/about', (req, res)=> {
+    res.sendFile(__dirname + '/views/about-page.html');
+});
+app.get('/works', (req, res)=> {
+    res.sendFile(__dirname + '/views/works-page.html');
+});
+
+app.get('/photo-gallery', (req, res)=> {
+    res.sendFile(__dirname + '/views/photogallery-page.html');
+});
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
