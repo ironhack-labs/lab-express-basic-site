ef7a0addf00885032a5c46bc5776121e03fb9a3f
Ritesh <tauraniritesh@gmail.com>
2020-06-24 17:38:26 +0200

    css pending

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home-page.html');
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/gallery', (req, res) => {
+    res.sendFile(__dirname + '/views/photo-gallery.html');
+});
+
+app.get('/works', (req, res) => {
+    res.sendFile(__dirname + '/views/works.html');
+});
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
