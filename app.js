8d1f6d1f7bca8f5887443bd840fa08d003f8f141
Izquiano <izquiano@hotmail.com>
2020-06-25 20:39:25 +0200

    Done

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require ('express')
-var app = express()
+const app = express()
+
 
 app.use(express.static('public'))
 
@@ -7,6 +8,16 @@ app.use(express.static('public'))
 app.get('/home', (req, res) => 
     res.sendFile(__dirname + '/views/home.html'))
     
+app.get('/about', (req, res) => 
+res.sendFile(__dirname + '/views/about.html'))
+
+
+app.get('/works', (req, res) => 
+res.sendFile(__dirname + '/views/works.html'))
+
+
+app.get('/gallery', (req, res) => 
+res.sendFile(__dirname + '/views/gallery.html'))
     
 
 
---
+++ public/scripts/script.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+    $('.gallery').slick({
+        infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1
+        
+  })
+});
+
