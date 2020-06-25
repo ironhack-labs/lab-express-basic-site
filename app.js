20ab215187cff92196c8dae051b172750a49eb1e
Andrea Astorga <andrea.astorga95@gmail.com>
2020-06-25 10:30:55 +0200

    All HTML done, CSS and fixing errors still in process

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const app = express();
 app.use(express.static('public'));
 
 //home page Route:
-app.get('/home', (request, response, next) => response.sendFile(__dirname + '/views/home-page.html'));
+app.get('/', (request, response, next) => response.sendFile(__dirname + '/views/home-page.html'));
 
 app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about.html'));
 
 app.get('/works', (request, response, next) => response.sendFile(__dirname + '/views/works.html'));
 
-app.get('/photos', (request, response, next) => response.sendFile(__dirname + '/views/photos.html'));
+app.get('/gallery', (request, response, next) => response.sendFile(__dirname + '/views/photos.html'));
 
 // Server Started
 app.listen(3000, () => console.log('My first app listening on port 3000! '));
