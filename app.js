c1b0466816d0ba129d25276a23fff1f597cf04ff
Paola Garay <garay.angela@gmail.com>
2020-06-26 16:16:24 +0200

    done

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res, next) => {
+     res.sendFile(__dirname + '/views/index.html');
+});
+
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/views/about.html');
+});
+
+
+app.listen(3000, () => {
+     console.log('Server listening on port 3000'); 
+});
