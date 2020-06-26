455ac34619d6b6a50980a58f09d030daa62b4776
mateomp <mateomadariaga96@gmail.com>
2020-06-26 19:41:35 +0200

    All iterations done + bonus images are included in each of the routes

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/home', (request, response, next) => response.sendFile(__dirname + '/views/home.html'));
+
+app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about.html'));
+
+app.get('/works', (request, response, next) => response.sendFile(__dirname + '/views/works.html'));
+
+app.listen(3000, () => console.log(' App listening on port 3000! '));
