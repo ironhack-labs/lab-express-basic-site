b808c24772ad1858dd81a2cb3cc5968027e29137
Old <onelifedesigning@gmail.com>
2020-07-04 13:00:27 +0200

    end

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-// Require Express
 const express = require('express');
-// Express server handling requests and responses
 const app = express();
 
-// Define public folder as default static path
 app.use(express.static('public'))
 
 // Define engine an folder for templates
-app.set('view engine', 'hbs');
 app.set('views', `${__dirname}/views`);
 
-const data = {
-  name: 'Steve',
-  lastName: 'Esteban',
-}
-// our first Route:
-app.get('/', (req, res) => res.redirect('/home'))
-app.get('/home', (req, res) => res.render('home', {
-  data
-}))
+app.get('/', (request, response, next) => {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/works', (request, response, next) => {
+  response.sendFile(__dirname + '/views/works.html');
+});
+
+app.get('/gallery', (request, response, next) => {
+  response.sendFile(__dirname + '/views/gallery.html');
+});
 
 // Server Started
 app.listen(3000, () => console.log('My first app listening on port 3000! http://localhost:3000/'));
