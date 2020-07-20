82b97efd71eb64f048a640d3dd493e491bdd2429
Shane Schipper <shane99@vt.edu>
2020-07-20 17:46:08 -0400

    stuff

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+
+app.get("/", (req, res, next) => {
+    res.sendFile(__dirname + "/views/Home.html");
+  });
+  app.get("/about", (req, res, next) => {
+    res.sendFile(__dirname + "/views/About.html");
+  });
+  app.get("/works", (req, res, next) => {
+    res.sendFile(__dirname + "/views/Works.html");
+  });
