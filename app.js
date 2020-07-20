816b37f62d65b1bf905de3cced74980b8c2a28e1
Daniel Perz <dpere316@fiu.edu>
2020-07-20 14:07:28 -0400

    done

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/',(request,response,next)=>{
+    console.log(request);
+    response.sendFile(__dirname + '/views/home.html')
+});
+app.get('/about',(request,response,next)=>{
+    console.log(request);
+    response.sendFile(__dirname + '/views/about.html')
+});
+app.get('/works',(request,response,next)=>{
+    console.log(request);
+    response.sendFile(__dirname + '/views/works.html')
+});
+app.get('/photo',(request,response,next)=>{
+    console.log(request);
+    response.sendFile(__dirname + '/views/photo.html')
+});
+
+app.listen(3000, () => console.log('My first app listening on port 3000!'));
