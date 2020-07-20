db3c3aea559bed8dbc400e4e61d62a8771388584
Shane Schipper <shane99@vt.edu>
2020-07-20 17:47:48 -0400

    done

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ app.use(express.static('public'));
 
 app.get("/", (req, res, next) => {
     res.sendFile(__dirname + "/views/Home.html");
-  });
-  app.get("/about", (req, res, next) => {
+});
+app.get("/about", (req, res, next) => {
     res.sendFile(__dirname + "/views/About.html");
-  });
-  app.get("/works", (req, res, next) => {
+});
+app.get("/works", (req, res, next) => {
     res.sendFile(__dirname + "/views/Works.html");
-  });
+});
