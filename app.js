20170da99e97e278a01aadc616f710d990fe7c81
Kai Quander <kq1003@gmail.com>
2020-07-20 15:32:25 -0400

    Done

--- app.js
+++ app.js
@@ -8,16 +8,12 @@ app.use(express.static('public'));
 
 app.get('/', (request, response, next) => response.sendFile(__dirname + '/views/intro.html'));
  
-app.get('/home', (request, response, next) => {
-    response.send('<h1>Welcome Visitor.</h1>');
-});
+app.get('/home', (request, response, next) => response.sendFile(__dirname + '/views/intro.html'));
 
-app.get('/about', (request, response, next) => {
-    response.send('<h1>This is some info about the person I am making this page for!</h1>');
-});
 
-app.get('/works', (request, response, next) => {
-    response.send('<h1>Here I will showcase some of the work this person has done!</h1>');
-});
+app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about.html'));
+
+app.get('/works', (request, response, next) => response.sendFile(__dirname + '/views/works.html'));
+
 
 app.listen(process.env.PORT, () => console.log(`My first app listening on port ${process.env.PORT} `));
