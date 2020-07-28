5c09ba5ebad676ea7870ba31c678d46f5bf2953b
Elise Djn <elisedjn@protonmail.com>
2020-07-28 17:32:53 +0200

    done

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'))
+app.get('/about', (req, res) => res.sendFile(__dirname + '/views/about.html'))
+app.get('/works', (req, res) => res.sendFile(__dirname + '/views/works.html'))
+app.get('/gallery', (req, res) => res.sendFile(__dirname + '/views/gallery.html'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
