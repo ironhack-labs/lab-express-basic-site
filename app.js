05d0593b943f9fa72fc140aeaa6ca45b5712f174
anna dodo <anna.dorenskaya@gmail.com>
2020-08-22 16:37:30 +0200

    done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'))
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 app.get('/', (request, response, next) => {
     response.sendFile(`${__dirname}/views/home.html`)
