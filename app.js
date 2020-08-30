a2103c1f0c90b35959b41b26d2bfa5140f9a7367
unimatrix2 <dnny.nis@gmail.com>
2020-08-30 18:23:09 -0300

    basic server setup complete

---
+++ app.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-console */
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (request, response, next) => {
+    console.log(`We recieved a new request in route ${request.url}`);
+
+    response.sendFile(`${__dirname}/views/home.html`);
+});
+
+app.get('/about', (request, response, next) => {
+    console.log(`We recieved a new request in route ${request.url}`);
+
+    response.sendFile(`${__dirname}/views/about.html`);
+});
+
+app.get('/works', (request, response, next) => {
+    console.log(`We recieved a new request in route ${request.url}`);
+
+    response.sendFile(`${__dirname}/views/works.html`);
+});
+
+app.get('/phrases.js', (request, response, next) => {
+    console.log(`We recieved a new request in route ${request.url}`);
+
+    response.sendFile(`${__dirname}/phrases.js`);
+});
+
+app.listen(3000, () => {
+    console.log('App running on PORT 3000');
+});
