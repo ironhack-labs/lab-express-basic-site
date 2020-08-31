c0c66caa6d832ec4a8799846c482b75fbb33f6af
unimatrix2 <dnny.nis@gmail.com>
2020-08-31 18:04:49 -0300

    implemented disclaimer alert

--- app.js
+++ app.js
@@ -30,6 +30,12 @@ app.get('/phrases.js', (request, response, next) => {
     response.sendFile(`${__dirname}/phrases.js`);
 });
 
+app.get('/alert.js', (request, response, next) => {
+    console.log(`We recieved a new request in route ${request.url}`);
+
+    response.sendFile(`${__dirname}/alert.js`);
+});
+
 app.listen(3000, () => {
     console.log('App running on PORT 3000');
 });
