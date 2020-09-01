adce54131d41547042b0d5abedd7a78892aed81f
unimatrix2 <dnny.nis@gmail.com>
2020-09-01 20:52:17 -0300

    implemented layout and partials

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 const express = require('express');
+const hbs = require('hbs');
 const books = require('./public/js/books.js');
 
 const app = express();
@@ -7,25 +8,24 @@ const app = express();
 app.use(express.static(`${__dirname}/public`));
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'hbs');
+hbs.registerPartials(`${__dirname}/views/partials`);
 
 app.get('/', (request, response, next) => {
     console.log(`We recieved a new request in route ${request.url}`);
 
-    response.render('home');
+    response.render('public-views/home');
 });
 
 app.get('/about', (request, response, next) => {
     console.log(`We recieved a new request in route ${request.url}`);
 
-    response.render('about');
+    response.render('public-views/about');
 });
 
 app.get('/works', (request, response, next) => {
     console.log(`We recieved a new request in route ${request.url}`);
 
-    response.render('works', {
-        books,
-    });
+    response.render('public-views/works', { books });
 });
 
 app.listen(3000, () => {
