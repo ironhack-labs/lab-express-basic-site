b99bb10a83c87c65bd0376113e15cf391b092f6d
Mira Vogt <miravogt@gmail.com>
2020-09-02 16:16:52 +0200

    finished

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/homepage.html');
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/views/about.html');
+})
+
+app.get('/photogallery', (req, res) => {
+    res.sendFile(__dirname + '/views/photogallery.html');
+})
+
+app.get('/works', (req, res) => {
+    res.sendFile(__dirname + '/views/works.html');
+})
+
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000')
+})
