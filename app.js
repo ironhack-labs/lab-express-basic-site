32cf5d7fe0d649b04bfd359c7e94e23c3398e349
Marly <marlenak90@gmail.com>
2020-09-02 16:29:52 +0200

    all pages ready

---
+++ app.js
@@ -0,0 +1,28 @@
+
+const port = 3000;
+const express = require('express');
+const app = express ();
+
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home-page.html')
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/views/about.html')
+});
+
+app.get('/works', (req, res) => {
+    res.sendFile(__dirname + '/views/works.html')
+});
+
+app.get('/gallery', (req, res) => {
+    res.sendFile(__dirname + '/views/gallery.html')
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
