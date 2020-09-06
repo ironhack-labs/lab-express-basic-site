56fd0ea1ebb0aa79942f34163c7489f7aa755863
Grazielli Pozzi <graziellipozzi@gmail.com>
2020-09-06 10:42:48 -0300

    done

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (request, response, next) => {
+
+response.sendFile(__dirname + '/views/home.html');
+
+});
+
+app.get('/about', (request, response, next) => {
+
+response.sendFile(__dirname + '/views/about.html');
+
+});
+
+app.get('/works', (request, response, next) => {
+
+response.sendFile(__dirname + '/views/works.html');
+
+});
+
+app.get('/photos', (request, response, next) => {
+
+response.sendFile(__dirname + '/views/photos.html');
+
+});
+
+app.listen(3000);
