79c40463e4f5ff0da5d14fe08961e5a106f384c0
Filipe Deboni <deboni.filipe@gmail.com>
2020-09-08 19:52:17 -0300

    fisrt day done: website base

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+  console.log(__dirname);
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/about", (request, response) => {
+  console.log(__dirname);
+  response.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/works", (request, response) => {
+  console.log(__dirname);
+  response.sendFile(__dirname + "/views/works.html");
+});
+
+app.listen(3000, () => console.log("Server up and running on port 3000"));
