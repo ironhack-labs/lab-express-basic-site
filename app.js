2cf2ba0693aea6250c681a48bb44d15a25c4ab39
Cynthia Takiishi <ycynthiaakemit@gmail.com>
2020-09-13 03:11:00 -0300

    Solved Lab

---
+++ app.js
@@ -0,0 +1,24 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/home', (request, response, next) => {
+  response.sendFile(`${__dirname}/views/home.html`);
+});
+
+app.get('/about', (request, response, next) => {
+  response.sendFile(`${__dirname}/views/about.html`);
+});
+
+app.get('/works', (request, response, next) => {
+  response.sendFile(`${__dirname}/views/works.html`);
+});
+
+
+app.listen(3000, () => console.log('App running on 3000!'))
