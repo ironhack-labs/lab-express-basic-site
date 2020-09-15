2785d7c8b8d7af980b8cc34a030a29831707072e
Lisa Poncet <poncet.lisa@gmail.com>
2020-09-15 23:39:40 +0200

    WIP

--- public/js/dom-goodthings.js
+++ public/js/dom-goodthings.js
@@ -1,2 +1,10 @@
 const btnTeachMe = document.getElementById("teach-btn");
 
+function displayInstructions() {
+    const list = document.getElementById("instructions-list");
+    list.classList.remove('hidden');
+    btnTeachMe.classList.add("hidden");
+}
+
+btnTeachMe.addEventListener("click", displayInstructions);
+
