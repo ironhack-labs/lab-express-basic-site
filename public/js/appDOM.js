f0d20c2cf1aec8c7842fd6a8d697a5c22af497dc
Lisa Poncet <poncet.lisa@gmail.com>
2020-09-19 15:03:08 +0200

    update date done

--- public/js/appDOM.js
+++ public/js/appDOM.js
@@ -1,2 +1,46 @@
 feather.replace(); // render feather icons
-console.log('Hello')
+
+var fetchWeather = "/weather";
+
+const weatherForm = document.getElementById("location-form");
+const input = document.querySelector(".location-input");
+const cityName = document.querySelector(".location");
+const weatherCondition = document.querySelector(".weather-desc");
+const weatherIcon = document.querySelector(".weather-icon");
+const temperature = document.querySelector(".weather-temp");
+const dayName = document.querySelector(".date-dayname");
+const fullDate = document.querySelector(".date-day");
+
+//DISPLAY DATE
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+dayName.textContent = `${days[new Date().getDay()]}`;
+fullDate.textContent = `${new Date().getDate()}, ${
+  months[new Date().getMonth()]
+} ${new Date().getFullYear()}`;
+
+weatherForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(input.value);
+});
