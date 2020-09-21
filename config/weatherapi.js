0dc0d4fd6195221c5b7fce6d8faf42be27482485
Lisa Poncet <poncet.lisa@gmail.com>
2020-09-21 12:16:40 +0200

    hide API key

--- config/weatherapi.js
+++ config/weatherapi.js
@@ -1,7 +1,7 @@
 const constants = {
   openWeatherMap: {
     BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
-    API_KEY: "5f53873c40e0016f74072a264344c35e",
+    API_KEY: process.env.WEATHER_API_KEY,
   },
 };
 
