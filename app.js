6c44edb56e5a76c7fe626d2477386b5fa8fd4766
Sant <santiagoandres@outlook.de>
2020-10-06 19:38:20 +0200

    Almost all.

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/about", function (req, res) {
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/album", function (req, res) {
+  res.sendFile(__dirname + "/views/album.html");
+});
+
+app.get("/home", function (req, res) {
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/works", function (req, res) {
+  res.sendFile(__dirname + "/views/works.html");
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(__dirname + "/views/404.html");
+});
+
+app.listen(3000);
