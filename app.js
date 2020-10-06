204afa88d5733dfc76066578eb64af861e14fe3c
Marta_Obregon <marta.garcia.obregon@gmail.com>
2020-10-06 17:10:02 +0100

    commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname+'/public'))
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname+'/views/landing.html')
+})
+
+app.get('/home', function(req, res){
+    res.sendFile(__dirname+'/views/home.html')
+})
+app.get('/works', function(req, res){
+    res.sendFile(__dirname+'/views/works.html')
+})
+app.get('/gallery', function(req, res){
+    res.sendFile(__dirname+'/views/gallery.html')
+})
+
+
+app.listen(3001, () => {
+    console.log('Working')
+})
