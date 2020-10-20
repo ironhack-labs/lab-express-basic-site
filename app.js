199c1881667db6d18d20acf08a99087aa0f5b96a
Christian <christian_8_neus@hotmail.com>
2020-10-20 16:46:58 +0200

    Solved Lab

---
+++ app.js
@@ -0,0 +1,26 @@
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (request, response, next) => {
+    response.sendFile(__dirname + "/views/home-page.html");
+});
+
+app.get("/about", (request, response, next) => {
+    response.sendFile(__dirname + "/views/about-page.html");
+});
+
+app.get("/works", (request, response, next) => {
+    response.sendFile(__dirname + "/views/works-page.html");
+});
+
+app.get("/photo", (request, response, next) => {
+    response.sendFile(__dirname + "/views/photo-page.html");
+});
+
+app.listen(3000, () => {
+    console.log("App listen on port 3000!!");
+});
