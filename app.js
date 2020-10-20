3dfaac3c9f45e779f9ea1820b5df9a6642e2f621
Oscar M <empanaisimo@gmail.com>
2020-10-20 17:12:16 +0100

    Solved Lab

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static('public'));
+
+app.get('/', (request, response, next) => {
+    response.sendFile(__dirname + '/views/home-page.html');
+  });
+
+app.get('/about', (request, response, next) => {
+response.sendFile(__dirname + '/views/about-page.html');
+});
+
+app.get('/gallery', (request, response, next) => {
+response.sendFile(__dirname + '/views/gallery-page.html');
+});
+
+app.get('/works', (request, response, next) => {
+response.sendFile(__dirname + '/views/works-page.html');
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
