08861e891aa5d2f9e80c68d4fdafac769350fc35
sophiepa <sophiepa@hotmail.com>
2020-11-02 17:13:05 +0100

    localhost:3000 does not work.. is it a problem with chrome? did the basic actions without styling

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require ("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get('/home', (request, response, next) => 
+    response.sendFile(__dirname + '/views/home-page.html')); 
+
+app.get('/about', (request, response, next) => 
+    response.sendFile(__dirname + '/views/about-page.html'));
+
+app.get('/works', (request, response, next) => 
+    response.sendFile(__dirname + '/views/works-page.html'));
+
+app.listen(3000, () => console.log('App works on port 3000'));
+
