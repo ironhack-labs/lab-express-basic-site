e245a857dda938e3758c86bd2ef745c00dbac6b3
Elia Chiarucci <elia.chiarucci@gmail.com>
2020-11-03 11:20:12 +0100

    Fixes

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/views/index.html")
-})
+    res.sendFile(__dirname + "/views/index.html")
+});
 
 app.get("/gallery", (req, res) => {
-    res.sendFile(__dirname + "/public/views/gallery.html")
+    res.sendFile(__dirname + "/views/gallery.html")
 })
 
 app.get("/about", (req, res) => {
-    res.sendFile(__dirname + "/public/views/about.html")
+    res.sendFile(__dirname + "/views/about.html")
 })
 
 app.listen(5000, () => {
