c3d92c750d8645a5b94b35bac85910789c8cadc0
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-03 17:36:35 -0600

    works, home and httml

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express()
 // /Users/ivanmac/Desktop/lab-express-basic-site
-const dirName = __dirname
-console.log(dirName);
+// const dirName = __dirname
+// console.log(dirName);
 app.use(express.static("public"))
+
+
+app.get("/", (request, response) => {
+  response.sendFile(`${__dirname}/views/index.html`)
+})
+
+app.get("/about", (req, res) => {
+  res.sendFile(`${__dirname}/views/about.html`)
+})
+app.get("/Works", (req, res) => {
+  res.sendFile(`${__dirname}/views/works.html`)
+})
+
+app.listen(3000)
