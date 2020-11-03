705825bd2d9f5a1c50feeabb7525fe05556e7821
Yann Quintin <quintin.yann@gmail.com>
2020-11-03 10:31:24 +0100

    pushing working website & server: no content

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 5500;
+
+app.use(express.static("/public"));
+app.use(express.static("/views"));
+// app.use("/views/")
+
+
+
+app.get("/home", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/views/About.html");
+});
+app.get("/works", (req, res) => {
+    res.sendFile(__dirname + "/views/Works.html");
+});
+
+// module.exports = app;
+
+app.listen(5500, () => console.log(`My first app listening on ${port}`));
+
