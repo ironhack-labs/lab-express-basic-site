6ab92080134ad33ea5390a74239fde70aae681d5
Yann Quintin <quintin.yann@gmail.com>
2020-11-03 11:34:58 +0100

    updating app.js sendFile

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ const express = require('express');
 const app = express();
 const port = 5500;
 
-app.use(express.static("/public"));
-app.use(express.static("/views"));
+app.use(express.static("public"));
+app.use(express.static("views"));
 // app.use("/views/")
 
 
 
 app.get("/home", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
+    res.sendFile(__dirname + "index.html");
 });
 app.get("/about", (req, res) => {
-    res.sendFile(__dirname + "/views/About.html");
+    res.sendFile(__dirname + "About.html");
 });
 app.get("/works", (req, res) => {
-    res.sendFile(__dirname + "/views/Works.html");
+    res.sendFile(__dirname + "Works.html");
 });
 
 // module.exports = app;
