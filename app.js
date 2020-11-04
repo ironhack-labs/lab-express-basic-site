b6c9773b83764e84a050819da35c483e998cab2f
Maria Reyna <mereynag@gmail.com>
2020-11-03 21:49:24 -0400

    site created

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/views/index.html`)
+})
+
+app.get("/about", (req, res) => {
+    res.sendFile(`${__dirname}/views/about.html`)
+})
+
+app.get("/artists", (req, res) => {
+    res.sendFile(`${__dirname}/views/artists.html`)
+})
+
+app.get("/gallery", (req, res) => {
+    res.sendFile(`${__dirname}/views/gallery.html`)
+})
+
+app.listen(3000)
