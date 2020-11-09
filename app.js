4f37b921b7f5bb2f4090c7609261b64db9ee7540
MADFutcher <mikalob247@gmail.com>
2020-11-09 16:59:43 +0100

    completed express basic site

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/home', function(request, response, next){
+    response.sendFile(__dirname+'/views/home.html')
+})
+
+app.get('/about', function(request, response, next){
+    response.sendFile(__dirname+'/views/about.html')
+})
+
+app.get('/work', function(request, response, next){
+    response.sendFile(__dirname+'/views/work.html')
+})
+
+app.get('/gallery', function(request, response, next){
+    response.sendFile(__dirname+'/views/gallery.html')
+})
+
+app.listen(3000)
