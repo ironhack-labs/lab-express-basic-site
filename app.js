bce9a45952e1b0b8aedc6659154e55f70c3d5f55
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2020-11-10 18:44:20 +0100

    Git initialized

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const app = express();
+
+app.use( express.static('public')  );
+
+app.get("/", (req, res, next) => {
+  
+    res.sendFile( __dirname + '/public/views/home.html');
+  });
+  
+  app.get("/about", (req, res, next) => {
+  
+    res.sendFile( __dirname + '/public/views/about.html');
+  });
+
+  app.get("/works", (req, res, next) => {
+
+    res.sendFile(__dirname + '/public/views/works.html');
+  })
+
+  app.get("/photo-gallery", (req, res, next) => {
+
+    res.sendFile(__dirname + '/public/views/photo-gallery.html');
+  })
+
+  app.get()
+  
+  
+  
+  app.listen(3000, () => {
+    console.log("Server is running at port 3000");
+  });
