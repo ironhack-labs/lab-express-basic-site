3c018d91d8ca535695c762e9023bd0a78d1648de
Niklas Wernfeldt <niklas.wernfeldt.94@gmail.com>
2020-11-10 16:57:49 +0100

    added html boilerplate and express server

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+// CREATE SERVER
+
+const app = express();
+
+// MIDDLEWARE
+
+app.use(express.static("public"));
+
+// ROUTES
+
+app.get("/", (req, res, next) => {
+  res.sendFile(__dirname + "/public/views/index.html");
+});
+
+app.get("/about", (req, res, next) => {
+  res.sendFile(__dirname + "/public/views/index.html");
+});
+
+app.get("/works", (req, res, next) => {
+  res.sendFile(__dirname + "/public/views/index.html");
+});
+
+app.get("/gallery", (req, res, next) => {
+  res.sendFile(__dirname + "/public/views/index.html");
+});
+
+// START SERVER
+
+app.listen(3000, (req, res, next) => {
+  console.log("server is running on port 3000");
+});
