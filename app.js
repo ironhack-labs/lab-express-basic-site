e19b3d36ea79b52a71761844f5137750b5f7ef86
Lucia Skudlik <lucia.skudlik@gmail.com>
2020-11-10 19:40:48 +0100

    Solved Lab

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res, next) => {
+  res.sendFile(__dirname + "/public/views/index.html");
+});
+
+app.get("/about", (req, res, next) => {
+  res.sendFile(__dirname + "/public/views/about.html");
+});
+
+app.get("/work", (req, res, next) => {
+  res.sendFile(__dirname + "/public/views/work.html");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running at port 3000");
+});
