85031a25c414148b1ed1d1a5a87a2b68daa19236
VincenzoDeLucia <vincenzo.delucia@icloud.com>
2020-11-10 19:23:08 +0100

    Just enough CSS to say I did CSS, gallery still missing

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/home", (request, response, next) =>
+  response.sendFile(__dirname + "/views/home.html")
+);
+
+app.get("/about", (request, response, next) =>
+  response.sendFile(__dirname + "/views/about.html")
+);
+
+app.get("/works", (request, response, next) =>
+  response.sendFile(__dirname + "/views/works.html")
+);
+
+app.get("/gallery", (request, response, next) =>
+  response.sendFile(__dirname + "/views/gallery.html")
+);
+
+app.get("*", (request, response, next) =>
+  response.sendFile(__dirname + "/views/error.html")
+);
+
+app.listen(3000, () => console.log("The server is listening."));
