a71e5ad5240bc9b54c961badbed2009aa1e72ad4
hugoblack <hugoblack57@gmail.com>
2020-11-10 17:37:12 +0000

    Lab finished, without images

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+// console.log(__dirname);
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/about", (request, response) => {
+  response.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/works", (request, response) => {
+    response.sendFile(__dirname + "/views/works.html");
+  });
+  app.get("/images", (request, response) => {
+    response.sendFile(__dirname + "/views/images.html");
+  });
+
+app.listen(3001, () => {
+  console.log("LISTENING ON PORT 3001");
+});
