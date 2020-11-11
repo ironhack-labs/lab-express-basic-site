fbb8c51c3ab82a1d85110dc69281f58c34f7c646
Hortencia Jorquera <hortencia.pj@gmail.com>
2020-11-11 21:56:40 +0100

    done

---
+++ app.js
@@ -0,0 +1,24 @@
+const { response } = require('express');
+const express=require('express');
+const app=express();
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/views/home.html')
+});
+
+app.get('/career',(req,res)=>{
+    res.sendFile(__dirname+'/views/career.html')
+});
+
+app.get('/about',(req,res)=>{
+    res.sendFile(__dirname+'/views/about.html')
+});
+
+app.get('/gallery',(req,res)=>{
+    res.sendFile(__dirname+'/views/gallery.html')
+});
+app.listen(3000,()=>{
+    console.log('listening')
+})
