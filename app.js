b74480a453aeeb003b7be8334938bcbc7579efde
lighteronn <mezora197@gmail.com>
2020-11-11 21:17:43 +0100

    lab done

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname+'/views/home.html')
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname+'/views/about.html')
+  })
+
+  app.get('/works', (req, res) => {
+    res.sendFile(__dirname + '/views/works.html')
+  }) 
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
