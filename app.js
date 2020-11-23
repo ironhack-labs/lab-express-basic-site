43788f31718ee66ca5851f3331e7202762752d29
carmen valencia <carmenpilar1990@gmail.com>
2020-11-23 17:10:28 +0100

    Problems fixed

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const app = express();
 
 app.use(express.static('public'))
 
-app.get('/', (req, res, next) => {
-    res.sendFile(__dirname + '/view/home.html')
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/public/view/home.html')
 })
-app.get('/about',(req, res,next)=>{
-    res.sendFile(__dirname + '/view/about.html')
+app.get('/about',(req, res)=>{
+    res.sendFile(__dirname + '/public/view/about.html')
 })
-app.get('/work',(req, res, next)=>{
-    res.sendFile(__dirname + '/view/work.html')
+app.get('/work',(req, res)=>{
+    res.sendFile(__dirname + '/public/view/work.html')
 })
 
 app.listen(4000,()=>{
