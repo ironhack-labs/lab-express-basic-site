5bb79cd6985946d16bd1739e662f2caf796de668
Jacqueline <j.jumaga@gmail.com>
2020-12-01 23:06:29 +0100

    changes made at campus

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-app.set("views", __dirname + "views");
+app.set("views", __dirname + "/views");
 
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
   res.render("home", {
     pageTitle: "Home Page",
+    css: ["home"],
   });
 });
 
 app.get("/content", (req, res) => {
   res.render("content", {
     pageTitle: "Must Try's of 2020",
+    css: ["content"],
   });
 });
 
