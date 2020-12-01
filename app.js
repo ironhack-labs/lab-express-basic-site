3e4a117ab6e3d996f7146372565741bff25328d7
Victorien Provenzano <victorien.provenzano@icloud.com>
2020-12-01 16:22:56 +0100

    JS changes + flags

--- app.js
+++ app.js
@@ -11,8 +11,49 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/about", (req, res) => {
-  res.render("about");
+app.get("/argentina", (req, res) => {
+  res.render("argentina", {
+    name: "",
+    capital: "",
+    language: "",
+    population: 0,
+    religion: "",
+    area: "",
+    GDP: "",
+    currency: "",
+    timeZone: "",
+    culture: "",
+  });
+});
+
+app.get("/china", (req, res) => {
+  res.render("china", {
+    name: "",
+    capital: "",
+    language: "",
+    population: 0,
+    religion: "",
+    area: "",
+    GDP: "",
+    currency: "",
+    timeZone: "",
+    culture: "",
+  });
+});
+
+app.get("/france", (req, res) => {
+  res.render("france", {
+    name: "",
+    capital: "",
+    language: "",
+    population: 0,
+    religion: "",
+    area: "",
+    GDP: "",
+    currency: "",
+    timeZone: "",
+    culture: "",
+  });
 });
 
 app.listen(3000);
