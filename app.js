04df3162f322fcfe37ea5a07ba40c86a48aa0047
denisonamaral <denison.amaral@outlook.com>
2020-12-01 10:10:22 -0300

    done

---
+++ app.js
@@ -0,0 +1,20 @@
+const { response } = require("express");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/about", (request, response) => {
+  response.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/works", (request, response) => {
+  response.sendFile(__dirname + "/views/works.html");
+});
+
+app.listen(4000, () => console.log("My first app listening on port 4000!"));
