5211c05fd13dce03e2e13cf682d750b29305f149
Gwerborc'h Mahéo <gwerborch.maheo.com>
2020-12-01 18:51:50 +0100

    Fifth commit

---
+++ public/main.js
@@ -0,0 +1,9 @@
+// const resto1 = document.getElementById('resto1');
+// const resto2 = document.getElementById('resto2');
+// const resto3 = document.getElementById('resto3');
+// const resto4 = document.getElementById('resto4');
+// const resto5 = document.getElementById('resto5');
+// const resto6 = document.getElementById('resto6');
+
+// resto1.addEventListener('click')
+    
