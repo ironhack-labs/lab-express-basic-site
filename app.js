b48e93df068f161c88e7022038e35d57fcd7aae0
Elisa Chen <skechen@gmail.com>
2020-12-01 17:02:57 +0100

    lots of changes

--- app.js
+++ app.js
@@ -9,15 +9,57 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {
+    pageTitle: "Princess Diana Home",
+    imageCaptions: {
+      images: "Diana-home",
+      captions: "caption 1",
+    },
+  });
 });
 
 app.get("/bio", (req, res) => {
-  res.render("bio");
+  res.render("bio", {
+    pageTitle: "Biography",
+    imageCaptions: [
+      {
+        images: "Diana-bio",
+        captions: "caption 1",
+      },
+      {
+        images: "diana-bio2",
+        captions: "caption 2",
+      },
+      {
+        images: "diana-bio3",
+        captions: "caption 3",
+      },
+      {
+        images: "diana-bio4",
+        captions: "caption 4",
+      },
+    ],
+  });
 });
 
 app.get("/philanthropy", (req, res) => {
-  res.render("philanthropy");
+  res.render("philanthropy", {
+    pageTitle: "Philanthropy",
+    imageCaptions: [
+      {
+        images: "diana-philanthropy1",
+        captions: "with Hillary Clinton",
+      },
+      {
+        images: "diana-philanthropy2",
+        captions: "Walking amongst landmines in Angola",
+      },
+      {
+        images: "diana-philanthropy3",
+        captions: "with an AIDS victim",
+      },
+    ],
+  });
 });
 
 app.listen(3000, () => {
