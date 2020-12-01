1242a99a1ddd4dc608c4f55e315918693218bb8d
AnneVictoria <avduclaux@gmail.com>
2020-12-01 18:49:40 +0100

    add list

--- app.js
+++ app.js
@@ -10,10 +10,17 @@ app.get("/", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", {
+    album: [
+      { titre: "Desobeissance", annee: 2019 },
+      { titre: "Interstellaires", annee: 2015 },
+      { titre: "Monkey Me", annee: 2012 },
+      { titre: "Bleu Noir", annee: 2010 },
+      { titre: "Point de suture", annee: 2008 },
+    ],
+  });
 });
 
 app.listen(3000, () => {
   console.log("server is reeady! : http://localhost:3000");
 });
-
