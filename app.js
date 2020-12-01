6f434546d0e55de28a286ecaa03673829d4101ef
Intan Paoli <intankhairunnisa@gmail.com>
2020-12-01 15:47:08 +0100

    first try

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express"); // returns a function
+const app = express(); 
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+    res.render("home")
+});
+       
+app.get("/about", (req, res) => {
+    res.render("about")
+});
+
+app.listen(3000, () => {
+    console.log("server is reeady! : http://localhost:3000")
+    })
