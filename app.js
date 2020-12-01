a308ada2ebe6c5a9679fdac950f538bcd6d95ae2
AnneVictoria <avduclaux@gmail.com>
2020-12-01 16:06:13 +0100

    files ready

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require("express"); // returns a function
-const app = express(); 
+const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.set("view engine", "hbs");
+
 app.get("/", (req, res) => {
-    res.render("home")
+  res.render("home");
 });
-       
+
 app.get("/about", (req, res) => {
-    res.render("about")
+  res.render("about");
 });
 
 app.listen(3000, () => {
-    console.log("server is reeady! : http://localhost:3000")
-    })
+  console.log("server is reeady! : http://localhost:3000");
+});
