1418e9f8801c977ff4c89be37b01684f7787cec6
Renzo <renzo.roca.m@gmail.com>
2020-12-22 18:13:02 +0100

    done

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.get('/', (req, res, next) => {
     res.sendFile(__dirname + '/views/home.html');
 });
+
+// definir public:
+app.use('/public', express.static('public'));
 
 app.get('/about', (req, res, next) => {
     res.sendFile(__dirname + '/views/about.html');
@@ -14,6 +18,13 @@ app.get('/works', (req, res, next) => {
     res.sendFile(__dirname + '/views/works.html');
 });
 
+app.get('/styles', (req, res, next) => {
+    res.sendFile(__dirname + '/public/css/styles.css');
+});
+
+app.get('/bg-img', (req, res, next) => {
+    res.sendFile(__dirname + '/public/images/top-mobile.jpg');
+});
 
 app.listen(port, () => {
     console.log(`Ready! Listening on port ${port}`);
