375fc3cf9ca38839f1347c1ea1eaf88a8c0b70b4
Javier Repi <xavirepi@hotmail.com>
2021-01-15 13:22:32 +0100

    finish assignment

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.static('public'));
 
 app.get('/home', (request, response, ext) => response.sendFile(__dirname + '/views/home-page.html'));
 app.get('/about', (request, response, ext) => response.sendFile(__dirname + '/views/about-page.html'));
-app.get('/works', (request, response, ext) => response.sendFile(__dirname + '/views/work.html'));
+app.get('/works', (request, response, ext) => response.sendFile(__dirname + '/views/works-page.html'));
 app.get('/photo-gallery', (request, response, ext) => response.sendFile(__dirname + '/views/photo-gallery-page.html'));
 
 app.listen(3000, () => console.log('App listening!'));
