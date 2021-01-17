dd127c9f5a1d51472653f2fdba94a6e74eeca84e
Alicia Parra <alicia@redparra.com>
2021-01-17 12:19:40 +0100

    Start server and all folders

---
+++ app.js
@@ -0,0 +1,26 @@
+//Express requirements
+const express = require('express');
+const app= express();
+
+//Add Public forlder for static files
+app.use(express.static('public'));
+
+//Routes to htmls
+app.get('/home', (request, response, next) => {
+    response.sendFile(__dirname + '/views/home.html')
+  });
+
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about.html')
+});
+
+app.get('/works', (request, response, next) => {
+  response.sendFile(__dirname + '/views/works.html')
+});
+
+app.get('/imagesgallery', (request, response, next) => {
+response.sendFile(__dirname + '/views/gallery.html')
+});
+
+//Port listener
+app.listen(3000, () => console.log('My first app listening on port 3000! '));
