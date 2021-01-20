6d600c90d4b7406ba6afecc6b85821730f614935
karuvsentor <karuvsentor@gmail.com>
2021-01-20 13:03:09 +0100

    fixed problem with Home

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ const app = express()
 
 app.use(express.static('public'))
 
-app.get('/home', (request, response, next) => {
-    response.sendFile(__dirname + 'views/home.html')
+app.get('/', (request, response, next) => {
+    response.sendFile(__dirname + '/views/home.html')
 });
 
 app.get('/about', (request, response, next) => {
-    response.sendFile(__dirname + 'views/about.html')
+    response.sendFile(__dirname + '/views/about.html')
 });
 
 app.get('/photo-gallery', (request, response, next) => {
-    response.sendFile(__dirname + 'views/photo-gallery.html')
+    response.sendFile(__dirname + '/views/photo-gallery.html')
 });
 
 app.get('/works', (request, response, next) => {
-    response.sendFile(__dirname + 'views/works.html')
+    response.sendFile(__dirname + '/views/works.html')
 });
 
 app.listen(3000,()=> console.log('Listening on port 3000!'))
