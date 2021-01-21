6966e9781c5b2ba993166173d0099b17c6296722
andrea morales <amoralesp88@gmail.com>
2021-01-20 21:36:31 -0600

    almost ready

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log('hola')
+
+const express = require("express")
+//const hbs = require('hbs') 
+const info = require("./data/profile")
+// const bootstrap = require('bootstrap') 
+
+const app = express();
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/home', (request, response, ext) => response.sendFile(__dirname + '/views/partials/home.html'));
+app.get('/about', (request, response, ext) => response.sendFile(__dirname + '/views/partials/about.html'));
+app.get('/works', (request, response, ext) => response.sendFile(__dirname + '/views/partials/works.html'));
+app.get('/photo-gallery', (request, response, ext) => response.sendFile(__dirname + '/views/partials/photoGallery.html'));
+
+
+app.listen(3000, () => console.log("Listening on port 3000!"));
+
+
---
+++ data/profile.js
@@ -0,0 +1,21 @@
+const jr = {
+    name: "John Joseph ",
+    lastName: "Moehringer",
+    nickname: "J. R. Moehringer",
+    birthday: {
+        year: 1964,
+        month: "diciembre",
+        day: 7,
+        age: 65,
+        city: "Nueva York, Estados Unidos"
+    },
+
+    education: "Universidad de Yale",
+    proffesion: "novelist and journalist",
+    Awards: ["Literary Award PEN Center USA West (1997)",
+    , "Livingston Award para Jóvenes Periodistas (1997)", "Premio Pulitzer de Periodismo (2000)"],
+    books: ["El bar de las grandes esperanzas", "Sutton", "Open", "El campeón ha vuelto", "A plena Luz"],
+    
+}
+
+module.exports.jr = jr
