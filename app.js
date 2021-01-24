0a904bc1de7ffd1011467d2fc3a3e804e2f857c7
Pedro Frias <plfriasf@gmail.com>
2021-01-24 15:14:33 -0800

    backend ready

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 //Route handlers 
+app.get('/', (req, res, next) => {
+    res.sendFile(__dirname + '/views/home.html')
+})
 
+app.get("/about", (req, res, next) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/gallery", (req, res, next) => {
+  res.sendFile(__dirname + "/views/gallery.html");
+});
+
+app.get("/works", (req, res, next) => {
+  res.sendFile(__dirname + "/views/works.html");
+});
 
 //App 
 app.listen(process.env.PORT || 3000, () => console.log("🏃‍ on port 3000"));
