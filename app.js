283fe3be13ac5b8b04e27d145b0ee920e9cfcb0e
Georgia Adams <georgia.adams92@gmail.com>
2021-02-01 22:23:02 +0100

    basic site completed

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res, next) => {
+  res.sendFile(__dirname + "/public/views/index.html");
+});
+
+app.get("/about", (req, res, next) => {
+  res.sendFile(__dirname + "/public/views/about.html");
+});
+
+app.get("/works", (req, res, next) => {
+  res.sendFile(__dirname + "/public/views/works.html");
+});
+
+app.get("/gallery", (req, res, next) => {
+  res.sendFile(__dirname + "/public/views/gallery.html");
+});
+
+app.listen(3000, () => {
+  console.log("This is port 3000");
+});
