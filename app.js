3b01362f7edf351753a5eb55cdd760b1b157fe62
Javi Sastre <javisastrecal@gmail.com>
2021-02-01 17:56:08 +0100

    finished lab

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res, next) => {
+  res.sendFile(__dirname + "/public/views/index.html");
+});
+
+app.get("/about", (req, res, next) => {
+  res.sendFile(__dirname + "/public/views/about.html");
+});
+
+app.get("/works", (req, res, next) => {
+  res.sendFile(__dirname + "/public/views/works.html");
+});
+
+app.get("/gallery", (req, res, next) => {
+  res.sendFile(__dirname + "/public/views/gallery.html");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT ${PORT}`);
+});
