54f40f4564f4c5d5d5e9d54788ff771e1c23ddaf
David Castejon <ombengue@hotmail.com>
2021-02-01 19:29:53 +0100

    Lab exercise completed

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static("public"));
+
+app.get('/about', (req, res, next) => {
+    res.sendFile(__dirname + "/public/views/about.html");
+});
+
+app.get('/gallery', (req, res, next) => {
+    res.sendFile(__dirname + "/public/views/gallery.html");
+});
+
+app.get('/', (req, res, next) => {
+    res.sendFile(__dirname + "/public/views/home.html");
+});
+
+app.get('/works', (req, res, next) => {
+    res.sendFile(__dirname + "/public/views/works.html");
+});
+
+app.listen(3000, () => {
+    console.log('server is connected');
+});
