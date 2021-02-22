ba835d71d574da6c05703486ceecd647936f9213
Naomi Yamanaka <n.yamanaka70@gmail.com>
2021-02-22 19:15:18 +0100

    add

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const hbs = require("hbs");
+const app = express();
+
+// do app config
+app.use(express.static(__dirname + "/public"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+
+// setup app routes
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/works", (req, res) => {
+  res.render("works");
+});
+
+// kickstart the http server
+app.listen(8888, () => {
+  console.log("ready @ http://localhost:8888");
+});
