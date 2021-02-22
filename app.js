6777800446b293ee55ccaa7b02d8b9f09755630d
Florian Jam <jam.florian@gmail.com>
2021-02-22 18:02:45 +0100

    first version of website

---
+++ app.js
@@ -0,0 +1,49 @@
+// GET express and hbs
+const express = require("express");
+const hbs = require ("hbs");
+const app = express();
+
+// Set up
+app.set(express.static(__dirname + "/public"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+
+// Define routes
+
+app.get("/", (request, response) => {
+    response.render("home")
+});
+
+app.get("/about", (request, response) => {
+    response.render("about")
+});
+
+app.get("/restaurants", (request, response) => {
+    response.render("restaurants");
+})
+
+app.get("/gallery", (request, response) => {
+    response.render("gallery");
+})
+
+app.get("/tokyo", (request, response) => {
+    response.render("tokyo")
+});
+
+app.get("/tokyo", (request, response) => {
+    response.render("kyoto");
+});
+
+app.get("/seattle", (request, response) => {
+    response.render("seattle");
+});
+
+app.get("/florence", (request, response) => {
+    response.render("florence");
+});
+
+app.get("*", (request, response) => console.log("Page not found"));
+
+app.listen(3000, () => {
+    console.log("Server is up: http://localhost:3000");
+});
