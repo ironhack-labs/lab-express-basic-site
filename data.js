13c212b82ebedaa294fe630541cef8fb6fafebc4
RudaF <floresta.ruda@gmail.com>
2021-02-23 18:48:01 -0300

    Finished

---
+++ data.js
@@ -0,0 +1,9 @@
+imgSrcs = [
+  { src: "/images/katyazamo.jpeg" },
+  { src: "/images/katyadools.jpeg" },
+  { src: "/images/katyafeet.jpeg" },
+  { src: "/images/katyastretch.jpeg" },
+  { src: "/images/katyamic.jpeg" },
+];
+
+module.exports = { imgSrcs };
---
+++ routes/index.routes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { imgSrcs } = require("../data");
+
+const router = express.Router();
+
+router.get("/", (request, response) => {
+  response.render("home");
+});
+
+router.get("/about", (request, response) => {
+  response.render("about");
+});
+
+router.get("/works", (request, response) => {
+  response.render("works");
+});
+
+router.get("/photo-gallery", (request, response) => {
+  response.render("photo-gallery", { imgSrcs });
+});
+
+module.exports = router;
