2c2cb12d97dae92bd5f946f7153c44cba4814531
Felipe Freitas <afelipe.freitas@gmail.com>
2021-02-23 19:55:47 -0300

    lab solved

---
+++ data.js
@@ -0,0 +1,29 @@
+const movies = [
+	{
+		name: 'Superbad',
+		picture:
+			'https://upload.wikimedia.org/wikipedia/en/8/8b/Superbad_Poster.png',
+	},
+	{
+		name: 'Scott Pilgrim vs. the World',
+		picture:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_fMIcDru1e3ymIiVE_ug4UE-9DRRjvaHnWA&usqp=CAU',
+	},
+	{
+		name: 'Juno',
+		picture:
+			'https://upload.wikimedia.org/wikipedia/en/3/36/Junoposter2007.png',
+	},
+	{
+		name: 'Year One',
+		picture:
+			'https://m.media-amazon.com/images/M/MV5BMTcyMjI2OTgxN15BMl5BanBnXkFtZTcwODU3ODkzMg@@._V1_.jpg',
+	},
+	{
+		name: 'Sausage Party',
+		picture:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvp8dEU1U-w-aDHs6fbHDLzVHExU-m9LZhVg&usqp=CAU',
+	},
+];
+
+module.exports = { movies };
---
+++ routes/index.routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const { movies } = require('../data');
+
+const router = express.Router();
+
+router.get('/home', (request, response) => {
+	response.render('index');
+});
+
+router.get('/about', (request, response) => {
+	response.render('about-page');
+});
+
+router.get('/photo-gallery', (request, response) => {
+	response.render('photo-gallery-page');
+});
+
+router.get('/works', (request, response) => {
+	response.render('works-page', { movies });
+});
+
+module.exports = router;
