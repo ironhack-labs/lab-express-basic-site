3ead1f80dee9704a9d7c5c806c03a01b29f83d27
Jean <jvictor669@hotmail.com>
2021-02-23 23:25:40 -0300

    Lab Solved

---
+++ routes/index.routes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const { album } = require("../work-data")
+
+const router = express.Router();
+
+router.get("/home", (request, response) => {
+    response.render("index")
+});
+
+router.get("/about", (request, response) => {
+    response.render("about")
+})
+
+router.get("/works", (request, response) => {
+    response.render("works", { album })
+})
+
+module.exports = router
---
+++ work-data.js
@@ -0,0 +1,36 @@
+const album = [
+    {
+        name: 'The Way of the Fist',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/619GINtkzdL._SY355_.jpg'
+    },
+    {
+        name: 'War is the Answer',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/A1ostDuTkNL._AC_SL1500_.jpg'
+    },
+    {
+        name: 'American Capitalist',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61UYenNnhaL.jpg'
+    },
+    {
+        name: 'The Wrong Side of Heaven and the Righteous Side of Hell, Volume 1',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61XgbaBS6AL._AC_SY355_.jpg'
+    },
+    {
+        name: 'The Wrong Side of Heaven and the Righteous Side of Hell, Volume 2',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/815CGm7MM7L._SY355_.jpg'
+    },
+    {
+        name: 'Got Your Six',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61sk7OX384L.jpg'
+    },
+    {
+        name: 'And Justice For None',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/A1edJNc8c%2BL._AC_SL1500_.jpg'
+    },
+    {
+        name: 'F8',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/71G2hRDPg6L._AC_SY355_.jpg'
+    },
+];
+
+module.exports = { album }
