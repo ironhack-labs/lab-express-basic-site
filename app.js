2c9ce5f731cc6a4d2bb44e268352deba0fbb69b1
Romain B <romain.bnd@gmail.com>
2021-03-01 21:47:28 +0100

    some improvements on homepage and style

--- app.js
+++ app.js
@@ -7,13 +7,50 @@ const app = express();
 app.use(express.static(__dirname + "/public"));
 app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
 
 //define the routes
-app.get(["/", "/home"], (req, res) => {
-    res.render("home");
+app.get(["/"], (req, res) => {
+    const photographers = [
+        {
+            firstName: "Brassai",
+            lastName: "",
+            photo: "img/brassai_main.jpg",
+            url: "/brassai"
+        },
+        {
+            firstName: "Vivian",
+            lastName: "Maier",
+            photo: "img/vivian_maier_bd.jpg",
+            url: "/maier"
+        },
+        {
+            firstName: "Joel",
+            lastName: "Meyerowitz",
+            photo: "img/joel-meyerowitz.jpg",
+            url: "/meyerowitz"
+        },
+        {
+            firstName: "Martin",
+            lastName: "Parr",
+            photo: "img/martin-parr.jpg",
+            url: "/parr"
+        }
+    ]
+    res.render("home", {photographers});
 })
 app.get("/about", (req, res) => {
-    res.render("about");
+    const data = {
+        title: "About"
+    }
+    res.render("about", data);
+})
+
+app.get("/maier", (req, res) => {
+    const data = {
+        title: "Vivian Maier"
+    }
+    
 })
 app.get("*", (req, res) => {
     res.send("sorry not found.")
