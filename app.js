1ffb8b7d3bd91cc2371afd896e07a3096e93c5e6
michaloumen <michelle.lmendonca@gmail.com>
2021-03-06 14:26:46 -0300

    Inicializando lab

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+//inicializando o meu express
+const app = express();
+
+//registrando a rota "/"
+app.get("/", (request, response) => {
+  console.log(request.url);
+
+  response.sendFile(__dirname + "/views/home.html");
+});
+
+//registrar a rota "/about"
+app.get("/about", (request, response) => {
+  console.log(request.url);
+
+  response.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/works", (request, response) => {
+  console.log(request.url);
+
+  response.sendFile(__dirname + "/views/works.html");
+});
+
+//ligando o servidor na porta 3003
+app.listen(3003, () => console.log("Server ligado na porta 3003"));
+
+app.use(express.static("public"));
+//então agora no html o caminho da pasta vai ser assim
