c6ad365d2930852546acc8167c5d4bbdafa6a391
Derek <shouldergiant@icloud.com>
2021-03-09 18:35:51 -0500

    Solved Lab

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require("express");
 
-const app = express.createApplication();
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (request, response, next) =>
+  response.sendFile(__dirname + "/views/home-page.html")
+);
+app.get("/about", (request, response, next) =>
+  response.sendFile(__dirname + "/views/about-page.html")
+);
+app.get("/works", (request, response, next) =>
+  response.sendFile(__dirname + "/views/works-page.html")
+);
 
 app.listen(3000);
