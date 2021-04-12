9aa2f849002947cb4fea5cd130f6c378ef0422ba
Andrea Sforza <andrearobertosforza@gmail.com>
2021-04-12 17:11:00 +0200

    Changed static

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 app.get('/home', (request, response) => {
     response.sendFile(__dirname + '/views/index.html');
