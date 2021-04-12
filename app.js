3ba2ac9bbb675d85ac8f55858e7686aa3ccfd3b5
Tigran Harutyunyan <tigrandeboris@outlook.com>
2021-04-12 23:06:27 +0200

    ADD About html page

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const  express = require('express');
+const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
@@ -7,4 +7,4 @@ app.get('/', (request, response) => response.sendFile(__dirname + '/views/index.
 app.get('/about', (request, response) => response.sendFile(__dirname + '/views/about.html'));
 app.get('/photos', (request, response) => response.sendFile(__dirname + '/views/photos.html'));
 
-app.listen(3000);
+app.listen(3000, () => console.log('Port 3000'));
