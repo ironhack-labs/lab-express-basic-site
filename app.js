1858ad524d2bbd24bbc95c230aeb5b846281eb98
l-masip <laia.masip.a@gmail.com>
2021-04-12 22:00:58 +0200

    css done

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.static('public'));
 app.get('/', (request, response) => response.sendFile(__dirname + '/views/index.html'));
 app.get('/about', (request, response) => response.sendFile(__dirname + '/views/about.html'));
 app.get('/works', (request, response) => response.sendFile(__dirname + '/views/works.html'));
+app.get('/gallery', (request, response) => response.sendFile(__dirname + '/views/gallery.html'));
 
 // Server started
 app.listen(3000, () => console.log('Listening on port 3000'));
