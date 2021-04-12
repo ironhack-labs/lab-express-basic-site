aee21235f15e1eed6586a62a73d895716695a254
Lorgio Roda <lorgioroda@Lorgios-MacBook-Air.local>
2021-04-12 11:08:16 -0400

    styles 💄

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('/public'))
+app.use(express.static('public'))
 
 app.get("/about", (request, response) => {
     response.sendFile(__dirname + '/views/about.html')
