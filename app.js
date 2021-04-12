5979bd1b698a4fd398efe30c8cd913a6b2dab268
Jesica Nugnez <jesica.nugnez@gmail.com>
2021-04-12 20:25:50 +0200

    navbar, main title, home and about, home and about linked, done

---
+++ app.js
@@ -0,0 +1,21 @@
+const express= require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (require, response) => {
+    response.sendFile(__dirname + '/views/home.html')
+});
+
+app.get('/about', (require, response) => {
+    response.sendFile(__dirname + '/views/about.html');
+})
+
+app.get('/gallery', (require, response) => {
+    response.sendFile(__dirname + '/views/works.html');
+})
+
+app.listen(3000, () => {
+    console.log('connected on port 3000')
+});
+
