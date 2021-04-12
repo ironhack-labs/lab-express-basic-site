e1afb3515702398838562acb54452c830e09ee71
Sisco Falcó <siscofalcop@gmail.com>
2021-04-12 19:44:24 +0200

    all pages done, working on css

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+app.use(express.static('public'));
+
+app.get('/', (request, response) => response.sendFile(__dirname + '/views/index.html'));
+app.get('/about', (request, response) => response.sendFile(__dirname + '/views/about.html'));
+app.get('/work', (request, response) => response.sendFile(__dirname + '/views/work.html'));
+app.get('/photogallery', (request, response) => response.sendFile(__dirname + '/views/photogallery.html'));
+app.use(express.static(__dirname + "/"));
+
+app.listen(3000, () => console.log('Listening on port 3000'));
