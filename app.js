768f8d22f8463f6ad9aaa1f6c955f4391d811911
QuimRobres <quim.robres@gmail.com>
2021-04-13 08:47:26 +0200

    Solved Lab

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-
-const express = requipe('express');
+const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('public'));//Especifica que tots els erxius exteriors es carregaran desde la carpeta public
+
 
 //Routes
-app.get('/index', (request, response) => response.sendFile(__dirname + '/views/index.html'));
+app.get('/', (request, response) => response.sendFile(__dirname + '/views/index.html'));
 app.get('/about', (request, response) => response.sendFile(__dirname + '/views/about.html'));
 app.get('/work', (request, response) => response.sendFile(__dirname + '/views/work.html'))
 
