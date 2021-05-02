4b3f7aa1d22d6c91e236a113c59b91f643310eb4
Sophie Krag <sophie.9@live.nl>
2021-05-02 23:27:13 +0200

    done

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const hbs = require('hbs')
+const path = require('path')
+
+const app = express()
+
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res, next) =>
+    res.render('index')
+)
+
+app.get('/about', (req, res, next) => {
+    res.render('about')
+})
+
+app.get('/works', (req, res, next) => {
+    res.render('works')
+})
+
+app.get('/gallery', (req, res, next) => {
+    res.render('gallery')
+})
+
+app.listen(3000)
