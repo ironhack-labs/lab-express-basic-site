ab2aa6d2b059039ea997a602fcef391a7fa1f397
Jordan Caley <jordancaley10@gmail.com>
2021-05-03 16:35:05 +0200

    Add itemCard partial

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.use(express.static(__dirname + "/public"));
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 
+hbs.registerPartials(__dirname + "/views/partials");
+
 // step 3 : routes handling
 app.get("/", (req, res) => {
   res.render("home.hbs", {pageTitle: "Au revoir, Paris !"});
