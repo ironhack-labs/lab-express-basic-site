f7e5d6a663c2c1ef35bdaeb6e5b5861f6e12fa8f
Jordan Caley <jordancaley10@gmail.com>
2021-05-03 16:22:37 +0200

    Add items array and first item card

--- app.js
+++ app.js
@@ -15,7 +15,18 @@ app.get("/", (req, res) => {
 });
 
 app.get("/items", (req, res) => {
-  res.render("items.hbs", {pageTitle: "Items"});
+  res.render("items.hbs", {
+    pageTitle: "Items",
+    items: [
+      {
+        name: "HEMA Electric Kettle",
+        photo: "https://www.hema.com/dw/image/v2/BBRK_PRD/on/demandware.static/-/Sites-HEMA-master-catalog/default/dw5d3dafdb/product/80010105_01_001_01.jpg?sw=1058&sh=1200&sm=fit",
+        alt: "Kettle",
+        color: "Pink",
+        specs: "https://www.hema.com/fr-be/manger-cuisiner/cuisiner/electromenager/bouilloire-sans-fil-1.7l-80010105.html"
+      }
+    ]
+  });
 });
 
 app.get("/about", (req, res) => {
