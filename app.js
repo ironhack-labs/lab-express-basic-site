761986202a9fab6b3c9c74fa2b04327d93831ff1
elenamarsili <e.marsili89@gmail.com>
2021-05-21 18:31:00 +0200

    lab finished

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express'); 
+const app = express(); 
+
+app.use(express.static('public'));
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html')
+} );
+
+app.get('/works', (req, res) => {
+    res.sendFile(__dirname + '/views/works.html')
+} );
+
+app.get('/photo-gallery', (req, res) => {
+    res.sendFile(__dirname + '/views/photo-gallery.html')
+} );
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/views/about.html')
+} );
+
+app.listen(3000)
