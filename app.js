532967263b4500a9171762fa98e6cab34ebad08d
Yani666 <yanis051989@gmail.com>
2021-06-01 18:44:36 -0500

    Solved Lab

---
+++ app.js
@@ -0,0 +1,35 @@
+//solicitar express
+const{response} = require('express')
+const express = require('express');
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.static("views"));
+
+//con get pedimos la ruta
+app.get('/', (request, response) => {
+    
+    response.sendFile(`${__dirname}/views/home.html`)
+  });
+
+  
+
+  app.get('/about', (request, response) => {
+    
+    response.sendFile(`${__dirname}/views/about.html`)
+  });
+
+  app.get('/works', (request, response) => {
+    
+    response.sendFile(`${__dirname}/views/works.html`)
+  });
+
+  app.get('/photos', (request, response) => {
+    
+    response.sendFile(`${__dirname}/views/photos.html`)
+  });
+  //inicializa servidor
+  app.listen(3000, () => {
+  console.log('corre')})
+
+
