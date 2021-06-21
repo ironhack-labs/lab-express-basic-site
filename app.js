af39d6decf3e050f39a0459a2284eee7a0137a7d
vhlsslhv <vhlsslhv@gmail.com>
2021-06-21 16:40:13 +0100

    Kendrick Website

---
+++ app.js
@@ -0,0 +1,22 @@
+const {response} = require("express");
+const express = require("express");
+
+const app=express();
+
+app.use(express.static("public"));
+
+app.get("/home", (request, response)=>{
+    response.sendFile(__dirname + "/views/home.html");
+});
+app.get("/about", (request, response)=> {
+    response.sendFile(__dirname + "/views/about.html");
+});
+app.get("/works", (request, response)=> {
+    response.sendFile(__dirname + "/views/works.html");
+});
+app.get("/gallery", (request, response)=> {
+    response.sendFile(__dirname + "/views/gallery.html");
+});
+
+
+app.listen(3000, ()=> console.log("Server listenning on port 3000"));
