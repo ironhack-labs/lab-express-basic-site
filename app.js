0531bd08d2239ac2c388eebb60f16c3cb66f5832
giorgiagalasso <giorgiagalasso5@gmail.com>
2021-06-21 16:11:27 +0100

    done

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+
+app.get('/home', (request, response) => {
+    response.sendFile(__dirname + "/views/home.html");
+})
+
+app.get('/about', (request, response) => {
+    response.sendFile(__dirname + "/views/about.html");
+})
+
+app.get('/works', (request, response) => {
+    response.sendFile(__dirname + "/views/works.html");
+})
+
+app.listen(8000, () => console.log("server listening on port 8000"));
