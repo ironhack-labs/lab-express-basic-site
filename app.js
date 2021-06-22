756e7813f19538d506da774779ac18f388a7da66
Miki Geoghegan <michaelageoghegan@hotmail.co.uk>
2021-06-22 18:52:47 +0200

    most recent commit

--- app.js
+++ app.js
@@ -2,36 +2,36 @@
 // 1. npm init
 // 2. npm install express
 // 3. import newly installed express module (library)
-const path = require('path')
-
 const express = require('express')
+const path = require('path')
 
 //4. setup the express.app() to initialise the library
 const app = express()
 
+app.use(express.static('public'))
 // 5. tell express.app() what it needs to respond to (set a URL route)
-app.all('/', mainRouteHandler)
+app.all('/home', mainRouteHandler)
 
 function mainRouteHandler(request, response) {
-    response.sendFile(path.join(__dirname, 'home.html'))
+    response.sendFile(path.join(__dirname, './views/home.html'))
 }
 
 app.all('/about', aboutRouteHandler)
 
 function aboutRouteHandler(request, response) {
-    response.send(`<h1>About: Gaz Oakley</h1>`)
+    response.sendFile(path.join(__dirname, './views/about.html'))
 }
 
-app.all('/works', worksRouteHandler)
+app.all('/recipes', recipesRouteHandler)
 
-function worksRouteHandler(request, response) {
-    response.send(`<h1>Recipes</h1>`)
+function recipesRouteHandler(request, response) {
+    response.sendFile(path.join(__dirname, './views/recipes.html'))
 }
 
 app.all('/gallery', galleryRouteHandler)
 
 function galleryRouteHandler(request, response) {
-    response.send(`<h1>Gallery</h1>`)
+    response.sendFile(path.join(__dirname, './views/gallery.html'))
 }
 
 //6. start the express.app() so that it "listens" to a port
@@ -40,4 +40,3 @@ app.listen(5000)
 //7. prepare the .html and .img resource
 
 //8. set static files serve
-app.use(express.static('public'))
