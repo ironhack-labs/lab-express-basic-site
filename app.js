aa466d2b5a1d1de0a591a761fea9186c3f11997d
kintrp <philipp.kintrup@googlemail.com>
2021-06-23 16:00:44 +0200

    lab done

---
+++ app.js
@@ -0,0 +1,32 @@
+// set's up express
+const express = require('express');
+const app = express();
+
+// this is needed to register the public folder
+// containing all the assets so that it is accessible in the browser
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+	// console.log('the home page')
+	// this variable is provided by node 
+	// console.log(__dirname);
+	// res.send('this is the home page');
+	res.sendFile(__dirname + '/views/index.html');
+});
+
+    app.get('/about', function (req, res) {
+        // console.log('the home page')
+        // this variable is provided by node 
+        // console.log(__dirname);
+        // res.send('this is the home page');
+        res.sendFile(__dirname + '/views/about.html');
+    });
+
+app.get('/works', function (req, res) {
+	// console.log('the home page')
+	res.sendFile(__dirname + '/views/works.html');
+});
+
+app.listen(3000, function () {
+	console.log('Server listening');
+});
